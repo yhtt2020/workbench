3cffc169dd42219dbb602644d4777e1a95f5b2f5
陈一枭 <cyx@thisky.com>
2021-09-12 18:41:17 +0800

    【修复】修复不出现title的问题

--- js/navbar/sidebarComp.js
+++ js/navbar/sidebarComp.js
@@ -7,7 +7,8 @@ Vue.component('sidebar', {
 			tasks: null,
 			browserUI: null,
 
-			items: []
+			items: [],
+			selectedIndex: -1
 		}
 	},
 	mounted: function() {
@@ -24,37 +25,49 @@ Vue.component('sidebar', {
 			that.tasks = tasks
 			that.items = tasks.getAll();
 			that.browserUI = browserUI
-		
+
 
 		}, 1000)
 
 	},
 	computed: {
+		isActive() {
+			return (id)=> {
+				return {
+					active:id == this.tasks.getSelected().id,
+				'app-task':true
+				}
+				
+			}
+			
+		},
 		getTitle() {
 			return (name, index) => {
 				//如果标签没名字，就给它取个默认名字
 				if (name == null) {
 					return l('defaultTaskName').replace('%n', index + 1)
+				}else{
+					return name
 				}
 			}
 		},
 		getIcon() {
 			return task => {
-				if(task.tabs.count()==0){
-					return  "icons/empty.png"
+				if (task.tabs.count() == 0) {
+					return "icons/empty.png"
 				}
-				let favicon= task.tabs.getAtIndex(0).favicon;
-				
+				let favicon = task.tabs.getAtIndex(0).favicon;
+
 				if (typeof favicon == 'undefined') {
 					return "icons/empty.png"
 				} else if (typeof favicon == 'undefined') {
 					return "icons/empty.png"
 				} else if (favicon == null) {
-					return  "icons/empty.png"
+					return "icons/empty.png"
 				} else {
 					return favicon.url
 				}
-		
+
 			}
 
 		}
@@ -63,15 +76,16 @@ Vue.component('sidebar', {
 	template: `
 	<div id="sidebar" style="left:0;height: 100vh;width: 45px;border-right: 1px solid #D7D7D7;">
 		<ul id="appGroup" class="app-task">
-			<li @click="openTask(task.id)" v-for="(task,i) in items" data-role="task" class="openTask" :task-id="task.id" :title="getTitle(task.name,i)">
+			<li @click="openTask(task.id,i)" v-for="(task,i) in items" data-role="task" :class="isActive(task.id)" :task-id="task.id" :title="getTitle(task.name,i)">
 				<img class="icon" :src="getIcon(task)">
 			</li>
 		</ul>
 	</div>
 	`,
 	methods: {
-		openTask(taskId){
+		openTask(taskId, index) {
 			this.browserUI.switchToTask(taskId)
+			this.selectedIndex = index
 		}
 	},
 })
