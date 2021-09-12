63f62dcf55199cf4868e2909f5e2059099737e22
陈一枭 <cyx@thisky.com>
2021-09-12 20:59:22 +0800

    【新增】初步引入drag，但是还未实现修改生效功能

--- js/navbar/sidebarComp.js
+++ js/navbar/sidebarComp.js
@@ -9,8 +9,11 @@ Vue.component('sidebar', {
 			browserUI: null,
 			//用于渲染组件的数组
 			items: [], //置顶的数组
-			pinItems: [] //下方列表
+			pinItems: [] ,//下方列表
+			
+			drag:false
 		}
+		
 	},
 	mounted: function() {
 
@@ -56,6 +59,7 @@ Vue.component('sidebar', {
 					let parsedIcon = that.getIcon(task)
 					let item = {
 						title: parsedTitle, //名称，用于显示提示
+						name: parsedTitle,
 						index: index, //索引组
 						id: task.id, //任务id
 						icon: parsedIcon, //图标
@@ -65,14 +69,33 @@ Vue.component('sidebar', {
 					that.items.push(item)
 
 				})
-				console.log(this.items)
 				return this.items
 			} else {
 				return {}
 			}
 		},
-		getPinItems() {
-			return {}
+		getPinItems: {
+
+			get: function() {
+				this.pinItems = []
+				let item = {
+					title: '收藏夹', //名称，用于显示提示
+					index: 0, //索引
+					id: 1, //id
+					icon: "icons/fav.png", //图标
+					draggable: true, //是否允许拖拽
+					ext: '' //额外的信息
+				}
+				this.pinItems.push(item)
+				return this.pinItems
+			},
+			// setter
+			set: function(newValue) {
+				this.pinItems = newValue
+				console.log('触发了修改元素')
+				console.log(newValue)
+			}
+
 		},
 		isActive() {
 			return (id) => {
@@ -89,25 +112,32 @@ Vue.component('sidebar', {
 	template: `
 	<div id="sidebar" style="left:0;height: 100vh;width: 45px;border-right: 1px solid #D7D7D7;">
 		<ul id="pinGroup" class="app-task">
-		<li click="openFav()" data-role="task"  title="收藏夹">
-			<img class="icon" src="icons/fav.png">
-		</li>
-			<li v-for="(item,i) in pinItems" @click="openTask(item.id,i)" data-role="task" :class="isActive(item.id)" :item-id="item.id" :title="item.title">
-				<img class="icon" :src="item.icon">
-			</li>
+
+			
+			 <draggable v-model="pinItems" group="sideBtn" animation="300" dragClass="dragClass" ghostClass="ghostClass" chosenClass="chosenClass" @start="onStartPin" @end="onEndPin">
+			                    <transition-group>
+								<li v-for="(item,i) in getPinItems" :key="item.id" @click="openTask(item.id,i)" data-role="task" :class="isActive(item.id)" :item-id="item.id" :title="item.title">
+									<img class="icon" :src="item.icon">
+								</li>	
+			                    </transition-group>
+			  </draggable>
 		</ul>
 		
 		<div style="border-top: 1px solid lightgrey;margin: 8px;"></div>
 		
 		<ul id="appGroup" class="app-task">
-			<li @click="openTask(item.id,i)" v-for="(item,i) in getItems" data-role="task" :class="isActive(item.id)" :task-id="item.id" :title="item.title">
+		<draggable v-model="items" group="sideBtn" animation="300" dragClass="dragClass" ghostClass="ghostClass" chosenClass="chosenClass" @start="onStart" @end="onEnd">
+		                   <transition-group>
+			<li @click="openTask(item.id,i)" v-for="(item,i) in getItems"  :key="item.id" data-role="task" :class="isActive(item.id)" :task-id="item.id" :title="item.title">
 				<img class="icon" :src="item.icon">
 			</li>
+			</transition-group>
+			</draggable>
 		</ul>
 	</div>
 	`,
 	methods: {
-		openFav(){
+		openFav() {
 			//todo实现打开书签
 		},
 		openTask(taskId, index) {
@@ -138,7 +168,31 @@ Vue.component('sidebar', {
 				return favicon.url
 			}
 
-		}
+		},
+		//开始拖拽事件
+		onStart() {
+			this.drag = true;
+		},
+		//拖拽结束事件
+		onEnd(end) {
+			console.log(end)
+			this.drag = false;
+			console.log('拖动了普通列表')
+			console.log(this.items)
+			
+		},
+		//开始拖拽事件
+		onStartPin() {
+			this.drag = true;
+		},
+		//拖拽结束事件
+		onEndPin(end) {
+			console.log(end)
+			this.drag = false;
+			console.log('拖动了置顶列表')
+			console.log(this.pinItems)
+			
+		},
 
 	},
 })
