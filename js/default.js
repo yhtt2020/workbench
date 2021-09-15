047fcb1b53654d5b14eec39f8d088c5ae7dff86d
陈一枭 <cyx@ourstu.com>
2021-09-15 22:16:44 +0800

    【调整】补充注释

--- js/default.js
+++ js/default.js
@@ -186,16 +186,16 @@ window.onload = function() {
 	Vue.use(Vuex)
 	const store = new Vuex.Store({
 		state: {
-			pinItems: [], //置顶区域的items
+			pinItems: [], //置顶区域的items，横线上方部分
 			items: [] //普通区域的items
 		},
 		getters: {
-
+			//将任务填充到item列表当中，系统会自动刷新一次列表。
+			//算法主要是遍历两个组，然后将task按顺序填充进去，如果有多的，就在队列尾部插入（应对插入新组的情况），如果有少的，则从队尾删除相应数量（应对删除任务的情况）
 			fillTasksToItems: state => {
-
 				//将tasks转化为items
-				console.log('fillTasksToItems')
 				let replaceIndex = 0
+				//遍历置顶的区域，把任务都替换进来
 				state.pinItems.forEach(function(pinItem, indexPin) {
 					if (pinItem.type == "task") {
 						console.log(tasks.get(replaceIndex))
@@ -205,9 +205,11 @@ window.onload = function() {
 					}
 
 				});
-				console.log(state.pinItems);
+				
+				//遍历非置顶区域，把任务都替换进来
 				state.items.forEach(function(item, index) {
 					if (item.type == "task") {
+						//如果现存的任务都替换进去了，接下来就不是再替换了，而是删除掉剩余的task，以保持两边同步
 						if (replaceIndex >= tasks.getLength()) {
 							state.items.splice(index, 1)
 						} else {
@@ -219,6 +221,7 @@ window.onload = function() {
 					}
 
 				});
+				//如果到最后，还有没替换的，就在队尾插入剩余的任务
 				let last = tasks.getLength() - replaceIndex
 				if (last > 0) {
 					//如果有剩下的，就往数组最后添加上
@@ -229,20 +232,22 @@ window.onload = function() {
 				} 
 
 
+				//返回最终的侧边栏项目
 				let result = {
 					'pinItems': state.pinItems,
 					'items': state.items
 				}
 				return result
-				//1.循环一遍items，找到新的tasks里该顺序的task，替换掉对应的item即可
+				
 			},
+			//获取全部的item，包括置顶和非置顶的部分
 			getItems: state => {
 				return {
 					'pinItems': state.pinItems,
 					'items': state.items
 				}
 			},
-
+			//获取单个item的标题，自动处理无分组名的情况，支持语言包
 			getItemTitle: (state) => (task) => {
 				//如果标签没名字，就给它取个默认名字
 				if (task.name == null) {
@@ -251,6 +256,7 @@ window.onload = function() {
 					return task.name
 				}
 			},
+			//获取任务的icon
 			getItemIcon: (state) => (task) => {
 				if (task.tabs.count() == 0) {
 					return "icons/empty.png"
@@ -268,6 +274,7 @@ window.onload = function() {
 				}
 
 			},
+			//从任务转化出一个item，用于items列表
 			getItemFromTask: (state) => (task) => {
 				let parsedTitle = store.getters.getItemTitle(task)
 				let parsedIcon = store.getters.getItemIcon(task)
@@ -287,16 +294,19 @@ window.onload = function() {
 
 		},
 		mutations: {
+			//设置置顶区域的item
 			savePinItems(state, pinItems) {
 				//将根据pin和items生成Tasks
 				state.pinItems = pinItems
 			},
+			//设置item
 			saveItems(state, items) {
 				state.items = items
 			},
 
+			//从任务组读入置顶区域，目前还不支持存档，每次进去会重新填充一次
 			initItems: state => {
-				console.log('initItems')
+			
 				//在置顶区域插入一个收藏夹的图标按钮
 				let item = {
 					title: '收藏夹', //名称，用于显示提示
@@ -309,8 +319,6 @@ window.onload = function() {
 					type: 'system-bookmark'
 				}
 				state.pinItems.push(item)
-
-
 				if (tasks != null) {
 					//从任务当中取得任务的小组
 					let tasksArray = tasks.getAll()
