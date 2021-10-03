11b02a2a6dbd85cfc598980ec4df709986b6297c
陈一枭 <cyx@ourstu.com>
2021-10-03 11:01:31 +0800

    【新增】新增开发环境和生产环境的api地址的判断//todo还需要加入环境的判断

--- pages/apps/preload.js
+++ pages/apps/preload.js
@@ -33,33 +33,39 @@ window.$appsRestore = appsRestore
 
 
 function initAppsData() {
-	const apiUrl = 'http://d.xiangtian.ren/api/apps.json'
+	const DevUrl = 'http://127.0.0.1:7002/apps/allApps' //使用egg接口返回数据
+	const PrdUrl = 'http://d.xiangtian.ren/api/apps.json' //正式站点的apps数据
+	const isDevelopmentMode = true//todo 需要加入开发环境的判断 process.argv.some(arg => arg === '--development-mode')
+	const url = isDevelopmentMode ? DevUrl : PrdUrl
+	const apiUrl = url
 	const apiNative = '/../../api/apps.json'
 	var resultsPromise
-	window.$appsApiData=null
+	window.$appsApiData = null
 	const requestOptions = {
-	  method: 'get',
-	  headers: { 'Content-Type': 'application/json' }
+		method: 'get',
+		headers: {
+			'Content-Type': 'application/json'
+		}
 	}
 	fetch(apiUrl, requestOptions)
-	  .then(res => res.json())
-	  .then(function (result) {
-		window.$appsApiData=result
-	  })
-	  .catch(function (e) {
-	    // retry once
-	    setTimeout(function () {
-	      console.warn('无法获取到api')
-	      fetch(apiUrl, requestOptions)
-	        .then(res => res.json())
-	        .then(function (result) {
-			   window.$appsApiData=result
-	        })
-	    }, 5000)
-	  })
-	
-	
-	window.nativeData= JSON.parse(fs.readFileSync(__dirname+apiNative, 'utf-8'))
+		.then(res => res.json())
+		.then(function(result) {
+			window.$appsApiData = result
+		})
+		.catch(function(e) {
+			// retry once
+			setTimeout(function() {
+				console.warn('无法获取到api')
+				fetch(apiUrl, requestOptions)
+					.then(res => res.json())
+					.then(function(result) {
+						window.$appsApiData = result
+					})
+			}, 5000)
+		})
+
+
+	window.nativeData = JSON.parse(fs.readFileSync(__dirname + apiNative, 'utf-8'))
 
 }
 initAppsData()
