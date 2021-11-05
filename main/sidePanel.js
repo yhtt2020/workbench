ca71b9204312198fcd6d6ac59dabf3683c3bc58c
陈一枭 <cyx@thisky.com>
2021-11-05 14:36:16 +0800

    【调整】调整打开书签的实现

--- main/sidePanel.js
+++ main/sidePanel.js
@@ -495,7 +495,8 @@ ipc.on('receiveGlobal', function(event, data) {
 })
 
 //显示书签的时候，将sidepanel隐藏起来
-ipc.on('showBookmarks', function() {
+ipc.on('openBookmarks', function() {
+  sendIPCToWindow(mainWindow, 'showBookmarks') //直传给mainWindow，让它唤出书签页面
 	sidePanel.hide()
 })
 ipc.on('openSetting', function() {
