423f0ed77d381ae996482fbf7e57e2385b2d619c
陈功哲 <105950987@qq.com>
2021-11-05 23:05:06 +0800

    [修复]: 应该已解决刷新白屏 allapps报错 dialog页脚清除

--- pages/newtab/components/appStore/index.js
+++ pages/newtab/components/appStore/index.js
@@ -1,21 +1,14 @@
 Vue.component('app-dialog', {
   props: {
     //dialog显示
-    // adVisible: {
-    //   type: Boolean,
-    //   default: false
-    // }
+    adVisible: {
+      type: Boolean,
+      default: false
+    },
+    //所有应用数据
+    allApps: {
+      type: Array,
+    }
   },
-	// data() {
-	// 	return {
-  //     dd:1
-	// 	};
-	// },
-	template:'#appstore',
-  methods: {
-    // handleOk() {
-    //   console.log('点击了提交')
-    //   this.adVisible = false
-    // }
-  }
+	template:'#appstore'
 })
