11ca7ae7ca3b45f129f96618f7e10a08c61d5657
cyx@thisky.com <cyx@thisky.com>
2021-11-25 08:46:38 +0800

    【修复】在部分机型上不一定能接收到ipc

--- main/handleAxios.js
+++ main/handleAxios.js
@@ -37,6 +37,7 @@ const handleAxios =  {
 
     //分享组
     ipc.on('shareTask', async (event, arg) => {
+      sidePanel.get().webContents.send('message',{type:'loading',config:{content:'正在生成分享链接。',key:"shareTask"}})
       const data = {
         title: `${storage.getItem(`userInfo`).nickname} 在${new Date()}分享的组`,
         uid: storage.getItem(`userInfo`).uid,
@@ -50,22 +51,24 @@ const handleAxios =  {
         })
         if(createRes.code === 1000) {
           clipboard.writeText(createRes.data.shareTask_link)
-          sidePanel.get().webContents.send('message',{type:'success',config:{content:'复制成功，已为您自动排除系统页面。'}})
+          sidePanel.get().webContents.send('message',{type:'success',config:{content:'复制成功，已为您自动排除系统页面。',key:"shareTask"}})
         }else{
-          sidePanel.get().webContents.send('message',{type:"error",config:{content:'分享失败，服务器繁忙。'}})
+          sidePanel.get().webContents.send('message',{type:"error",config:{content:'分享失败，服务器繁忙。',key:"shareTask"}})
 
         }
       }
       catch(err){
-        sidePanel.get().webContents.send('message',{type:"error",config:{content:'分享失败，请检查网络。'}})
+        sidePanel.get().webContents.send('message',{type:"error",config:{content:'分享失败，请检查网络。',key:"shareTask"}})
         console.log(err)
       }
 
     })
   }
 }
-
-app.whenReady().then(()=>{
+app.on('ready',()=>{
   handleAxios.initialize()
 })
+// app.whenReady().then(()=>{
+//   handleAxios.initialize()
+// })
 
