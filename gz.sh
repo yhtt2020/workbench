79bc4b416e741912ffb1fa31607b4137bc443d93
陈一枭 <cyx@thisky.com>
2022-01-28 09:18:40 +0800

    【公证】修改公证方法

--- gz.sh
+++ gz.sh
@@ -1,3 +1,3 @@
 xcrun altool --notarize-app --primary-bundle-id "com.thisky.browser" --username "appstore@thisky.com" \
 --password "elrb-fhob-kdbx-cslg" --asc-provider "S2KRGCMM87" -t osx \
---file ./setup/想天浏览器-2.4.9-arm64.dmg
+--file ./setup/想天浏览器-2.5.2-arm64.dmg
