487c6fca4569bb4012b7c2a6b83c6b53b2e9bbb9
陈一枭 <cyx@thisky.com>
2022-02-23 14:17:36 +0800

    【修复】修复winxp上左侧栏一个像素高度差

--- main/sidePanel.js
+++ main/sidePanel.js
@@ -183,10 +183,15 @@ class SidePanel {
     }
     if(isWin()){
       setHeight+=1
+      if(process.getSystemVersion().startsWith('6.1')){//win7少也要少一个像素
+        setHeight-=1
+      }
     }
 	  if(isWin11()){
+      //win11要少一个像素
 		setHeight-=1
 	  }
+
     this._sidePanel.setBounds({
       x: setX,
       y: setY,
