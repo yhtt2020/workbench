d3eb62589fd51d268c36b5a36e90270e2c0673d6
陈一枭 <yixiao2020@qq.com>
2022-03-06 23:47:47 +0800

    【调整】修改项目名称

--- pages/fav/fav/vue.config.js
+++ pages/fav/fav/vue.config.js
@@ -2,3 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = '收藏夹'
+      return args
+    })
+  }
+}
