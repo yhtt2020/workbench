b6a5c47917fd474213ac0780d84684225d5f777f
陈一枭 <yixiao2020@qq.com>
2022-03-10 19:54:12 +0800

    【修复】过滤掉不包含content内容的消息，避免其他消息干扰

--- js/preload/fav.js
+++ js/preload/fav.js
@@ -1,9 +1,11 @@
 window.addEventListener('message', function (e) {
-  const content={
-    href:e.origin,
-    ...e.data.content
+  if (e.data.content) {
+    const content = {
+      href: window.location.href,
+      ...e.data.content
+    }
+    console.log('最终得到的采集内容', content)
+    ipc.send('getFavContent', {content: content})
   }
-  console.log('最终得到的采集内容',content)
-  ipc.send('getFavContent',{content:content})
 })
 console.log('监听fav的preload')
