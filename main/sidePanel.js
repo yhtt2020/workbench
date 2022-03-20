5b13385c741d16d55afbeaa533b8d1e49a6ff64c
陈一枭 <yixiao2020@qq.com>
2022-03-20 20:15:58 +0800

    【修复】修复windows上，无边框窗体不显示阴影的bug 设置resizable为true，并禁止will-resize

--- main/sidePanel.js
+++ main/sidePanel.js
@@ -816,7 +816,7 @@ function createAllAppsWindow(){
     acceptFirstMouse: true,
     alwaysOnTop: true,
     show:false,
-    resizable:false,
+   // resizable:false,
     frame:false,
     webPreferences: {
       //preload: __dirname+'/pages/saApp/settingPreload.js',
@@ -833,6 +833,9 @@ function createAllAppsWindow(){
         '--app-name=' + app.getName()
       ]
     }
+  })
+  allAppsWindow.on('will-resize',(event)=>{
+    event.preventDefault()
   })
   allAppsWindow.loadURL('file://'+path.join(__dirname,'/pages/saApp/list.html'))
   allAppsWindow.on('close',(event)=>{
