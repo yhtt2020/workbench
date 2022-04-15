b3c113fc67f45cf6bc9851a31aa16a25c4bf46a5
yian <105950987@qq.com>
2022-04-15 09:15:21 +0800

    fix: 修复移动标签到其他标签组的一系列bug问题

--- pages/selectTask/preload.js
+++ pages/selectTask/preload.js
@@ -1,48 +1,10 @@
-const path = require("path");
-const fs = require("fs");
-var electron = require("electron");
-var ipc = electron.ipcRenderer;
+var ipc = require("electron").ipcRenderer;
 
-let mainWindowId = 0;
+function getMessageType(data) {
+  if (data && data.message) return data.message;
+  else return "";
+}
 
-window.globalArgs = {};
-let loaded = false;
-process.argv.forEach(function (arg) {
-  if (arg.startsWith("--")) {
-    var key = arg.split("=")[0].replace("--", "");
-    var value = arg.split("=")[1];
-    globalArgs[key] = value;
-  }
-});
-if (navigator.platform === "MacIntel") {
-  window.platformType = "mac";
-} else if (navigator.platform === "Win32") {
-  window.platformType = "windows";
-} else {
-  window.platformType = "linux";
-}
-var savePath =
-  window.globalArgs["user-data-path"] +
-  (platformType === "windows"
-    ? "\\sessionRestore.json"
-    : "/sessionRestore.json");
-var savedStringData = null;
-var tasks = [];
-try {
-  savedStringData = fs.readFileSync(savePath, "utf-8");
-} catch (e) {
-  console.warn("failed to read session restore data", e);
-}
-try {
-  var data = JSON.parse(savedStringData);
-  data.state.tasks.forEach(function (task) {
-    tasks.push(task);
-  });
-} catch (e) {
-  console.error("restoring session failed: ", e, savedStringData);
-}
-console.log(tasks);
-window.tasks = tasks;
 
 window.addEventListener("message", function (e) {
   if (!e.origin.startsWith("file://")) {
@@ -66,11 +28,6 @@ window.addEventListener("message", function (e) {
       break;
   }
 });
-
-function getMessageType(data) {
-  if (data && data.message) return data.message;
-  else return "";
-}
 
 //从应用购物车跳转过来的
 ipc.on("appsCartList", (event, arg) => {
