d7d3a01148287f4904c307e3a5ad6739f7689fdd
陈一枭 <cyx@thisky.com>
2022-04-27 14:21:26 +0800

    【新增】增加一个分享按钮在标签栏右侧

--- js/navbar/addTabButton.js
+++ js/navbar/addTabButton.js
@@ -1,10 +1,14 @@
 var browserUI = require('browserUI.js')
 
 var addTabButton = document.getElementById('add-tab-button')
-
+var shareButton= document.getElementById('share-button')
 function initialize () {
   addTabButton.addEventListener('click', function (e) {
     browserUI.addTab()
+  })
+
+  shareButton.addEventListener('click',function (){
+    require('./tabBar.js').shareTask()
   })
 }
 
