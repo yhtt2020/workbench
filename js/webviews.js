b146b870d6778c61ccde4dc2db76df430ba8d925
yian <105950987@qq.com>
2022-05-27 16:37:46 +0800

    feat: ldb存储本地当前设备新手引导进度信息 & 暂时先开放新手引导url的node权限

--- js/webviews.js
+++ js/webviews.js
@@ -236,6 +236,24 @@ const webviews = {
       ],
       allowPopups:true
     }
+  } else if(sourceUrl.startsWith('http://localhost:5008/guide')) {
+    webPreferences={
+      preload: __dirname + '/pages/noobGuide/preload.js',
+      nodeIntegration: true, //node集成开高了
+      contextIsolation:false,
+      enableRemoteModule: true,
+      scrollBounce: false,
+      sandbox: false,
+      safeDialogs:false,
+      safeDialogsMessage:false,
+      additionalArguments: [
+        '--user-data-path=' + window.globalArgs['user-data-path'],
+        '--app-version=' + window.globalArgs['app-version'],
+        '--app-name=' +  window.globalArgs['app-name'],
+        //'--is-Dev='+window.globalArgs['development--mode']
+      ],
+      allowPopups:true
+    }
   }
 
 
