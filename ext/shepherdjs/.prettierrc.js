19896c47534eeb36c9aa3c3210f48ed20b89bf0e
穆伊凡 <1641124292@qq.com>
2022-06-16 16:55:15 +0800

    穆伊凡 6.16 改变脚本的引入方式 删除cdn引入

---
+++ ext/shepherdjs/.prettierrc.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  arrowParens: 'always',
+  trailingComma: 'none',
+  singleQuote: true
+};
---
+++ ext/shepherdjs/babel.config.js
@@ -0,0 +1,24 @@
+module.exports = function(api) {
+  api.cache(true);
+
+  return {
+    env: {
+      development: {
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              loose: true
+            }
+          ]
+        ]
+      },
+      test: {
+        presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+        plugins: [
+          'transform-es2015-modules-commonjs'
+        ]
+      }
+    }
+  };
+};
