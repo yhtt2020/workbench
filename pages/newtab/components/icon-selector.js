3f776ffc9304f5bd3e7cfe899f4c2a549acc0627
陈一枭 <cyx@thisky.com>
2022-06-20 13:21:28 +0800

    【调整】简化新桌面的图标列表

--- pages/newtab/components/icon-selector.js
+++ pages/newtab/components/icon-selector.js
@@ -20,15 +20,16 @@ const iconSelector=Vue.component('icon-selector', {
     return {
       value: 'bulb',
       icons: [
-        'bulb','account-book', 'alert', 'api', 'appstore', 'audio', 'bank', 'bell', 'book', 'bug',  'calculator', 'build', 'calendar', 'camera', 'car',
-        'carry-out','cloud','code','compass','contacts','container','control','credit-card','crown','customer-service','dashboard','database','dislike','environment',
-        'experiment','eye-invisible','eye','file-add','file-excel','file-exclamation','file-image',
-        'file-markdown','file-pdf','file-ppt','file-text','file-unknown','file-word','folder','folder-open','frown','funnel-plot','gift','hdd','heart','home','hourglass','idcard',
-        'insurance','interaction','layout','like','lock','mail','medicine-box','meh','message','mobile','money-collect','pay-circle','notification','phone','picture','play-square',
-        'printer','profile','pushpin','property-safety','read','reconciliation','red-envelope','rest','rocket','safety-certificate','save','schedule','security-scan','setting','shop',
-        'shopping','skin','smile','sound','star','switcher','tablet','tag','tags','tool','thunderbolt','trophy','unlock','usb','video-camera','wallet','apartment','audit','barcode','bars','' +
-        'block','border','branches','ci','cloud-download','cloud-server','cloud-sync','cloud-upload','cluster','coffee','copyright' ,'deployment-unit','desktop','disconnect','dollar',
-        'download','ellipsis','euro','exception','export','file-done','file-jpg','file-protect','file-sync','file-search','fork','gateway','global'
+        'bulb', 'alert', 'api', 'appstore', 'audio', 'bank', 'bell',
+        'bug',   'calendar', 'camera', 'car',
+        'carry-out','cloud','code','compass','control',
+        'crown','customer-service','dashboard','eye',
+        'folder','gift','heart','home',
+        'like','mail','message','mobile','notification','picture','play-square'
+        ,'profile','pushpin','read','rocket','schedule','shop',
+        'shopping','skin','smile','star','tablet','tag','tool','thunderbolt','trophy'
+        ,'video-camera','wallet','apartment','audit','bars','coffee' ,'desktop','dollar',
+       'global'
 
       ]
     }
