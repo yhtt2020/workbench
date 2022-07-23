faf6d3e5b6ec63e3ca4d543bde5b46cd2e6597c2
穆伊凡 <1641124292@qq.com>
2022-07-23 17:25:51 +0800

    [新增] 应用安装打开实现调整

--- main/appStore.js
+++ main/appStore.js
@@ -1,12 +1,12 @@
 
 
 app.whenReady().then(()=> {
-  ipc.on('openPage', (event, args) => {
-    mainWindow.webContents.send('addTab', {url: args})
-  })
-
-
-  ipc.on('openApp', (event, args) => {
+  // ipc.on('openPage', (event, args) => {
+  //   mainWindow.webContents.send('addTab', {url: args})
+  // })
+  //
+  //
+  ipc.on('addNewApp', (event, args) => {
     SidePanel.send('addApp',args)
   })
   ipc.on('openSystemApp',(event,args)=>{
