f2fb4231a5a8f67f70f4fef919120fd1313a7763
陈一枭 <cyx@thisky.com>
2022-08-15 10:43:42 +0800

    【调整】重构appManger，增加saApp类

---
+++ src/main/saAppClass.js
@@ -0,0 +1,12 @@
+class SaApp {
+  instance //saApp  { info:saApp,window:window,view:view}
+
+  constructor (saApp) {
+    this.instance=saApp
+  }
+  getIconImage(){
+
+  }
+}
+
+module.exports=SaApp
