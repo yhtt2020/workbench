bbce35ecbc35f5f991f93de0334b7b5b0aceac30
cyx <cyx@thisky.com>
2022-08-31 17:08:39 +0800

    【新增】增加点赞按钮 【调整】调整交互操作

--- src/browserApi/user.js
+++ src/browserApi/user.js
@@ -3,6 +3,7 @@ const {
 } = require('electron')
 const ipc = ipcRenderer
 let apiUserInfo={}
+const {api} =require('../../server-config')
 
 const user={
   get:async () => {
@@ -17,6 +18,22 @@ const user={
     })
     ipc.send('login')
   },
+  /**
+   *
+   * @param uid 0则为自己
+   * @param type 类型app为应用内打开，web为网页打开
+   */
+  openSpace(uid=0,type='app'){
+    //todo 自己的逻辑
+    let uidStr=uid===0?'':'?uid='+uid
+    let url=api.getUrl(api.API_URL.user.space)+uidStr
+    console.log(url)
+    if(type==='app'){
+      ipc.send('handleTsbProtocol',{url:'tsb://app/redirect/?package=com.thisky.com&url='+url})
+    }else{
+      ipc.send('addTab',{url:url})
+    }
+  }
 
 }
 module.exports=user
