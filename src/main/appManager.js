a307ff414b6b87ffc4d23cc9752d465185b65153
穆伊凡 <1641124292@qq.com>
2022-12-07 17:25:58 +0800

    [删除] 应用市场开发模式环境注释掉

--- src/main/appManager.js
+++ src/main/appManager.js
@@ -910,10 +910,10 @@ class AppManager {
     } else if (saApp.package === 'com.thisky.fav') {
       url='file://'+path.join(___dirname,'/pages/fav/index.html')
     }
-    if (saApp.package === 'com.thisky.appStore' ) {
-      url = 'http://localhost:5008/'
-      // url=saApp.url
-    }
+    // if (saApp.package === 'com.thisky.appStore' ) {
+    //   url = 'http://localhost:5008/'
+    //   // url=saApp.url
+    // }
     if (saApp.package === 'com.thisky.fav' && isDevelopmentMode) {
       // appView.webContents.openDevTools()
     }
