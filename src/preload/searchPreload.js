e03eb533566906e9813dbcae1dbff7db10edb2e4
陈一枭 <cyx@thisky.com>
2023-02-08 20:28:46 +0800

    【重构】基本完成重构

--- src/preload/searchPreload.js
+++ src/preload/searchPreload.js
@@ -1,6 +1,7 @@
 window.$models= {
   appModel: require('../model/appModel'),
   settingModel:require('../model/settingModel.js'),
-  placesModel:require('../../pages/util/model/placesModel.js')
+  placesModel:require('../../pages/util/model/placesModel.js'),
+  statsh: require('../../js/util/statsh/statsh.js')
 }
 window.$models.appModel.initDb()
--- vite/packages/search/pages/Index.vue
+++ vite/packages/search/pages/Index.vue
@@ -1,17 +1,22 @@
 <script lang="ts">
 import tools from '../../../src/util/tools.js'
-let {appModel,placesModel}=window.$models
-let ipc=require('electron').ipcRenderer
+
+let {appModel, placesModel,statsh} = window.$models
+let ipc = require('electron').ipcRenderer
 import {mapActions, mapWritableState, mapState, mapGetters} from "pinia";
+import {SearchOutlined, SmileOutlined,CloseOutlined} from '@ant-design/icons-vue'
 import {appStore} from "../store";
+import gpt from '../assets/img/gpt.svg'
 export default {
   components: {
+    SearchOutlined, SmileOutlined,CloseOutlined
   },
   watch: {
     searchWord: {
       handler: tools.debounce(async function (newValue, oldValue) {
         this.searchResult = []
         if (newValue.length > 0) {
+          console.log('设置为false')
           this.openFirst = false
           let appResult = await this.searchApp(newValue)
           this.searchResult = this.searchResult.concat(appResult, this.searchTab(newValue), this.searchTask(newValue))
@@ -42,6 +47,8 @@ export default {
   },
   data() {
     return {
+      gpt,
+      tip:'',
       indicator: '<a-icon type="loading" style="font-size: 24px" spin />',
       contentLoading: false,
       searchWord: '',
@@ -78,11 +85,11 @@ export default {
       recentReadyedIndex: 0
     }
   },
-  computed:{
-    ...mapState(appStore,['getAllTasks']),
+  computed: {
+    ...mapState(appStore, ['getAllTasks']),
     compSearchResult() {
       let findResult = this.tags.find(v => v.checked === true)
-      if(findResult.label === '全部') {
+      if (findResult.label === '全部') {
         this.itemReadyedIndex = 0
         this.itemReadyedItem = this.searchResult[0]
         this.calculateAreaHeight(this.searchResult)
@@ -121,40 +128,52 @@ export default {
     this.calculateAreaHeight(await this.getHistoryCount())
     this.recentOpenedHistory = await this.handleRecentOpenedHistory()
     this.recentReadyedItem = this.recentOpenedHistory[0]
+    this.tip=localStorage.getItem('closeTip')
   },
   methods: {
+    closeTip(){
+      localStorage.setItem('closeTip','1')
+      this.tip='1'
+    },
+    changeHeight(height: number) {
+      ipc.send('changeBrowserWindowHeight', height)
+    },
+    close() {
+      ipc.send('closeGlobalSearch')
+    },
+
     calculateAreaHeight(res) {
       //计算搜索结果区域的高度，传给main变换窗体高度
       let num
-      if(this.openFirst) {
+      if (this.openFirst) {
         num = res
-        if(num >= 6) {
-          ipc.send('changeBrowserWindowHeight', 520)
-        } else if(num === 5) {
-          ipc.send('changeBrowserWindowHeight', 480)
-        } else if(num === 4) {
-          ipc.send('changeBrowserWindowHeight', 430)
-        } else if(num === 3) {
-          ipc.send('changeBrowserWindowHeight', 380)
-        } else if(num === 2) {
-          ipc.send('changeBrowserWindowHeight', 330)
+        if (num >= 6) {
+          this.changeHeight(520)
+        } else if (num === 5) {
+          this.changeHeight(480)
+        } else if (num === 4) {
+          this.changeHeight(430)
+        } else if (num === 3) {
+          this.changeHeight(380)
+        } else if (num === 2) {
+          this.changeHeight(330)
         } else {
-          ipc.send('changeBrowserWindowHeight', 280)
+          this.changeHeight(280)
         }
       } else {
         num = res.length
-        if(num >= 6) {
-          ipc.send('changeBrowserWindowHeight', 400)
-        } else if(num === 5) {
-          ipc.send('changeBrowserWindowHeight', 350)
-        } else if(num === 4) {
-          ipc.send('changeBrowserWindowHeight', 300)
-        } else if(num === 3) {
-          ipc.send('changeBrowserWindowHeight', 250)
-        } else if(num === 2) {
-          ipc.send('changeBrowserWindowHeight', 200)
+        if (num >= 6) {
+          this.changeHeight(400)
+        } else if (num === 5) {
+          this.changeHeight(350)
+        } else if (num === 4) {
+          this.changeHeight(300)
+        } else if (num === 3) {
+          this.changeHeight(250)
+        } else if (num === 2) {
+          this.changeHeight(200)
         } else {
-          ipc.send('changeBrowserWindowHeight', 150)
+          this.changeHeight(150)
         }
       }
     },
@@ -175,8 +194,8 @@ export default {
       })
       return mapHistory
     },
-    executeApp(app){
-      ipc.send('executeApp',{app:app})
+    executeApp(app) {
+      ipc.send('executeApp', {app: JSON.parse(JSON.stringify(app))})
 
       //statsh 全局搜索打开的应用数量
       statsh.do({
@@ -184,17 +203,17 @@ export default {
         key: 'globalSearchBaseOpenApp',
         value: 1
       })
-      ipc.send('closeGlobalSearch')
+      this.close()
     },
     clickRecentHistory(item) {
-      ipc.send('addTab',{url: item.url});
-      ipc.send('closeGlobalSearch')
+      ipc.send('addTab', {url: item.url});
+      this.close()
     },
-    openAppstore(){
-      ipc.send('executeAppByPackage',{package:'com.thisky.appStore'})
+    openAppstore() {
+      ipc.send('executeAppByPackage', {package: 'com.thisky.appStore'})
     },
     click(item) {
-      if(item.tag === 'tab') {
+      if (item.tag === 'tab') {
         ipc.send('switchToTab', {
           taskId: item.taskId,
           tabId: item.tabId
@@ -206,8 +225,8 @@ export default {
           key: 'globalSearchBaseOpenWeb',
           value: 1
         })
-        ipc.send('closeGlobalSearch')
-      } else if(item.tag === 'task') {
+        this.close()
+      } else if (item.tag === 'task') {
         ipc.send('switchToTask', {
           id: item.taskId,
         })
@@ -218,20 +237,20 @@ export default {
           key: 'globalSearchBaseOpenTask',
           value: 1
         })
-        ipc.send('closeGlobalSearch')
-      } else if(item.tag === 'app') {
+        this.close()
+      } else if (item.tag === 'app') {
         this.executeApp(item.app)
-        ipc.send('closeGlobalSearch')
+        this.close()
       }
     },
     calculateHeight() {
       let currentHeight = 0
-      if(this.openFirst) {
-        if(this.recentReadyedIndex > 5) {
+      if (this.openFirst) {
+        if (this.recentReadyedIndex > 5) {
           currentHeight = (this.recentReadyedIndex - 5) * 50
         }
       } else {
-        if(this.itemReadyedIndex > 5) {
+        if (this.itemReadyedIndex > 5) {
           currentHeight = (this.itemReadyedIndex - 5) * 50
         }
       }
@@ -239,7 +258,7 @@ export default {
     },
     bindKeys() {
       window.onkeyup = async (e) => {
-        if(e.keyCode === 13) {
+        if (e.keyCode === 13) {
           let appresult = await this.searchApp(e.target.value)
           this.searchResult = this.searchResult.concat(appresult, this.searchTab(e.target.value), this.searchTask(e.target.value))
           this.itemReadyedIndex = 0
@@ -251,9 +270,9 @@ export default {
         if (e.keyCode === 40) {
           e.preventDefault()
           // 向下键切换li
-          if(this.openFirst) {
+          if (this.openFirst) {
             //如果是在初始页
-            if(this.recentReadyedIndex + 1 < this.recentOpenedHistory.length) {
+            if (this.recentReadyedIndex + 1 < this.recentOpenedHistory.length) {
               this.recentReadyedIndex = this.recentReadyedIndex + 1
               this.recentReadyedItem = this.recentOpenedHistory[this.recentReadyedIndex]
               document.getElementsByClassName('recent-list-hook')[0].scrollTop = this.calculateHeight()
@@ -263,7 +282,7 @@ export default {
               document.getElementsByClassName('recent-list-hook')[0].scrollTop = this.calculateHeight()
             }
           } else {
-            if(this.itemReadyedIndex + 1 < this.compSearchResult.length) {
+            if (this.itemReadyedIndex + 1 < this.compSearchResult.length) {
               this.itemReadyedIndex = this.itemReadyedIndex + 1
               this.itemReadyedItem = this.compSearchResult[this.itemReadyedIndex]
               document.getElementsByClassName('list-hook')[0].scrollTop = this.calculateHeight()
@@ -276,9 +295,9 @@ export default {
         } else if (e.keyCode === 38) {
           e.preventDefault()
           //向上键切换li
-          if(this.openFirst) {
+          if (this.openFirst) {
             //如果是在初始页
-            if(this.recentReadyedIndex + 1 <= this.recentOpenedHistory.length && this.recentReadyedIndex !== 0) {
+            if (this.recentReadyedIndex + 1 <= this.recentOpenedHistory.length && this.recentReadyedIndex !== 0) {
               this.recentReadyedIndex = this.recentReadyedIndex - 1
               this.recentReadyedItem = this.recentOpenedHistory[this.recentReadyedIndex]
               document.getElementsByClassName('recent-list-hook')[0].scrollTop = this.calculateHeight()
@@ -288,7 +307,7 @@ export default {
               document.getElementsByClassName('recent-list-hook')[0].scrollTop = this.calculateHeight()
             }
           } else {
-            if(this.itemReadyedIndex + 1 <= this.compSearchResult.length && this.itemReadyedIndex !== 0) {
+            if (this.itemReadyedIndex + 1 <= this.compSearchResult.length && this.itemReadyedIndex !== 0) {
               this.itemReadyedIndex = this.itemReadyedIndex - 1
               this.itemReadyedItem = this.compSearchResult[this.itemReadyedIndex]
               document.getElementsByClassName('list-hook')[0].scrollTop = this.calculateHeight()
@@ -300,29 +319,29 @@ export default {
           }
         } else if (e.keyCode === 9) {
           //按下tab键切换类型
-          if(this.openFirst) {
-            setTimeout(()=> {
+          if (this.openFirst) {
+            setTimeout(() => {
               document.getElementById("myTextField").focus();
             }, 500)
           } else {
             let index = this.tags.findIndex(v => v.checked == true)
-            if(index < 3) {
+            if (index < 3) {
               this.handleChange(this.tags, index + 1)
             } else {
               this.handleChange(this.tags, 0)
             }
-            setTimeout(()=> {
+            setTimeout(() => {
               document.getElementById("myTextField").focus();
             }, 500)
           }
-        } else if(e.keyCode === 13 && (Object.keys(this.itemReadyedItem).length > 0 || Object.keys(this.recentReadyedItem).length > 0)) {
+        } else if (e.keyCode === 13 && (Object.keys(this.itemReadyedItem).length > 0 || Object.keys(this.recentReadyedItem).length > 0)) {
           //回车按键
-          if(this.openFirst) {
+          if (this.openFirst) {
             this.clikRecentHistory(this.recentReadyedItem)
           } else {
             this.clkli(this.itemReadyedItem)
           }
-        } else if(e.keyCode === 27) {
+        } else if (e.keyCode === 27) {
           //退出键
           this.searchWord.length > 0 ? this.searchWord = '' : ipc.send('closeGlobalSearch')
         }
@@ -342,10 +361,10 @@ export default {
       return await placesModel.getHistoryCount()
     },
     async getAllApps() {
-      this.apps = await appModel.getAllApps({order:'last_execute_time',limit:6})
+      this.apps = await appModel.getAllApps({order: 'last_execute_time', limit: 6})
     },
     async searchApp(word) {
-      let apps = await appModel.find(word,{order:'last_execute_time'})
+      let apps = await appModel.find(word, {order: 'last_execute_time'})
       return apps.map(v => {
         return {
           title: v.name,
@@ -397,74 +416,69 @@ export default {
 
 <template>
   <div id="globalSearch" class="gs-wrap">
-    <div class="gs-dialog" :class="{'first-gs-dialog': openFirst}">
+    <div :class="{'first-gs-dialog': openFirst}" class="gs-dialog">
       <div class="search flex justify-start align-center">
-        <a-icon
-          class="search-lf"
-          type="search"
-          :style="{ fontSize: '26px' }"
-        ></a-icon>
+        <search-outlined :style="{ fontSize: '26px' }"></search-outlined>
         <input
+          id="myTextField"
+          :value="searchWord"
+          class="search-rg"
+          onfocus="this.select()"
           placeholder="搜索应用、网页、标签组"
           spellcheck="false"
-          id="myTextField"
-          onfocus="this.select()"
+          style="font-size: 20px"
           tabindex="-1"
           type="text"
-          class="search-rg"
-          :value="searchWord"
           @input="handleInput($event)"
-          style="font-size: 20px"
         />
         <div>
-          <a-avatar :size="35" @click="openAppstore" class="app-hover"
-                    src="../appStore/favicon.svg"
-                    alt=""
-                    shape="circle"
+          <a-avatar :size="35" alt="" class="app-hover" shape="circle"
+                    src="https://up.apps.vip/logo/favicon.svg"
+                    style="-webkit-app-region:no-drag"
+                    @click="openAppstore"
           ></a-avatar>
         </div>
       </div>
       <div
-        class="apps flex flex-direction justify-between align-start"
         v-if="openFirst"
+        class="apps flex flex-direction justify-between align-start"
       >
         <span class="text-grey">推荐</span>
         <div class="apps-content flex">
           <ul class="flex">
-              <li v-for="(app, index) in apps"
+            <li v-for="(app, index) in apps"
                 :key="index"
                 class="flex flex-direction justify-around align-center"
                 @click="executeApp(app)"
+            >
+              <img
+                :src="app.logo"
+                alt=""
+                onerror="this.src='../../icons/default.svg'"
+                shape="circle"
+              />
+              <span
+                class="text-black sg-omit-sm flex justify-center align-center"
+              >{{ app.name }}</span
               >
-                <img
-                  onerror="this.src='../../icons/default.svg'"
-                  :src="app.logo"
-                  alt=""
-                  :style="{'border-color':app.userThemeColor?app.userThemeColor:app.themeColor}"
-                  shape="circle"
-                />
-                <span
-                  class="text-black sg-omit-sm flex justify-center align-center"
-                >{{app.name}}</span
-                >
-              </li>
+            </li>
           </ul>
         </div>
       </div>
-      <div class="category flex justify-between align-center" v-else>
+      <div v-else class="category flex justify-between align-center">
         <div class="category-lf">
 
-            <a-tag v-for="(tag, index) in tags"
-              :key="index"
-              :color="tag.checked ? 'blue' : ''"
-              @click="handleChange(tag, index)"
-              style="font-size: 15px; border-radius: 10px"
-            >
-              {{tag.label}}
-            </a-tag>
+          <a-tag v-for="(tag, index) in tags"
+                 :key="index"
+                 :color="tag.checked ? 'blue' : ''"
+                 style="font-size: 15px; border-radius: 10px"
+                 @click="handleChange(tag, index)"
+          >
+            {{ tag.label }}
+          </a-tag>
         </div>
         <div class="category-rg flex justify-around align-center">
-          <a-icon type="smile"></a-icon>
+          <smile-outlined/>
           <div
             class="flex align-center justify-center"
             style="padding-left: 5px"
@@ -474,66 +488,56 @@ export default {
         </div>
       </div>
 
-      <div
-        class="recent-open flex flex-direction justify-between"
-        v-if="openFirst"
-      >
-        <span class="text-grey">最近打开</span>
-        <div class="recent-open-content flex flex-direction recent-list-hook">
-          <ul>
-              <li v-for="(item, index) in recentOpenedHistory"
-                :key="index"
-                class="flex justify-start align-center"
-                :class="{'li-ready': recentReadyedIndex === index}"
-                @click="clickRecentHistory(item)"
-              >
-                <img
-                  src="../../icons/default.svg"
-                  style="width: 30px; height: 30px"
-                />
-                <div
-                  class="flex flex-direction justify-around align-start"
-                  style="padding-left: 20px; height: 100%"
-                >
-                  <div class="text-black-lg sg-omit-sm">{{item.title}}</div>
-                  <div class="text-grey-sm sg-omit-sm">{{item.url}}</div>
-                </div>
-              </li>
-          </ul>
+            <div
+              class="recent-open flex flex-direction justify-between"
+              v-if="openFirst"
+            >
+             <div v-if="tip!=='1'" class="gpt-tip-wrapper">
+               <div class="gpt-icon">
+                 <img  :src="gpt">
+               </div>
+               按下ctrl+回车，或者无搜索结果将会询问ChatGPT
+                <span  @click="closeTip" style="float: right" class="close-btn">
+                  <close-outlined />
+                </span>
+
+             </div>
+
+            </div>
+      <div v-else class="content flex justify-center list-hook">
+        <div v-if="contentLoading" key="loading">
+          <a-spin>
+            <a-icon
+              slot="indicator"
+              spin
+              style="font-size: 40px"
+              type="loading"
+            ></a-icon>
+          </a-spin>
         </div>
-      </div>
-      <div class="content flex justify-center list-hook" v-else>
-          <div v-if="contentLoading" key="loading">
-            <a-spin>
-              <a-icon
-                slot="indicator"
-                type="loading"
-                style="font-size: 40px"
-                spin
-              ></a-icon>
-            </a-spin>
-          </div>
         <ul v-else>
 
-            <li v-for="(item, index) in compSearchResult"
-              class="flex justify-start align-center"
-              :class="{'li-ready': itemReadyedIndex === index}"
+          <li v-for="(item, index) in compSearchResult"
               :key="index"
+              :class="{'li-ready': itemReadyedIndex === index}"
+              class="flex justify-start align-center"
               @click="click(item)"
+          >
+            <img
+              :src="item.icon"
+              onerror="this.src='../../icons/default.svg'"
+              style="width: 30px; height: 30px"
+            />
+            <div
+              class="flex flex-direction justify-around align-start"
+              style="padding-left: 20px; height: 100%"
             >
-              <img
-                onerror="this.src='../../icons/default.svg'"
-                :src="item.icon"
-                style="width: 30px; height: 30px"
-              />
-              <div
-                class="flex flex-direction justify-around align-start"
-                style="padding-left: 20px; height: 100%"
-              >
-                <div class="text-black-lg sg-omit-sm" v-html="item.title.replaceAll(searchWord,`<strong style='color:orangered'>${searchWord}</strong>`)"></div>
-                <div class="text-grey-sm sg-omit-sm" v-html="item.attached.replaceAll(searchWord,`<strong style='color:orangered'>${searchWord}</strong>`)"></div>
-              </div>
-            </li>
+              <div class="text-black-lg sg-omit-sm"
+                   v-html="item.title.replaceAll(searchWord,`<strong style='color:orangered'>${searchWord}</strong>`)"></div>
+              <div class="text-grey-sm sg-omit-sm"
+                   v-html="item.attached.replaceAll(searchWord,`<strong style='color:orangered'>${searchWord}</strong>`)"></div>
+            </div>
+          </li>
           <div
             v-show="compSearchResult.length === 0 && !openFirst"
             class="text-grey flex justify-center align-center"
@@ -551,6 +555,6 @@ html, body {
   overflow: hidden !important;
 }
 </style>
-<style scoped lang="scss">
+<style lang="scss" scoped>
 
 </style>
