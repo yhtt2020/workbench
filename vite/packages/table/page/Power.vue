75bec99f1e224f551ae3dd330b91830e6ae00b30
陈一枭 <cyx@thisky.com>
2023-02-23 17:12:22 +0800

    【新增】退出功能

--- vite/packages/table/page/Power.vue
+++ vite/packages/table/page/Power.vue
@@ -1,49 +1,65 @@
 <template>
   <div class="main" style="text-align: center;padding-top: 3em">
     <BackBtn></BackBtn>
-    <div @click="shutdown" class="power-btn">关机</div>
-    <div class="power-btn">注销</div>
+    <div @click="exit" class="power-btn">退出</div>
+    <div style="margin:auto;width: 51em;text-align: center">
+      <a-row>
+        <a-col>
+          <div @click="shutdown" class="power-btn">关机</div>
+          <div class="power-btn">注销</div>
+        </a-col>
+        <a-col>
+          <div @click="reboot" class="power-btn">重启</div>
+          <div class="power-btn">休眠</div>
+        </a-col>
+      </a-row>
+    </div>
+
+
     <br/>
-    <div @click="reboot" class="power-btn">重启</div>
-    <div class="power-btn">休眠</div>
+
   </div>
 
 </template>
 
 <script>
 import BackBtn from '../components/comp/BackBtn.vue'
-import {Modal,message} from 'ant-design-vue'
-const execShell=require('child_process').exec
+import { Modal, message } from 'ant-design-vue'
+
+const execShell = require('child_process').exec
 export default {
   name: 'Power',
   components: { BackBtn },
-  methods:{
-    shutdown(){
+  methods: {
+    exit () {
+      ipc.send('exitTable')
+    },
+    shutdown () {
       Modal.confirm({
-        content:'关闭系统？请保存文件后确认。',
-        onOk(){
-          let shell='shutdown -s -t 00'
-          if(require('os').platform()!='win32'){
-            shell='shutdown now'
+        content: '关闭系统？请保存文件后确认。',
+        onOk () {
+          let shell = 'shutdown -s -t 00'
+          if (require('os').platform() != 'win32') {
+            shell = 'shutdown now'
           }
-          let command=execShell(shell, (err,stdout,stderr)=>{
-            if(err||stderr){
+          let command = execShell(shell, (err, stdout, stderr) => {
+            if (err || stderr) {
               message.error('关机失败，请确认软件权限。')
             }
           })
         }
       })
     },
-    reboot(){
+    reboot () {
       Modal.confirm({
-        content:'重启系统？请保存文件后确认。',
-        onOk(){
-          let shell='shutdown -r -t 0'
-          if(require('os').platform()!='win32'){
-            shell='shutdown -r now'
+        content: '重启系统？请保存文件后确认。',
+        onOk () {
+          let shell = 'shutdown -r -t 0'
+          if (require('os').platform() != 'win32') {
+            shell = 'shutdown -r now'
           }
-          let command=execShell(shell, (err,stdout,stderr)=>{
-            if(err||stderr){
+          let command = execShell(shell, (err, stdout, stderr) => {
+            if (err || stderr) {
               message.error('重启失败，请确认软件权限。')
             }
           })
@@ -54,17 +70,22 @@ export default {
 }
 </script>
 
-<style scoped>
-.power-btn{
+<style scoped lang="scss">
+.power-btn {
   display: inline-block;
   border-radius: 100%;
   color: white;
   width: 3.2em;
   height: 3.2em;
-  line-height: 3.2em;
+  line-height: 3em;
   text-align: center;
-  font-size: 3.5em;
+  font-size: 3em;
   background: rgba(0, 0, 0, 0.24);
-  margin:1em
+  margin: 0.5em;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.7;
+  }
 }
 </style>
