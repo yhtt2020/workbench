8873e2a0f7ee5a7fdbd84bdc769c0d81a35820b5
陈一枭 <cyx@thisky.com>
2023-03-02 22:11:15 +0800

    【新增】社区部分增强

---
+++ vite/packages/table/components/comp/ComActionPanel.vue
@@ -0,0 +1,61 @@
+<template>
+  <h3 style="margin-bottom: 1em">我的社区</h3>
+  <div>
+    <a-row :gutter="[20,20]">
+      <a-col>
+        <a-button @click="go('https://s.apps.vip/user/info')">
+          <idcard-filled /> 修改资料
+        </a-button>
+      </a-col>
+      <a-col>
+        <a-button @click="go('https://s.apps.vip/user/points')">
+          <gold-filled /> 积分明细
+        </a-button>
+      </a-col>
+      <a-col>
+        <a-button @click="go('https://s.apps.vip/user/message')">
+          <bell-filled /> 社区消息
+        </a-button>
+      </a-col>
+      <a-col>
+        <a-button @click="go('https://s.apps.vip/user/account-info')">
+          <api-filled /> 账号绑定
+        </a-button>
+      </a-col>
+      <a-col>
+        <a-button @click="go('https://s.apps.vip/user/passwd')">
+          <lock-filled /> 修改密码
+        </a-button>
+      </a-col>
+      <a-col>
+        <a-button @click="go('https://s.apps.vip/task')">
+          <schedule-filled />社区任务
+        </a-button>
+      </a-col>
+    </a-row>
+
+
+  </div>
+</template>
+
+<script>
+import {IdcardFilled,GoldFilled,BellFilled,ApiFilled,LockFilled,ScheduleFilled} from '@ant-design/icons-vue'
+export default {
+  name: 'ComActionPanel',
+  components:{
+    IdcardFilled,GoldFilled,BellFilled,ApiFilled,LockFilled,ScheduleFilled
+  },
+  methods:{
+    edit(){
+      ipc.send('addTab',{url:"https://s.apps.vip/user/info"})
+    },
+    go(url){
+      ipc.send('addTab',{url:url})
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vite/packages/table/components/comp/ComPanel.vue
@@ -0,0 +1,73 @@
+<template>
+<h3 style="margin-bottom: 1em"><a-avatar src="https://up.apps.vip/logo/yuan.png"></a-avatar> 元社区
+
+  <div  style="float: right">
+    <a-button @click="go('https://s.apps.vip')">
+      <Icon icon="yichu"> </Icon> 前往元社区
+    </a-button>
+  </div></h3>
+  <div>
+  <a-row :gutter="[0,10]" >
+    <a-col :span="24">
+      UID：{{userInfo.uid}} （用于社区活动身份）
+    </a-col>
+    <a-col :span="24">
+      社区等级：{{userInfo.grade.new_name}}
+    </a-col>
+    <a-col :span="24" style="text-align: center">
+      <a-row>
+        <a-col :span="8">
+          <div class="num">{{userInfo.post_count}}</div>
+          <div>内容</div>
+        </a-col>
+        <a-col :span="8">
+          <div class="num">{{userInfo.fans}}</div><div>粉丝</div>
+        </a-col>
+        <a-col :span="8">
+          <div class="num">{{userInfo.follow}}</div><div>关注</div>
+        </a-col>
+      </a-row>
+    </a-col>
+    <a-col :span="24">
+      <edit-outlined style="margin-right: 0.5em" /> {{userInfo.signature}}
+    </a-col>
+  </a-row>
+  </div>
+</template>
+
+<script>
+import { appStore } from '../../store'
+import {mapState} from 'pinia'
+import {SendOutlined,UserAddOutlined,EyeOutlined,EditOutlined} from '@ant-design/icons-vue '
+
+export default {
+  name: 'ComPanel',
+  components:{
+    SendOutlined,UserAddOutlined,EyeOutlined,EditOutlined
+  },
+  data(){
+    return {
+
+    }
+  },
+  computed:{
+    ...mapState(appStore,['userInfo'])
+  },
+  mounted () {
+    console.log(this.userInfo)
+
+  },
+  methods:{
+    go(url){
+      ipc.send('addTab',{url:url})
+    }
+  }
+}
+</script>
+
+<style scoped>
+.num{
+  font-size: 1.2em;
+  font-weight: bold;
+}
+</style>
