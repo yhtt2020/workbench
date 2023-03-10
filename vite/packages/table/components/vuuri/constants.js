986b253ad61faa058e07eb6031c2aef988b9ce1a
陈一枭 <cyx@thisky.com>
2023-03-10 09:38:07 +0800

    【新增】vuuri组件

---
+++ vite/packages/table/components/vuuri/constants.js
@@ -0,0 +1,13 @@
+
+export const ItemKey = {
+  key: '_$muuri_id'
+}
+
+export const ItemSize = {
+  width: '100px',
+  height: '100px'
+}
+
+export const ItemDragHandle = {
+  selector: '.muuri-item:first-child'
+}
