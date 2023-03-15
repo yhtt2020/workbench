29aa0958a3f0b807238b919499892f9fcdb8c1c3
hjm <hjm@163.com>
2023-03-15 17:15:24 +0800

    【新增】新增壁纸页面的倒计时组件，优化代码

--- vite/packages/table/store.ts
+++ vite/packages/table/store.ts
@@ -190,6 +190,15 @@ export const tableStore = defineStore(
 
       },
     },
+    persist: {
+      enabled: true,
+      strategies: [{
+        // 自定义存储的 key，默认是 store.$id
+        // 可以指定任何 extends Storage 的实例，默认是 sessionStorage
+        storage: localStorage,
+        // state 中的字段名，按组打包储存
+      }]
+    }
   },
   {}
 );
@@ -260,6 +269,7 @@ export const countDownStore = defineStore(
       }
     },
   },
+
   {}
 );
 
