0538f8106e694ee8d7515cc93042791f3e33a1d7
陈一枭 <cyx@thisky.com>
2023-03-30 17:52:08 +0800

    【新增】新增激活码前端页面

--- src/preload/tablePreload.js
+++ src/preload/tablePreload.js
@@ -1,30 +1,42 @@
 const messageModel = require('../model/messageModel')
 const api = require('../browserApi/baseApi')
-window.tsbApi=require('../browserApi/baseApi')
-window.tableApi=require('../tableApi/baseApi')
-const groupApi=require('../api/groupApi')
-let ipc=require('electron').ipcRenderer
-ipc.on('updateMusicStatus',(e,a)=>{
-  if(window.updateMusicStatusHandler){
+window.tsbApi = require('../browserApi/baseApi')
+window.tableApi = require('../tableApi/baseApi')
+const groupApi = require('../api/groupApi')
+let ipc = require('electron').ipcRenderer
+ipc.on('updateMusicStatus', (e, a) => {
+  if (window.updateMusicStatusHandler) {
     window.updateMusicStatusHandler(a)
   }
 })
-window.loudness=require('loudness')
-window.iconv=require('iconv-lite')
-window.brightness=require('brightness')
-window.readAida64=require('aida64-to-json')
-window.fs=require('fs-extra')
+window.loudness = require('loudness')
+window.iconv = require('iconv-lite')
+window.brightness = require('brightness')
+window.readAida64 = require('aida64-to-json')
+window.fs = require('fs-extra')
 window.$models = {
-  appModel:require('../model/appModel'),
+  appModel: require('../model/appModel'),
   messageModel
 }
 window.$models.appModel.initDb()
-window.$apis={
+window.$apis = {
   groupApi
 }
-tsbApi.barrage.setOnUrlChanged((a)=>{
-  if(tsbApi.barrage.urlChangedCallback){
+tsbApi.barrage.setOnUrlChanged((a) => {
+  if (tsbApi.barrage.urlChangedCallback) {
     tsbApi.barrage.urlChangedCallback(a.url)
     tsbApi.barrage.reload()
   }
 })
+let si = require('systeminformation')
+
+window.getSerialNum=async () => {
+  let staticData = await si.getStaticData()
+  let serial = {
+    baseboardSerial: staticData.baseboard.serial,
+  }
+  let crypto=require('crypto')
+  let serialHash=crypto.createHash('sha256').update(JSON.stringify(serial)).digest('hex')
+  return serialHash
+}
+
