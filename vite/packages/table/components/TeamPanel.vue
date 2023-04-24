7884c3f25a69eb8035da9889386e1ba745f55bc1
陈一枭 <cyx@thisky.com>
2023-04-24 22:18:03 +0800

    【调整】小队功能

--- vite/packages/table/components/TeamPanel.vue
+++ vite/packages/table/components/TeamPanel.vue
@@ -1,52 +1,74 @@
 <template>
-
-
-    <div class="common-panel s-bg " style="width: 100px">
-      <div @click="cleanTeam" class="p-2 pt-2 p-3 truncate font-large text-center" style="font-size: 1.1em">#{{ team.name }}</div>
-      <div class="text-center" v-for="user in userList">
-        <a-avatar :size="50" :src="user.avatar"></a-avatar>
-        <div class="p-2">{{ user.nickname }}</div>
+  <div class="flex s-bg rounded-xl" :style="{height:showDetail?'100%':'auto'}" style="overflow: hidden">
+    <transition name="fade">
+      <div v-if="showDetail" style="width:250px;height: 100%;background: rgba(0,0,0,0.09)">
+        <div @click="toggleDetail" class="p-2 rounded-md inline-block m-2 pointer" style="background: rgba(0,0,0,0.17);position:absolute;"><Icon icon="guanbi" style="font-size: 1.2em"></Icon></div>
+        <div class="line mt-5 text-center">
+          <a-avatar :size="50" shape="square" :src="team.avatar"></a-avatar>
+          <div class="mt-3">{{ team.name }}</div>
+        </div>
       </div>
-      <div class="text-center pb-2">
+    </transition>
+    <div class="common-panel  " style="width: 80px">
+      <div v-if="!showDetail"  @click="toggleDetail" class="p-2 pt-2 p-3 truncate font-large text-center pointer" style="font-size: 1.1em">
+        <div class="mb-2">
+          <a-avatar :size="50" shape="square" :src="team.avatar"></a-avatar>
+        </div>
+<!--        <div  class="truncate">-->
+<!--          {{ team.name }}-->
+<!--        </div>-->
+      </div>
+      <div v-if="showDetail" class="text-center">小队成员</div>
+      <a-divider  style="margin-top: 10px;margin-bottom: 10px"></a-divider>
+      <div class="text-center mb-5 mt-5" v-if="teamLeader.userInfo">
+        <a-avatar :size="50" :src="teamLeader.userInfo.avatar"></a-avatar>
+        <div v-if="showDetail" class="p-2 truncate">{{ teamLeader.userInfo.nickname }}</div>
+      </div>
+      <div class="text-center  mb-5 " v-for="user in teamMembers">
+        <a-avatar :size="50" :src="user.userInfo.avatar"></a-avatar>
+      <div v-if="showDetail" class="p-2 truncate">{{ user.userInfo.nickname }}</div>
+      </div>
+      <div class="text-center pb-2" v-if="team.leader===userInfo.uid">
         <a-avatar :size="50">
           <PlusOutlined/>
         </a-avatar>
       </div>
-
     </div>
+  </div>
+
 </template>
 
 <script>
-import {PlusOutlined} from '@ant-design/icons-vue'
+import { PlusOutlined } from '@ant-design/icons-vue'
 import { teamStore } from '../store/team'
-import {mapWritableState} from 'pinia'
-const userList = [
-  {
-    nickname: '张三',
-    avatar: 'https://tse1-mm.cn.bing.net/th/id/OIP-C.ml9T1565kAEF5L8SEzpEYwAAAA?w=218&h=218&c=7&r=0&o=5&dpr=1.1&pid=1.7'
-  },
-  {
-    nickname: '李四',
-    avatar: 'https://tse3-mm.cn.bing.net/th/id/OIP-C.o-n8oM8_wYLsQVmnly6fdQAAAA?w=218&h=218&c=7&r=0&o=5&dpr=1.1&pid=1.7'
-  }
-]
+import { mapWritableState, mapActions, mapState } from 'pinia'
+import { appStore } from '../store'
+
 export default {
-  name: "TeamPanel",
-  components: {PlusOutlined},
-  computed:{
-    ...mapWritableState(teamStore,['team','teamVisible']),
+  name: 'TeamPanel',
+  components: { PlusOutlined },
+  computed: {
+    ...mapWritableState(teamStore, ['team', 'teamVisible', 'teamLeader', 'teamMembers']),
+    ...mapState(appStore, ['userInfo'])
 
   },
-  data() {
+  data () {
     return {
-      userList
+      showDetail: false
     }
   },
-  methods:{
-    cleanTeam(){
-      this.teamVisible=false
-      this.team={
-        status:false
+  mounted () {
+    this.updateTeamShip(this.team.no).then()
+  },
+  methods: {
+    ...mapActions(teamStore, ['updateTeamShip']),
+    toggleDetail () {
+      this.showDetail = !this.showDetail
+    },
+    cleanTeam () {
+      this.teamVisible = false
+      this.team = {
+        status: false
       }
     }
   }
--- vite/packages/table/store/team.ts
+++ vite/packages/table/store/team.ts
@@ -5,23 +5,79 @@ import dbStorage from "./dbStorage";
 const {axios} = window.$models
 const createUrl = Server.baseUrl + '/app/team/create'
 const joinByNoUrl = Server.baseUrl + '/app/team/joinByNo'
+const getTeamLeaderUrl = Server.baseUrl + '/app/team/getLeader'
+const getTeamMembersUrl = Server.baseUrl + '/app/team/getMembers'
+
+// @ts-ignore
 export const teamStore = defineStore("teamStore", {
   state: () => ({
-    teamVisible:false,//小组可见
+    teamVisible: false,//小组可见
     //小组
     team: {
       status: false
     },
+    teamLeader: {
+      userInfo:{}
+    },//当前小队队长
+    teamMembers: {},//当前小队成员
   }),
   actions: {
+    async getTeamLeader(no, cache = 1, userCache = 1) {
+      let teamLeaderResult = await axios.post(getTeamLeaderUrl, {no: no, cache: cache, userCache: userCache},
+        await getConfig())
+      if (teamLeaderResult.code === 1000) {
+        let data = teamLeaderResult.data
+        if (data.status) {
+          this.teamLeader = data.data
+          return this.teamLeader
+        } else {
+          console.warn('无法取到队长信息')
+        }
+      }
+    },
+    async getTeamMembers(no, cache = 1, userCache = 1) {
+
+      let teamMembersResult = await axios.post(getTeamMembersUrl, {no: no, cache: cache, userCache: userCache},
+        await getConfig())
+      if (teamMembersResult.code === 1000) {
+        let data = teamMembersResult.data
+        if (data.status) {
+          this.teamMembers = data.data
+          return this.teamMembers
+        } else {
+          console.warn('无法取到队员信息')
+        }
+      }
+    },
+
+    /**
+     * 更新全部的队伍信息
+     */
+    async updateTeamShip(no) {
+      await this.getTeamLeader(no)
+      await this.getTeamMembers(no)
+    },
+
+    async joinByNo(no) {
+      try {
+        let rs = await axios.post(joinByNoUrl, {no: no},
+          await getConfig())
+        return rs
+      } catch (e) {
+        return {
+          status: 0,
+          info: e.message
+        }
+        console.warn(e)
+      }
+
+    },
     async create(name, no, avatar) {
-      console.log(createUrl)
-      console.log(await getConfig())
       let rs = await axios.post(createUrl, {
-        name,
-        no,
-        avatar
-      },
+          name,
+          no,
+          avatar
+        },
         await getConfig())
       console.log(rs)
       return rs
@@ -31,7 +87,7 @@ export const teamStore = defineStore("teamStore", {
     enabled: true,
     strategies: [
       {
-        paths:['team','teamVisible'],
+        paths: ['team', 'teamVisible'],
         // 自定义存储的 key，默认是 store.$id
         // 可以指定任何 extends Storage 的实例，默认是 sessionStorage
         storage: dbStorage,
