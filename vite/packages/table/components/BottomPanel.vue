06306c85dd61b14f026f4bd6afb36974c22b15d6
陈一枭 <cyx@thisky.com>
2023-05-10 22:50:17 +0800

    【调整】去除自动添加小鸟的操作 【调整】添加自定义卡片文案去除开发中

--- vite/packages/table/components/BottomPanel.vue
+++ vite/packages/table/components/BottomPanel.vue
@@ -297,25 +297,25 @@ export default {
     window.removeEventListener('resize', that.checkScroll)
   },
   mounted () {
-    let inserted=localStorage.getItem('insertBird')
-    if(!inserted){
-      this.navigationList.unshift( {
-        icon: 'http://a.apps.vip/icons/flappy.jpg',
-        type:'coolApp',
-        name: 'Mlappy Bird',
-        summary: '和小伙伴们一起飞。',
-        needInstall: false,
-        data: {
-          theme: '#030c13',
-          name: 'mlappyBird',
-          url: 'http://bird.apps.vip/?',
-          background: false,
-          type:'game',
-          scale:160
-        }
-      })
-      localStorage.setItem('insertBird','1')
-    }
+    // let inserted=localStorage.getItem('insertBird')
+    // if(!inserted){
+    //   this.navigationList.unshift( {
+    //     icon: 'http://a.apps.vip/icons/flappy.jpg',
+    //     type:'coolApp',
+    //     name: 'Mlappy Bird',
+    //     summary: '和小伙伴们一起飞。',
+    //     needInstall: false,
+    //     data: {
+    //       theme: '#030c13',
+    //       name: 'mlappyBird',
+    //       url: 'http://bird.apps.vip/?',
+    //       background: false,
+    //       type:'game',
+    //       scale:160
+    //     }
+    //   })
+    //   localStorage.setItem('insertBird','1')
+    // }
     let that = this
     this.checkScroll()
     // const that = this
