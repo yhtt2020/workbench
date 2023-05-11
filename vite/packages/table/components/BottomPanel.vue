ab10c38e387a9c10cf780aba979a0c47a3b42dec
陈一枭 <cyx@thisky.com>
2023-05-11 22:03:30 +0800

    【新增】新增关注消息接口

--- vite/packages/table/components/BottomPanel.vue
+++ vite/packages/table/components/BottomPanel.vue
@@ -17,7 +17,9 @@
                  style="padding: 0.6em;position:relative;">
             <a-row style="text-align: left" :gutter="10">
               <a-col>
+                <a-badge  style="border:none;" :count="totalCount">
                 <a-avatar :src="userInfo.avatar" :size="50">{{ userInfo.nickname }}</a-avatar>
+                </a-badge>
               </a-col>
               <a-col v-if="!simple" @click.stop="goMy()" style="position: relative">
                 <span ref="minute" class="tip">+1</span>
@@ -250,6 +252,7 @@ import ScrolX from './ScrolX.vue'
 
 import TeamTip from './TeamTip.vue'
 import { teamStore } from '../store/team'
+import { messageStore } from '../store/message'
 
 export default {
   name: 'BottomPanel',
@@ -316,6 +319,7 @@ export default {
     //   })
     //   localStorage.setItem('insertBird','1')
     // }
+    this.getMessageIndex().then()
     let that = this
     this.checkScroll()
     // const that = this
@@ -343,7 +347,8 @@ export default {
   computed: {
     ...mapWritableState(appStore, ['userInfo', 'settings', 'lvInfo', 'simple']),
     ...mapWritableState(teamStore, ['team', 'teamVisible']),
-    ...mapWritableState(cardStore, ['navigationList', 'routeParams'])
+    ...mapWritableState(cardStore, ['navigationList', 'routeParams']),
+    ...mapWritableState(messageStore,['messageIndex','totalCount'])
   },
   watch: {
     navigationList: {
@@ -365,6 +370,7 @@ export default {
   },
   methods: {
     ...mapActions(teamStore, ['updateMy']),
+    ...mapActions(messageStore,['getMessageIndex']),
     async toggleTeam () {
       await this.updateMy(0)
       if (this.team.status === false) {
@@ -491,7 +497,11 @@ export default {
       }
     },
     social () {
-      this.$router.push({ path: '/social/' })
+      if(this.totalCount){
+        this.$router.push({ name: 'message' })
+      }else{
+        this.$router.push({ path: '/social/' })
+      }
     },
     power () {
       this.$router.push({ path: '/power' })
--- vite/packages/table/page/social/Message.vue
+++ vite/packages/table/page/social/Message.vue
@@ -9,7 +9,7 @@
            <span style="font-size: 16px; color: rgba(255,255,255,0.60);font-weight: 400;">全部已读</span>
        </div>
        <!-- 消息分类的tab -->
-       <HorizontalPanel :navList="category" v-model:selectType="categoryType" bgColor="drawer-item-select-bg" :height="44"></HorizontalPanel>
+       <HorizontalPanel @changed="navChanged" :navList="category" v-model:selectType="categoryType" bgColor="drawer-item-select-bg" :height="44"></HorizontalPanel>
        <template v-if="categoryType.name === 'interact'">
         <div style="height: 26.99em;">
          <vueCustomScrollbar :settings="scrollbarSettings" class="pt-4" style="height: 100%">
@@ -20,7 +20,7 @@
              <div>
                <a-avatar :size="40" :src="`../../../../public/img/${item.url}`" class="avatar-list"></a-avatar>
              </div>
-             <div class="flex flex-col ml-4"> 
+             <div class="flex flex-col ml-4">
                <div class="flex">
                  <span class="pr-1 truncate" style="max-width: 84px; font-size: 16px;color: rgba(255,255,255,0.85);font-weight: 600;">
                    {{item.name}}
@@ -42,21 +42,26 @@
            <!-- 数据空状态 -->
            <a-list :data-source="[]" v-if="concernList.length === 0" />
 
-           <div v-else v-for="item in  concernList" @click="concernToggle()" class="px-4 rounded-lg interact-hover pointer py-4 mb-3 flex items-center">
+           <div v-else v-for="item in  newFollower" @click="concernToggle(item)" class="px-4 rounded-lg interact-hover pointer py-4 mb-3 flex items-center">
              <div>
-               <a-avatar :size="40" :src="`../../../../public/img/${item.url}`" class="avatar-list"></a-avatar>
+               <a-avatar :size="40" :src="item.user.avatar_64" class="avatar-list"></a-avatar>
              </div>
-             <div class="flex flex-col ml-4"> 
+             <div class="flex flex-col ml-4">
                <div class="flex">
                  <span class="pr-1 truncate" style="max-width: 84px; font-size: 16px;color: rgba(255,255,255,0.85);font-weight: 600;">
-                   {{item.name}}
+                   {{item.user.nickname}}
                  </span>
                  <span class="pr-1 truncate" style="font-size: 16px;color: rgba(255,255,255,0.60);font-weight: 400;">
-                   {{item.dynamic}}
+                   关注了你
                  </span>
-                 <span class="truncate w-52" style=" font-size: 16px;color: rgba(255,255,255,0.85);font-weight: 600;">{{item.content}}</span>
+                 <span class="truncate w-52" style=" font-size: 16px;color: rgba(255,255,255,0.85);font-weight: 600;"></span>
                </div>
-               <span style="font-size: 14px; color: rgba(255,255,255,0.40); font-weight: 400;">{{ item.createTime }}</span>
+               <span style="font-size: 14px; color: rgba(255,255,255,0.40); font-weight: 400;">{{ item.user.add_time }}</span>
+<!--               url:'002.png',
+          name:'彩虹小白马 ',
+          createTime:'1小时前',
+          content:'',
+          dynamic:'关注了你'  -->
              </div>
            </div>
          </vueCustomScrollbar>
@@ -69,12 +74,12 @@
            <a-list :data-source="[]" v-if="systemList.length === 0" />
            <div v-else v-for="item in systemList" @click="systemItemClick()" class="px-4 pointer interact-hover rounded-lg py-4 mb-3 flex items-center">
              <div class="w-10 h-10 flex items-center  justify-center s-bg rounded-full">
-               <Icon icon="bell" style="font-size: 1.429em;"></Icon> 
+               <Icon icon="bell" style="font-size: 1.429em;"></Icon>
              </div>
              <div class="flex flex-col ml-4">
                 <span style="font-size: 16px;color: rgba(255,255,255,0.85); font-weight: 600;">
                  {{item.notice}}
-                </span>  
+                </span>
                 <span style="font-size: 16px;color: rgba(255,255,255,0.85); font-weight: 600;">
                  {{item.content}}
                 </span>
@@ -99,7 +104,7 @@
                {{item.description}}
              </span>
              <div class="rounded-lg mt-3 mb-3" style="height: 240px;">
-                <img :src="`../../../../public/img/${item.url}`" class="rounded-lg" style="width:100%;height: 100%; object-fit: cover;" alt=""> 
+                <img :src="`../../../../public/img/${item.url}`" class="rounded-lg" style="width:100%;height: 100%; object-fit: cover;" alt="">
              </div>
              <div class="flex justify-between">
                <span>{{item.createTime}}</span>
@@ -116,12 +121,12 @@
             <a-list :data-source="[]" v-if="customList.length === 0" />
             <div v-else v-for="item in customList" @click="customListClick()" class="px-4 pointer interact-hover rounded-lg py-4 mb-3 flex items-center">
               <div class="w-10 h-10 flex items-center  justify-center s-bg rounded-full">
-                <Icon icon="bell" style="font-size: 1.429em;"></Icon> 
+                <Icon icon="bell" style="font-size: 1.429em;"></Icon>
               </div>
               <div class="flex flex-col ml-4">
                  <span style="font-size: 16px;color: rgba(255,255,255,0.85); font-weight: 600;">
                   {{item.notice}}
-                 </span>  
+                 </span>
                  <span style="font-size: 16px;color: rgba(255,255,255,0.85); font-weight: 600;">
                   {{item.content}}
                  </span>
@@ -139,6 +144,8 @@
 
 <script>
 import HorizontalPanel from '../../components/HorizontalPanel.vue';
+import { messageStore } from '../../store/message'
+import {mapActions,mapState} from 'pinia'
 export default {
   name:'Message',
   components:{
@@ -211,7 +218,7 @@ export default {
           content:'分享几个自己整理的快捷指令',
           dynamic:'点赞了你发布的帖子'
         },
-      ],   
+      ],
       // 消息关注数据列表 后期对接后台数据
       concernList:[
         {
@@ -299,15 +306,32 @@ export default {
       ]
     }
   },
+  computed:{
+    ...mapState(messageStore,['newFollower'])
+  },
   methods:{
+    ...mapActions(messageStore,['getNewFollower']),
+    navChanged(){
+      console.log(this.categoryType)
+      switch (this.categoryType.name){
+        case 'attention':
+          this.getNewFollower().then()
+          break
+      }
+    },
     // 互动消息列表点击事件
     optionToggle(){},
     // 关注消息列表点击事件
-    concernToggle(){},
+    concernToggle(item){
+      ipc.send('addTab',{url:'https://s.apps.vip/user?uid='+item.user.uid})
+    },
     // 系统消息列表点击事件
     systemItemClick(){},
     // 推送消息列表item点击事件
-    clickPushDetail(){},
+    clickPushDetail(){
+      //this.getNewFollower().then()
+
+    },
     // 推送消息列表查看详情点击事件
     clickDetail(){},
     // 自定义消息列表点击事件
@@ -325,11 +349,11 @@ export default {
 }
 .interact-hover{
   &:hover{
-    background: rgba(0,0,0,0.30);   
+    background: rgba(0,0,0,0.30);
   }
   &:active{
     filter: brightness(0.8);
-    background:rgba(42, 42, 42, 0.25); 
+    background:rgba(42, 42, 42, 0.25);
   }
 }
 </style>
