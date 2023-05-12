5dbabf7ff83e668aadf2cfb93a06a30ff687ec6d
wjl <3123911548@qq.com>
2023-05-12 17:37:30 +0800

    【优化】游戏助手折扣详情的优化和页面按钮的优化

--- vite/packages/table/page/gameAssistant/GameDiscountDetail.vue
+++ vite/packages/table/page/gameAssistant/GameDiscountDetail.vue
@@ -5,67 +5,74 @@
         <div class="w-12 h-12 flex items-center cursor-pointer btn-active justify-center rounded-lg s-bg"  @click="goBack">
           <Icon icon="xiangzuo" style="font-size:1.5em;"></Icon>
         </div>
-        <span style="font-size:20px;font-weight: 500;" class="ml-4">{{ detailData.name }}</span>
+        <span style="font-size:20px;font-weight: 500;" id="detail-name" class="ml-4 truncate">{{ detailData.name }}</span>
       </div>
     </div>
     <div class="flex-grow-0">
       <div class="right-flex flex items-center justify-center mr-5">
-        <span class="rounded-lg btn-active s-bg cursor-pointer mr-3 " @click="enterSteamStore"  style="padding: 8px 41px 10px 41px;">Steam商店</span>
-        <a-select style="border: 1px solid rgba(255, 255, 255, 0.1);" @change="selectOptionValue($event)"  
-         class="w-56 h-auto rounded-xl  text-xs s-item" size="large"
-         :bordered="false" v-model:value="defaultDetailRegion">
-         <a-select-option v-for="item in rightSelect" :value="item.id">{{item.name}}</a-select-option>
-        </a-select>
+        <span class="rounded-lg px-10 py-2 btn-active s-bg cursor-pointer mr-3 min-store" @click="enterSteamStore">前往购买</span>
+        <span class="px-10 py-2 s-bg btn-active rounded-lg pointer" @click="openDetailDrawer">地区:{{defaultDetailRegion.name}}</span>
       </div>
     </div>
   </div>
   <vue-custom-scrollbar :settings="settingsScroller" style="height: calc(100vh - 15.8em)" class="mt-3 mr-3">
-    <a-row :wrap="true">
-      <a-col :span="24" :lg="16">
-        <WheelCastingUnit :screenshots="detailScreenshots" :movies="detailMovies" class="wheel-content"></WheelCastingUnit>
-      </a-col>
-      <a-col :span="24" :lg="8" >
-        <div class="flex flex-col " id="detail-ms">
-          <span style="font-size: 16px; font-weight: 400;color: rgba(255, 255, 255, 0.85);">{{ detailData.description }}</span>
-          <div class="flex justify-between items-center mt-4" >
-            <span style="font-size: 16px; font-weight: 400; color: rgba(255, 255, 255, 0.6);">发行日期</span>
-            <span style="font-size: 16px; font-weight: 400; color: rgba(255, 255, 255, 0.85);">{{ detailData.releaseDate }}</span>
+    <div class="detail-container">
+      <WheelCastingUnit :screenshots="detailScreenshots" :movies="detailMovies"></WheelCastingUnit>
+      <div class="flex flex-col min-width ml-4 rounded-lg px-4 py-4 s-bg">
+        <span style="font-size: 16px; font-weight: 400;color: rgba(255, 255, 255, 0.85);">{{ detailData.description }}</span>
+        <div class="flex justify-between items-center mt-4" >
+          <span style="font-size: 16px; font-weight: 400; color: rgba(255, 255, 255, 0.6);">发行日期</span>
+          <span style="font-size: 16px; font-weight: 400; color: rgba(255, 255, 255, 0.85);">{{ detailData.releaseDate }}</span>
+        </div>
+        <div class="flex justify-between items-center mt-4" >
+          <span style="font-size: 16px; font-weight: 400; color: rgba(255, 255, 255, 0.6);">发行商</span>
+          <span style="font-size: 16px; font-weight: 400; color: rgba(255, 255, 255, 0.85);">{{detailData.publishers}}</span>
+        </div>
+        <div class="flex justify-between items-center mt-4" >
+          <span style="font-size: 16px; font-weight: 400; color: rgba(255, 255, 255, 0.6);">开发商</span>
+          <span style="font-size: 16px; font-weight: 400; color: rgba(255, 255, 255, 0.85);">{{detailData.developers}}</span>
+        </div>
+        <span class="w-1/3 mt-1" style="font-size: 16px; font-weight: 400; color: rgba(255, 255, 255, 0.6);">标签</span>
+        <div class="flex items-center mt-1">
+          <span v-for="item in detailData.genres"  class="rounded-lg mr-1" style="background: rgba(255, 255, 255, 0.4);padding: 2px 6px 1px 6px;">{{item.description}}</span>
+        </div>
+        <span class="mt-4 mb-3" style="font-size: 16px; font-weight: 400; color: rgba(255, 255, 255, 0.6);">语言</span>
+        <span style="font-size: 16px; font-weight: 400; color: rgba(255, 255, 255, 0.6);">
+          {{ detailData.language }}
+        </span>
+        <div  style="padding: 12px 13px;" class="s-bg flex rounded-lg items-center justify-between mt-6">
+          <div class="flex flex-col mt-1">
+           <span style="font-size: 16px; font-weight: 400; color: rgba(255, 255, 255, 0.6);">价格</span>
+           <span style="font-size: 14px; font-weight: 400; color: rgba(255, 255, 255, 0.6);">特价促销！{{ acquisitionDate(exTime) }} 截止</span>
           </div>
-          <div class="flex justify-between items-center mt-4" >
-            <span style="font-size: 16px; font-weight: 400; color: rgba(255, 255, 255, 0.6);">发行商</span>
-            <span style="font-size: 16px; font-weight: 400; color: rgba(255, 255, 255, 0.85);">{{detailData.publishers}}</span>
-          </div>
-          <div class="flex justify-between items-center mt-4" >
-            <span style="font-size: 16px; font-weight: 400; color: rgba(255, 255, 255, 0.6);">开发商</span>
-            <span style="font-size: 16px; font-weight: 400; color: rgba(255, 255, 255, 0.85);">{{detailData.developers}}</span>
-          </div>
-          <div class="flex  items-center justify-between mt-4" >
-            <span class="w-1/3" style="font-size: 16px; font-weight: 400; color: rgba(255, 255, 255, 0.6);">标签</span>
-            <div class="flex items-center justify-center">
-               <span v-for="item in detailData.genres"  class="rounded-lg mr-1" style="background: rgba(255, 255, 255, 0.4);padding: 2px 6px 1px 6px;">{{item.description}}</span>
-            </div>
-          </div>
-          <span class="mt-4 mb-3" style="font-size: 16px; font-weight: 400; color: rgba(255, 255, 255, 0.6);">语言</span>
-          <span style="font-size: 16px; font-weight: 400; color: rgba(255, 255, 255, 0.6);">
-            {{ detailData.language }}
-          </span>
-          <div  style="padding: 12px 13px;" class="s-bg flex mb-4 rounded-lg items-center justify-between mt-6">
-            <div class="flex flex-col mt-1">
-             <span style="font-size: 16px; font-weight: 400; color: rgba(255, 255, 255, 0.6);">价格</span>
-             <span style="font-size: 14px; font-weight: 400; color: rgba(255, 255, 255, 0.6);">特价促销！{{ acquisitionDate(exTime) }} 截止</span>
-            </div>
-            <div class="flex flex-col">
-               <span class="line-through mr-2" style="text-align: right; color:rgba(255, 255, 255, 0.4);font-size: 14px; font-weight: 400;">{{ detailData.oldPrice }}</span>
-               <div>
-                  <span class="mr-2 rounded-lg" style="padding: 2px 4px 5px 4px;background: rgba(255, 77, 79, 1);"> -{{detailData.percent}}%</span>
-                  <span style="font-size: 18px;color:rgba(255, 77, 79, 1);font-weight: 500;">{{ detailData.newPrice }}</span>
-               </div>
-            </div>
+          <div class="flex flex-col">
+             <span class="line-through mr-2" style="text-align: right; color:rgba(255, 255, 255, 0.4);font-size: 14px; font-weight: 400;">{{ detailData.oldPrice }}</span>
+             <div>
+                <span class="mr-2 rounded-lg" style="padding: 2px 4px 5px 4px;background: rgba(255, 77, 79, 1);"> -{{detailData.percent}}%</span>
+                <span style="font-size: 18px;color:rgba(255, 77, 79, 1);font-weight: 500;">{{ detailData.newPrice }}</span>
+             </div>
           </div>
         </div>
-      </a-col>
-    </a-row>
+      </div>
+    </div>
+    <div class="flex items-center justify-center flex-col mt-5">
+      <div class="s-bg rounded-lg px-4 py-4 min-introduction" style="width: 960px;">
+        <span style="font-size: 18px;color: rgba(255,255,255,0.85); font-weight: 600;">游戏介绍</span>
+        <div v-html="gameIntroduction" class="game-intro"></div>
+      </div>
+    </div>
   </vue-custom-scrollbar>
+
+  <a-drawer  v-model:visible="detailDisplay" title="地区" style="text-align: center;"
+   :bodyStyle="{textAlign:'left'}" placement="right"  width="500" @close="onClose">
+   <vue-custom-scrollbar  @touchstart.stop @touchmove.stop @touchend.stop  :settings="settingsScroller" style="height: 86vh;">
+    <div class="w-full h-12 flex items-center right-select-active  justify-center pointer my-4 rounded-lg s-bg"
+      v-for="(item,index) in rightSelect" :class="defaultGameIndex === index ? 'active':''" @click="selectedAreaSuit(item,index)"
+    >
+      {{ item.name }}
+    </div>
+   </vue-custom-scrollbar>
+  </a-drawer>
 </template>
 
 <script>
@@ -89,7 +96,10 @@ export default {
     data(){
       return{
         rightSelect:regionRange,
-        defaultDetailRegion:'cn',
+        defaultDetailRegion:{
+          id:'cn',
+          name:'国区'
+        },
         detailData:{
           name:'',
           genres:[],
@@ -112,17 +122,13 @@ export default {
          suppressScrollX: true,
          wheelPropagation: true
         },
+        gameIntroduction:'',  // 接收游戏介绍
+        detailDisplay:false,
+        defaultGameIndex:0
       }
     },
     mounted(){
       this.getDetailVal()
-      // window.onresize = () => {
-      //   if(document.body.clientWidth <= 900){
-      //     document.querySelector('.detail-ms').style = 'margin:0 15%; !important'
-      //   }else{
-      //     document.querySelector('.detail-ms').style = 'margin:0; !important'
-      //   }
-      // }
     },
     methods:{
       goBack(){
@@ -130,7 +136,10 @@ export default {
       },
       getDetailVal(){
         if(this.id !== undefined){
-          sendRequest(`https://store.steampowered.com/api/appdetails?appids=${this.id}&cc=${this.defaultDetailRegion}&l=${this.defaultDetailRegion}`,3).then(res=>{
+          sendRequest(`https://store.steampowered.com/api/appdetails?appids=${this.id}&cc=${this.defaultDetailRegion.id}&l=${this.defaultDetailRegion.id}`,{},{
+            localCache:true,
+            localTtl:60*12*60
+          }).then(res=>{
             const result = res.data[this.id].data
             const newLanguages = result.supported_languages.replaceAll('<strong>*</strong>','')
             this.detailData = {
@@ -147,6 +156,7 @@ export default {
             }
             this.detailMovies = result.movies // 详情图片和视频数组拼接方便后期渲染
             this.detailScreenshots = result.screenshots
+            this.gameIntroduction = result.detailed_description
           })
         }else{
           this.$router.push({name:'gameDiscount'})
@@ -164,6 +174,16 @@ export default {
         let m = expireDate.getMonth() + 1 // 月
         let d = expireDate.getDate() // 日
         return m + '月' + d + '日'
+      },
+      // 打开详情区服选项
+      openDetailDrawer(){
+        this.detailDisplay = true
+      },
+      selectedAreaSuit(item,index){
+        this.defaultDetailRegion = item
+        this.defaultGameIndex = index
+        this.getDetailVal()
+        this.detailDisplay = false 
       }
     }
   }
@@ -177,10 +197,73 @@ export default {
 ::v-deep .ps__thumb-y{
   display: none !important;
 }
+.detail-container{
+  display: flex;
+  align-items: stretch;
+  justify-content: center;
+}
+.min-width{
+  max-width: 344px;
+}
+.active{
+  background: rgba(255,255,255,0.40);
+}
+@media screen and (max-width:1224px) {
+  #detail-name{
+    max-width: 120px;
+  }
+  .min-store{
+    padding: 8px 12px !important;
+  }
+  .detail-container{
+    display: flex !important;
+    flex-direction: column !important;
+    align-items: center !important;
+    justify-content:center !important;
+  }
+  .min-width{
+    max-width: 600px !important;
+    margin: 24px 0 0 0  !important;
+  }
+  .min-introduction{
+    max-width:600px !important;
+    .game-intro{
+      width: 100% !important;
+      ::v-deep p img{
+        width: 100% !important;
+      }
+      ::v-deep img{
+        width: 100% !important;
+      }
+    }
+  }
 
-@media screen and (max-width:1000px) {
-  #detail-ms{
-    margin: 0 15% !important;
+}
+
+@media screen and (max-width:850px){
+  .min-width{
+    max-width: 500px !important;
+  }
+  .detail-container{
+    display: flex !important;
+    flex-direction: column !important;
+    align-items: center !important;
+    justify-content:center !important;
+  }
+  .wheel-content{
+    max-width: 500px !important;
+  }
+  .min-introduction{
+    max-width:500px !important;
+    .game-intro{
+      width: 100% !important;
+      ::v-deep p img{
+        width: 100% !important;
+      }
+      ::v-deep img{
+        width: 100% !important;
+      }
+    }
   }
 }
 </style>
