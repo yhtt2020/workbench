8f4546218700c9beedc4975a65eaee128be34f7a
xiaoyang <1611037570@qq.com>
2023-06-05 11:00:10 +0800

    【优化】设备设置样式更新

--- vite/packages/table/page/Status.vue
+++ vite/packages/table/page/Status.vue
@@ -5,7 +5,7 @@
       <div style="width: auto;   ">
         <div style="display: inline-block;vertical-align: top">
           <div
-            style="margin: 2em;padding:2em;border-radius: 0.5em;width: 40em;background-color:var(--background-color) ; color: var(--font-color);">
+            style="margin: 2em;padding:2em;border-radius: 0.5em;width: 40em;background-color:var(--primary-bg) ; color: var(--primary-text);">
             <h3 style="color: var(--font-color)">音量</h3>
             <a-row>
               <a-col :span="3">
@@ -25,7 +25,7 @@
             </div>
           </div>
           <div
-            style="margin: 2em;background: #282828;padding:2em;border-radius: 0.5em;width: 40em;background-color:var(--background-color) ; color: var(--font-color);">
+            style="margin: 2em;background: #282828;padding:2em;border-radius: 0.5em;width: 40em;background-color:var(--primary-bg) ; color: var(--primary-text);">
             <h3 style="color: var(--font-color)">屏幕亮度</h3>
             <a-row>
               <a-col :span="3">
@@ -49,7 +49,7 @@
 
         <div style="display: inline-block; ">
           <div
-            style="margin: 2em;background: #282828;padding:2em;border-radius: 0.5em;width: 40em;vertical-align: top;background-color:var(--background-color) ; color: var(--font-color);">
+            style="margin: 2em;background: #282828;padding:2em;border-radius: 0.5em;width: 40em;vertical-align: top;background-color:var(--primary-bg) ; color: var(--primary-text);">
             <h3 style="color: var(--font-color)">音频输出设备</h3>
             <div v-for=" audio in audioList">
               <div @click="setAudio(audio)" class="audio" :class="{ 'active': audio.isDefaultForMultimedia }">
@@ -66,7 +66,7 @@
         </div>
         <div style="display: inline-block;vertical-align: top">
           <div
-            style="margin: 2em;background: #282828;padding:2em;border-radius: 0.5em;width: 40em;vertical-align: top;background-color:var(--background-color) ; color: var(--font-color);">
+            style="margin: 2em;background: #282828;padding:2em;border-radius: 0.5em;width: 40em;vertical-align: top;background-color:var(--primary-bg) ; color: var(--primary-text);">
             <h3 style="color: var(--font-color)">音频输入设备</h3>
             <div v-for="audio in micList">
               <div @click="setAudio(audio)" class="audio" :class="{ 'active': audio.deviceId === 'default' }">
