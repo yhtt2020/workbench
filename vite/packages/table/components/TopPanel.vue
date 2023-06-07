ebc5f364eead3113e807c04a018ef39c79ff1744
陈一枭 <cyx@thisky.com>
2023-06-07 14:05:55 +0800

    【新增】加入窗口控制的代码位置

--- vite/packages/table/components/TopPanel.vue
+++ vite/packages/table/components/TopPanel.vue
@@ -2,15 +2,16 @@
   <a-row v-if="isMain" class="top-panel drag" type="flex" :gutter="10">
     <a-col :span="8">
       <div class="pointer no-drag text-more" style="display: inline-block" @click="enterMusic"
-        v-if="status.music && status.music.title">
+           v-if="status.music && status.music.title">
         <a-avatar style="margin-right: 0.5em" :size="24" :src="status.music.cover"></a-avatar>
         {{ status.music.title }} {{ status.music.singer }}
       </div>
     </a-col>
     <a-col :span="8" style="text-align: center">
       <div @click="openGlobalSearch" class="input-box no-drag pointer"
-        :style="{ backgroundColor: 'var(--background-color)', color: 'var(--no-active-font-color)' }">
-        <Icon icon="sousuo"></Icon> 搜索 网页内容、标签
+           :style="{ backgroundColor: 'var(--background-color)', color: 'var(--no-active-font-color)' }">
+        <Icon icon="sousuo"></Icon>
+        搜索 网页内容、标签
         <!--        <span style="float: right; padding-right: 10px">-->
         <!--          <span style="margin-right: 3px" class="key-button">Alt</span-->
         <!--          ><span class="key-button">F</span>-->
@@ -18,20 +19,28 @@
       </div>
     </a-col>
     <a-col :span="8" style="text-align: right;color: var(--no-active-font-color)" class="s-text">
-      <span style=" font-size: 0.8em; margin-right: 1em" v-if="settings.tipLock && this.showLockTip">
-        {{ lockTimeoutDisplay }}后锁屏
-      </span>
-      <span class="no-drag" v-if="!loading">{{ dateTime.month }}/{{ dateTime.day }} {{ dateTime.hours }}:{{
-        dateTime.minutes
-      }}
-        {{ dateTime.week }}
+      <div style="text-align: right;display: flex;flex-direction: row;align-items: flex-end;justify-content: flex-end">
 
-        <span v-if="hasWeather && city.now">
+
+
+        <div class="no-drag" v-if="!loading">
+            <span style=" font-size: 0.8em; margin-right: 1em" v-if="settings.tipLock && this.showLockTip">
+        {{ lockTimeoutDisplay }}后锁屏
+      </span>{{ dateTime.month }}/{{ dateTime.day }} {{ dateTime.hours }}:{{
+            dateTime.minutes
+          }}
+          {{ dateTime.week }}
+          <span v-if="hasWeather && city.now">
           <i style="" :class="'qi-' + city.now.icon + '-fill'"></i> {{ city.now.temp }}℃
         </span>
+        </div>
+<!--   窗口控制     -->
+<!--        <div class="no-drag">-->
 
-
-      </span>
+<!--            <icon icon="guanbi"></icon>-->
+<!--          <icon icon="guanbi"></icon>-->
+<!--        </div>-->
+      </div>
     </a-col>
   </a-row>
   <a-row style="height: 1em;cursor: move" class="drag text-right" v-else>
@@ -39,7 +48,7 @@
   </a-row>
 </template>
 
-<script >
+<script>
 import { getDateTime } from '../../../src/util/dateTime'
 import { appStore } from '../store'
 import { cardStore } from '../store/card'
@@ -49,55 +58,55 @@ import { weatherStore } from '../store/weather'
 import { isMain } from '../js/common/screenUtils'
 
 export default {
-  name: "TopPanel",
-  data() {
+  name: 'TopPanel',
+  data () {
     return {
       loading: true,
       dateTime: {},
       timer: null,
       lockTimer: null,
       showLockTip: false,
-    };
+    }
   },
   computed: {
     ...mapWritableState(appStore, ['status']),
     ...mapState(weatherStore, ['cities']),
     ...mapWritableState(paperStore, ['settings', 'lockTimeout']),
     isMain,
-    lockTimeoutDisplay() {
+    lockTimeoutDisplay () {
       // if(this.lockTimeout>=60){
       //   return ((this.lockTimeout/60).toFixed(0)-1)+'分'+this.lockTimeout % 60+'秒'
       // }else{
       //   return this.lockTimeout+'秒'
       // }
-      function secTotime(s) {
-        var t = '';
+      function secTotime (s) {
+        var t = ''
         if (s > -1) {
           var hour = Math.floor(s / 3600)
           var min = Math.floor(s / 60) % 60
           var sec = s % 60
           if (hour === 0)
             t = ''
-          else
-            if (hour < 10) {
-              t = '0' + hour + "小时"
-            } else {
-              t = hour + "小时"
-            }
-          if (min < 10) {
-            t += "0"
+          else if (hour < 10) {
+            t = '0' + hour + '小时'
+          } else {
+            t = hour + '小时'
           }
-          t += min + "分"
+          if (min < 10) {
+            t += '0'
+          }
+          t += min + '分'
           if (sec < 10) {
-            t += "0"
+            t += '0'
           }
           t += sec.toFixed(0) + '秒'
         }
         return t
       }
+
       return secTotime(this.lockTimeout)
     },
-    city() {
+    city () {
       if (this.cities[0]) {
         return this.cities[0]
       } else {
@@ -105,11 +114,11 @@ export default {
       }
 
     },
-    hasWeather() {
-      return this.cities.length > 0;
+    hasWeather () {
+      return this.cities.length > 0
     },
   },
-  mounted() {
+  mounted () {
     window.onblur = () => {
       this.setLockTimer()
     }
@@ -121,12 +130,12 @@ export default {
       setInterval(this.getTime, 1000)
     }
   },
-  created() {
+  created () {
     this.getTime()
   },
   methods: {
-    ...mapActions(cardStore, ["setAppDate"]),
-    clearLockTimer() {
+    ...mapActions(cardStore, ['setAppDate']),
+    clearLockTimer () {
       if (this.lockTimer) {
         clearInterval(this.lockTimer)
         this.lockTimer = null
@@ -134,7 +143,7 @@ export default {
         this.showLockTip = false
       }
     },
-    setLockTimer() {
+    setLockTimer () {
       if (this.settings.enable) {
         //只有启用了锁屏才会触发这个效果
         if (this.lockTimer) {
@@ -152,20 +161,20 @@ export default {
         }
       }
     },
-    openGlobalSearch() {
+    openGlobalSearch () {
       ipc.send('openGlobalSearch')
     },
-    getTime() {
-      this.dateTime = getDateTime();
-      this.setAppDate(this.dateTime);
+    getTime () {
+      this.dateTime = getDateTime()
+      this.setAppDate(this.dateTime)
     },
-    enterMusic() {
+    enterMusic () {
       this.$router.push({
-        name: "music",
-      });
+        name: 'music',
+      })
     },
   },
-};
+}
 </script>
 
 <style scoped>
