548ce810ded8069af1acbe9b457577b85d45d139
wjl <3123911548@qq.com>
2023-06-08 09:30:42 +0800

    【调整】顶部窗口按钮布局样式

--- vite/packages/table/components/TopPanel.vue
+++ vite/packages/table/components/TopPanel.vue
@@ -1,13 +1,13 @@
 <template>
   <a-row v-if="isMain" class="top-panel drag" type="flex" :gutter="10">
-    <a-col :span="8">
+    <a-col :span="7">
       <div class="pointer no-drag text-more" style="display: inline-block" @click="enterMusic"
            v-if="status.music && status.music.title">
         <a-avatar style="margin-right: 0.5em" :size="24" :src="status.music.cover"></a-avatar>
         {{ status.music.title }} {{ status.music.singer }}
       </div>
     </a-col>
-    <a-col :span="8" style="text-align: center">
+    <a-col :span="6" style="text-align: center">
       <!-- 快速搜索 头部 搜索栏 -->
       <div @click="openGlobalSearch" class="input-box no-drag pointer"
            style=" background: var( --primary-bg); color: var(--secondary-text)">
@@ -19,9 +19,9 @@
         <!--        </span>-->
       </div>
     </a-col>
-    <a-col :span="8" style="text-align: right;color: var(--secondary-text);" class="s-text">
-      <div style="text-align: right;display: flex;flex-direction: row;align-items: flex-end;justify-content: flex-end">
-        <div class="no-drag pr-52" v-if="!loading">
+    <a-col :span="6" style="text-align: right;color: var(--secondary-text);" class="s-text">
+      <div class="mr-3" style="text-align: right;display: flex;flex-direction: row;align-items: flex-end;justify-content: flex-end">
+        <div class="no-drag" v-if="!loading">
             <span style=" font-size: 0.8em; margin-right: 1em" v-if="settings.tipLock && this.showLockTip">
         <!-- {{ lockTimeoutDisplay }}后锁屏 -->
       </span>{{ dateTime.month }}/{{ dateTime.day }} {{ dateTime.hours }}:{{
@@ -32,23 +32,25 @@
           <i style="" :class="'qi-' + city.now.icon + '-fill'"></i> {{ city.now.temp }}℃
         </span>
         </div>
-        <div class="flex s-item px-3 py-2 btn-container rounded-bl-lg">
-          <div @click="closeLargeScreen" class="flex mr-3 p-2 rounded-md items-center btn-top-active pointer no-drag">
-            <MinusOutlined style="font-size: 1.5em;color:rgba(255,255,255,0.25);"></MinusOutlined>
-          </div>
-          <div @click="fixedButton" class="flex pointer p-2 rounded-md btn-top-active mr-3 items-center no-drag">
-            <Icon icon="Pushpin" style="font-size: 1.5em;color:rgba(255,255,255,0.25);"></Icon>
-          </div>
-          <div @click="openLargeScreen" class="flex pointer p-2 rounded-md btn-top-active mr-3 items-center no-drag">
-            <BorderOutlined style="font-size: 1.5em;color:rgba(255,255,255,0.25);"></BorderOutlined>
-          </div>
-          <div @click="closeScreen" class="flex p-2 btn-top-active rounded-md pointer items-center no-drag">
-            <Icon icon="guanbi" style="font-size: 1.5em;color:rgba(255,255,255,0.25);"></Icon>
-          </div>
-        </div>
       </div>
     </a-col>
-    
+    <a-col  style="text-align: right;" >
+      <div class="flex s-item no-drag px-2 py-2 btn-container rounded-bl-lg">
+        <div @click="closeLargeScreen" class="flex mr-1 p-2 rounded-md items-center btn-top-active pointer no-drag">
+          <MinusOutlined style="font-size: 1.5em;color:rgba(255,255,255,0.25);"></MinusOutlined>
+        </div>
+        <div @click="fixedButton" class="flex pointer p-2 rounded-md btn-top-active mr-1 items-center no-drag">
+          <Icon icon="Pushpin" style="font-size: 1.75em;color:rgba(255,255,255,0.35);"></Icon>
+        </div>
+        <div @click="openLargeScreen" class="flex pointer p-2 rounded-md btn-top-active mr-1 items-center no-drag">
+          <BorderOutlined style="font-size: 1.75em;color:rgba(255,255,255,0.35);"></BorderOutlined>
+        </div>
+        <div @click="closeScreen" class="flex p-2 btn-top-active rounded-md pointer items-center no-drag">
+          <Icon icon="guanbi" style="font-size: 1.75em;color:rgba(255,255,255,0.35);"></Icon>
+        </div>
+      </div> 
+    </a-col>
+
   </a-row>
   <a-row style="height: 1em;cursor: move" class="drag text-right" v-else>
 
@@ -217,7 +219,7 @@ export default {
 
 .btn-top-active{
   &:hover{
-    color: rgba(255, 255, 255, 0.5) !important;
+    color: rgba(255, 255, 255, 0.85) !important;
     background: rgba(42, 42, 42, 0.2);
   }
   &:active{
