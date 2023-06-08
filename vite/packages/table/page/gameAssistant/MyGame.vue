1bc8cd3d7f59a3a5a801277b28419aa478816964
陈一枭 <cyx@thisky.com>
2023-06-08 17:20:01 +0800

    【修复】我的游戏仓库挂掉问题

--- vite/packages/table/page/gameAssistant/MyGame.vue
+++ vite/packages/table/page/gameAssistant/MyGame.vue
@@ -222,9 +222,11 @@ export default {
   mounted() {
     this.steamGameList = this.gameList
     console.log(this.gameList[0])
-    if(this.gameList[0].appinfo){
-      console.log('重新格式化')
-      this.setGameList(this.gameList)
+    if(this.gameList.length>0) {
+      if (this.gameList[0].appinfo) {
+        console.log('重新格式化')
+        this.setGameList(this.gameList)
+      }
     }
   },
   computed:{
