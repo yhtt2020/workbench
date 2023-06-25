f490bca3b8e5fe36108828221d0f801adfb20546
陈一枭 <cyx@thisky.com>
2023-06-25 16:52:15 +0800

    【新增】编辑导航栏的时候加入推荐导航栏的编辑

--- vite/packages/table/js/data/tableData.js
+++ vite/packages/table/js/data/tableData.js
@@ -1,20 +1,20 @@
-let vm = null;
-const sendThis = ( _this )=> {
-  vm = _this;
-};
+let vm = null
+const sendThis = (_this) => {
+  vm = _this
+}
 export default {
   sendThis,
-  navigationClassify :[
-    {name:'systemApp',cname:'系统应用'},
-    {name:'coolApp',cname:'酷应用'},
-    {name:'tableApp',cname:'Windows应用'},
-    {name:'localApp',cname:'自定义应用'},
-    {name:'lightApp',cname:'轻应用'},
+  navigationClassify: [
+    { name: 'systemApp', cname: '系统应用' },
+    { name: 'coolApp', cname: '酷应用' },
+    { name: 'tableApp', cname: 'Windows应用' },
+    { name: 'localApp', cname: '自定义应用' },
+    { name: 'lightApp', cname: '轻应用' },
   ],
-  coolAppList:[
+  coolAppList: [
     {
       icon: 'http://a.apps.vip/icons/flappy.jpg',
-      type:'coolApp',
+      type: 'coolApp',
       name: 'Mlappy Bird',
       summary: '和小伙伴们一起飞。',
       needInstall: false,
@@ -23,14 +23,14 @@ export default {
         name: 'mlappyBird',
         url: 'http://bird.apps.vip/?',
         background: false,
-        type:'game',
-        scale:160
+        type: 'game',
+        scale: 160
       }
     },
     {
       icon: 'https://s1.hdslb.com/bfs/static/jinkela/popular/assets/icon_popular.png',
       name: '网页数据监控小助手',
-      type:'coolApp',
+      type: 'coolApp',
       summary: '一个用于检测网页数据的工具，可以实时监测一个网页数据动态，并组织成可视化的报表，目前支持B站视频数据追踪。',
       needInstall: false,
       data: {
@@ -46,7 +46,7 @@ export default {
     {
       icon: 'https://a.apps.vip/icons/ppet.png',
       name: 'PPet桌面宠物',
-      type:'coolApp',
+      type: 'coolApp',
       summary: '一款开源桌面看板娘，让你不再孤单。',
       needInstall: true,
       data: {
@@ -56,7 +56,7 @@ export default {
     {
       icon: 'https://a.apps.vip/wallpaper/favicon.png',
       name: 'OneWallheaven壁纸',
-      type:'coolApp',
+      type: 'coolApp',
       summary: '开源壁纸轻应用，可以将wallheaven上的壁纸设置为桌面壁纸或者工作台壁纸。',
       needInstall: false,
       data: {
@@ -71,7 +71,7 @@ export default {
     }, {
       icon: 'https://a.apps.vip/icons/kook.png',
       name: 'Kook',
-      type:'coolApp',
+      type: 'coolApp',
       summary: '在副屏上使用Kook，一个好用的开黑组团语音沟通工具',
       needInstall: false,
       data: {
@@ -81,13 +81,13 @@ export default {
         background: true,
         node: false,
         security: true,
-        fullScreen:false,
+        fullScreen: false,
       }
     },
     {
       icon: 'https://res.wx.qq.com/a/wx_fed/assets/res/OTE0YTAw.png',
       name: '微信',
-      type:'coolApp',
+      type: 'coolApp',
       summary: '在副屏上使用网页版微信聊天。（已适配小屏）',
       needInstall: false,
       data: {
@@ -103,7 +103,7 @@ export default {
     {
       icon: 'https://p1-hera.byteimg.com/tos-cn-i-jbbdkfciu3/22718e94fbd9483ea54301cf431ce2ee~tplv-jbbdkfciu3-image:0:0.image',
       name: '飞书',
-      type:'coolApp',
+      type: 'coolApp',
       summary: '在副屏上使用飞书办公。（可用）',
       needInstall: false,
       data: {
@@ -119,7 +119,7 @@ export default {
     {
       icon: 'https://a.apps.vip/todo/logo.png',
       name: '轻待办',
-      type:'coolApp',
+      type: 'coolApp',
       summary: '快速创建和管理你的待办。',
       needInstall: false,
       data: {
@@ -136,35 +136,49 @@ export default {
   ],
   systemAppList: [
     {
-      type:'systemApp',
-      icon:'home',
-      name:'主页',
-      event:'home',
+      type: 'systemApp',
+      icon: 'home',
+      name: '主页',
+      event: 'home',
     },
     {
-      "type": "systemApp",
-      "icon": "youxishoubing",
-      "name": "游戏",
-      "event": "gameIndex"
-    },{
-        "type": "systemApp",
-        "icon": "yinle1",
-        "name": "音乐",
-        "event": "music"
+      'type': 'systemApp',
+      'icon': 'youxishoubing',
+      'name': '游戏',
+      'event': 'gameIndex'
+    },
+    {
+      'type': 'systemApp',
+      'icon': 'wangluo',
+      'name': '浏览器',
+      'event': 'browser'
+    },
+    {
+      'type': 'systemApp',
+      'icon': 'yinle1',
+      'name': '音乐',
+      'event': 'music'
     },
     {
       type:'systemApp',
-      icon:'lock',
-      name:'锁定屏幕',
-      event:'lock',
-      fn(){
-        vm.$router.push({ path: '/lock' })}
-    },{
-      type:'systemApp',
-      icon:'desktop',
-      name:'设备设置',
-      event:'status',
-      fn:()=>{
+      icon:'banner',
+      name:'壁纸',
+      event:'my',
+    },
+    {
+      type: 'systemApp',
+      icon: 'lock',
+      name: '锁定屏幕',
+      event: 'lock',
+      fn () {
+        vm.$router.push({ path: '/lock' })
+      }
+    }, {
+      type: 'systemApp',
+      icon: 'desktop',
+      name: '设备设置',
+      event: 'status',
+      fn: () => {
         if (vm.$route.path === '/status') {
           vm.$router.go(-1)
         } else {
@@ -173,62 +187,61 @@ export default {
       }
     },
     {
-      type:'systemApp',
-      icon:'fullscreen',
-      name:'全屏显示',
-      event:'fullscreen',
-      fn:()=>{
+      type: 'systemApp',
+      icon: 'fullscreen',
+      name: '全屏显示',
+      event: 'fullscreen',
+      fn: () => {
         if (vm.full) {
-          vm.$emit('setFull',false)
+          vm.$emit('setFull', false)
           tsbApi.window.setFullScreen(false)
         } else {
-          vm.$emit('setFull',true)
+          vm.$emit('setFull', true)
           tsbApi.window.setFullScreen(true)
         }
       }
     },
     {
-      type:'systemApp',
-      icon:'setting',
-      name:'基础设置',
-      event:'setting',
-      fn:()=>{  vm.$router.push({ name: 'setting' })}
-    },{
-      type:'systemApp',
-      icon:'tuichu',
-      name:'电源',
-      event:'power',
-      fn:()=>{vm.$router.push({ path: '/power' })}
+      type: 'systemApp',
+      icon: 'setting',
+      name: '基础设置',
+      event: 'setting',
+      fn: () => { vm.$router.push({ name: 'setting' })}
+    }, {
+      type: 'systemApp',
+      icon: 'tuichu',
+      name: '电源',
+      event: 'power',
+      fn: () => {vm.$router.push({ path: '/power' })}
     },
-     {
-      type:'systemApp',
-      icon:'thunderbolt',
-      name:'快捷指令',
-      event:'deck',
-    },{
-      type:'systemApp',
-      icon:'shangchuan',
-      name:'文件传输',
-      event:'fileHelper',
-      data:{
-          theme: '#2c2c2c',
-          name: 'fileHelper',
-          url: 'https://szfilehelper.weixin.qq.com/',
-          preload: 'fileHelper',
-          background: false,
+    {
+      type: 'systemApp',
+      icon: 'thunderbolt',
+      name: '快捷指令',
+      event: 'deck',
+    }, {
+      type: 'systemApp',
+      icon: 'shangchuan',
+      name: '文件传输',
+      event: 'fileHelper',
+      data: {
+        theme: '#2c2c2c',
+        name: 'fileHelper',
+        url: 'https://szfilehelper.weixin.qq.com/',
+        preload: 'fileHelper',
+        background: false,
       }
     },
     {
-      type:'systemApp',
-      icon:'yingyongzhongxin',
-      name:'应用管理',
-      event:'apps',
+      type: 'systemApp',
+      icon: 'yingyongzhongxin',
+      name: '应用管理',
+      event: 'apps',
     },
     {
-      type:'systemApp',
-      icon:'daibanshixiang',
-      name:'待办',
-      event:'home',
+      type: 'systemApp',
+      icon: 'daibanshixiang',
+      name: '待办',
       data:
         {
           fullScreen: false,
