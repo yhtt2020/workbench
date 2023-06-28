719355727f9847800ff4592e2c9c7d864cf70468
陈一枭 <cyx@thisky.com>
2023-06-28 09:28:39 +0800

    【修复】23013 左侧导航栏，如果超过一页的话，会有滚动条 http://zt.xiangtian.ren/bug-view-23013.html?tid=nu8sksyi

--- vite/packages/table/components/SidePanel.vue
+++ vite/packages/table/components/SidePanel.vue
@@ -9,7 +9,7 @@
           padding-top: 0;padding-bottom: 0;position:relative;" ref="sideContent"
          @contextmenu.stop="showMenu">
       <div style="width: 67px;overflow-x: hidden">
-        <div style="width: 80px;overflow-y:auto;max-height: 100%;display: flex;flex-direction: column;overflow-x: hidden;align-items: flex-start">
+        <div class="scroller-wrapper" style="width: 80px;overflow-y:auto;max-height: 100%;display: flex;flex-direction: column;overflow-x: hidden;align-items: flex-start">
           <div v-for="item in sideNavigationList" :key="item.name" @click="clickNavigation(item)">
             <div class="flex felx-col justify-center items-center item-nav" :class="{ 'active-back': current(item) }">
               <div class="icon-color" v-if="item.type === 'systemApp'">
@@ -241,6 +241,12 @@ export default {
 @media screen and (min-height: 551px) and (max-height: 610px) {
   .side-panel {
     zoom: 0.9;
+  }
+}
+
+.scroller-wrapper{
+  &::-webkit-scrollbar {
+    display: none; /* Chrome Safari */
   }
 }
 </style>
