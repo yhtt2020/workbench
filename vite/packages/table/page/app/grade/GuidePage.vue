fc1183efac49584e160d21bac22259778e5c2d17
wjl <3123911548@qq.com>
2023-06-29 20:47:24 +0800

    【优化】引导桌面添加逻辑

--- vite/packages/table/page/app/grade/GuidePage.vue
+++ vite/packages/table/page/app/grade/GuidePage.vue
@@ -224,53 +224,43 @@ export default {
       if(this.step === 1 ){
         if(this.selectItem.length > 0){  // 判断是不是多选
           for(let i=0;i<this.selectItem.length;i++){
-           this.addMoreDesk(this.guideData[i])
+           this.addSwitchDesk(this.guideData[i])
           }
+        }else{
+          this.addSwitchDesk(this.guideData[this.statusIndex])
         }
-        // 单选
-        this.addSwitchDesk(this.guideData[this.statusIndex])
+       
       }
     },
 
-    addSwitchDesk(obj){   // 添加单选桌面
-      switch (obj.title) {
-          case '游戏娱乐':
-            // 添加桌面
-            this.addDesk(this.deskTemplate.gameName,this.deskTemplate.game)
-            // 添加桌面底部和左侧导航
+    addSwitchDesk(obj){   // 添加桌面和导航
+      switch (obj.id) {
+        case 'gr': // 游戏娱乐
+          this.addDesk(this.deskTemplate.gameName,this.deskTemplate.game)
+          if(this.selectItem.length === 1){
             this.updateLeftNavData(gamePanel.left)
             this.updateBottomNavData(gamePanel.bottom)
-            break;
-          case '效率辅助':
-            // 添加桌面
-            this.addDesk(this.deskTemplate.workName,this.deskTemplate.work)
-            // 添加桌面底部和左侧导航
+          }
+          break;
+        case 'wf': // 效率辅助
+          this.addDesk(this.deskTemplate.workName,this.deskTemplate.work)
+          if(this.selectItem.length === 1){
             this.updateLeftNavData(workPanel.left)
             this.updateBottomNavData(workPanel.bottom)
-            break;
-          case '极简DIY':
-            // 添加桌面
-            this.addDesk(this.deskTemplate.emptyName,this.deskTemplate.empty)
-            // 添加桌面底部和左侧导航
+          }
+          break;
+        case 'dy': // 极简diy
+          this.addDesk(this.deskTemplate.emptyName,this.deskTemplate.empty)
+          if(this.selectItem.length === 1){
             this.updateLeftNavData(diyPanel.left)
             this.updateBottomNavData(diyPanel.bottom)
-            break;
+          }
+          break;
       }
-    },
-
-    addMoreDesk(obj){  // 添加多选桌面
-      switch (obj.title) {
-          case '游戏娱乐':
-            // 添加桌面
-            this.addDesk(this.deskTemplate.gameName,this.deskTemplate.game)
-            break;
-          case '效率辅助':
-            // 添加桌面
-            this.addDesk(this.deskTemplate.workName,this.deskTemplate.work)
-            break;
+      if(this.selectItem.length === 2){
+        this.updateLeftNavData(mergePanel.left)
+        this.updateBottomNavData(mergePanel.bottom)
       }
-      this.updateLeftNavData(mergePanel.left)
-      this.updateBottomNavData(mergePanel.bottom)
     },
 
   },
