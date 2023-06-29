59a147f41f77222fe30a72f6f13c5e758d029f75
陈一枭 <cyx@thisky.com>
2023-06-29 09:33:02 +0800

    【修复】22834 游戏折扣详情页样式错误 http://zt.xiangtian.ren/bug-view-22834.html?tid=tbeya2y2

--- vite/packages/table/components/WheelCastingUnit.vue
+++ vite/packages/table/components/WheelCastingUnit.vue
@@ -1,29 +1,36 @@
 <template>
+
   <div class="wheel-content flex flex-col" style="width:600px;">
     <div ref="slider" class="keen-slider rounded-lg wheel-bg">
       <div class="keen-slider__slide rounded-lg" v-for="item in wheelList">
-        <video class="w-full h-full rounded-lg" @ended="next"  controls="controls"   controlslist="nodownload  noremoteplayback noplaybackrate"
-        disablePictureInPicture  ref="wheelVideo"  playsinline="" autoplay="" muted="" v-if="item.mp4">
+        <video class="w-full h-full rounded-lg" @ended="next" controls="controls"
+               controlslist="nodownload  noremoteplayback noplaybackrate"
+               disablePictureInPicture ref="wheelVideo" playsinline="" autoplay="" muted="" v-if="item.mp4">
           <source :src="item.mp4.max" class="w-full rounded-lg" type="video/mp4" id="bgVid">
         </video>
         <viewer :images="item.path_full" :options="options" class="w-full h-full rounded-lg object-cover " v-else>
-          <img :src="item.path_full"  class="w-full pointer h-full rounded-lg object-cover " :data-source="item.path_full" alt="" >
+          <img :src="item.path_full" class="w-full pointer h-full rounded-lg object-cover "
+               :data-source="item.path_full" alt="">
         </viewer>
       </div>
     </div>
     <div class="flex mt-3 " style="height: 72px;">
-      <button class="keen-slider__arrow btn-active mr-2 wheel-bg pointer rounded-md keen-slider__arrow--left " @click="prev()" style="border: none;">
-         <Icon icon="xiangzuo" style="font-size: 1.5em;"></Icon>
+      <button class="keen-slider__arrow btn-active mr-2 wheel-bg pointer rounded-md keen-slider__arrow--left "
+              @click="prev()" style="border: none;">
+        <Icon icon="xiangzuo" style="font-size: 1.5em;"></Icon>
       </button>
       <div ref="thumbnail" class="keen-slider thumbnail">
         <div class="keen-slider__slide rounded-lg" v-for="item in wheelList">
-          <img :src="item.mp4 ? item.thumbnail : item.path_thumbnail" class="w-full h-full  rounded-md  object-cover" alt="">
-          <div class="thumbnail-bofang w-8 h-8  rounded-full flex items-center justify-center" style="background: var(--primary-bg);" v-if="item.mp4">
+          <img :src="item.mp4 ? item.thumbnail : item.path_thumbnail" class="w-full h-full  rounded-md  object-cover"
+               alt="">
+          <div class="thumbnail-bofang w-8 h-8  rounded-full flex items-center justify-center"
+               style="background: var(--primary-bg);" v-if="item.mp4">
             <Icon icon="bofang" style="font-size: 2em;color: var(--primary-text);"></Icon>
           </div>
         </div>
       </div>
-      <button class="keen-slider__arrow ml-2 wheel-bg pointer btn-active rounded-md keen-slider__arrow--right " @click="next()" style="border: none;">
+      <button class="keen-slider__arrow ml-2 wheel-bg pointer btn-active rounded-md keen-slider__arrow--right "
+              @click="next()" style="border: none;">
         <Icon icon="xiangyou" style="font-size: 1.5em;"></Icon>
       </button>
     </div>
@@ -31,32 +38,33 @@
 </template>
 
 <script>
-import KeenSlider from "keen-slider"
-import "keen-slider/keen-slider.min.css"
+import KeenSlider from 'keen-slider'
+import 'keen-slider/keen-slider.min.css'
 
-function ThumbnailPlugin(main) {
+function ThumbnailPlugin (main) {
   return (slider) => {
-    function removeActive() {
+    function removeActive () {
       slider.slides.forEach((slide) => {
-        slide.classList.remove("active")
+        slide.classList.remove('active')
       })
     }
-    function addActive(idx) {
-      slider.slides[idx].classList.add("active")
+
+    function addActive (idx) {
+      slider.slides[idx].classList.add('active')
     }
 
-    function addClickEvents() {
+    function addClickEvents () {
       slider.slides.forEach((slide, idx) => {
-        slide.addEventListener("click", () => {
+        slide.addEventListener('click', () => {
           main.moveToIdx(idx)
         })
       })
     }
 
-    slider.on("created", () => {
+    slider.on('created', () => {
       addActive(slider.track.details.rel)
       addClickEvents()
-      main.on("animationStarted", (main) => {
+      main.on('animationStarted', (main) => {
         removeActive()
         const next = main.animator.targetIdx || 0
         addActive(main.track.absToRel(next))
@@ -67,56 +75,78 @@ function ThumbnailPlugin(main) {
 }
 
 export default {
-  name: "WheelCastingUnit",
-  props:{
-    wheelList:{
-      type:Array,
-      default:()=>[]
+  name: 'WheelCastingUnit',
+  props: {
+    wheelList: {
+      type: Array,
+      default: () => []
+    },
+    loading: {
+      type: Boolean
     }
   },
-  mounted() {
-    this.$nextTick(()=>{
-      this.slider = new KeenSlider(this.$refs.slider)
-      this.thumbnail = new KeenSlider(
-       this.$refs.thumbnail,
-       {
-        initial: 0,
-        slides: {
-          perView: 4,
-          spacing: 10,
-        },
-       },
-       [ThumbnailPlugin(this.slider)]
-      )
-    })
+
+  mounted () {
+    console.log('载入时loading', this.loading)
+    if (this.loading === false) {
+      this.loaded = true
+      this.initComponent()
+    }
   },
-  data(){
-    return{
+  watch: {
+    'loading': {
+      handler (newVal) {
+        console.log('检测到变化')
+        if (newVal === false) {
+          this.initComponent()
+        }
+      }
+    }
+  },
+  data () {
+    return {
+      loaded: false,
+
       options: {
         url: 'data-source',
-        toolbar:false,
-        title:false,
-        tooltip:false,
-        navbar:false
+        toolbar: false,
+        title: false,
+        tooltip: false,
+        navbar: false
       },
     }
   },
-  methods:{
-    prev() {
+  methods: {
+    initComponent () {
+      console.log('变化为false')
+      this.slider = new KeenSlider(this.$refs.slider)
+      this.thumbnail = new KeenSlider(
+        this.$refs.thumbnail,
+        {
+          initial: 0,
+          slides: {
+            perView: 4,
+            spacing: 10,
+          },
+        },
+        [ThumbnailPlugin(this.slider)]
+      )
+    },
+    prev () {
       this.slider.prev()
-      if(this.$refs.wheelVideo[this.slider.track.details.rel] !== undefined){
+      if (this.$refs.wheelVideo[this.slider.track.details.rel] !== undefined) {
         this.$refs.wheelVideo[this.slider.track.details.rel].pause()
       }
     },
-    next(){
+    next () {
       this.slider.next()
       // 切换下一个,上一个视频暂停
-      if(this.$refs.wheelVideo[this.slider.track.details.rel] !== undefined){
+      if (this.$refs.wheelVideo[this.slider.track.details.rel] !== undefined) {
         this.$refs.wheelVideo[this.slider.track.details.rel].pause()
       }
     },
   },
-  beforeDestroy() {
+  beforeDestroy () {
     if (this.slider) this.slider.destroy()
     if (this.thumbnail) this.thumbnail.destroy()
   },
@@ -124,7 +154,7 @@ export default {
 </script>
 
 <style>
-.wheel-bg{
+.wheel-bg {
   background: var(--primary-bg);
   color: var(--primary-text);
 }
@@ -204,21 +234,25 @@ export default {
 .thumbnail .keen-slider__slide {
   cursor: pointer;
 }
-.keen-slider{
+
+.keen-slider {
   flex: 1;
   align-self: stretch;
 }
-.thumbnail-bofang{
+
+.thumbnail-bofang {
   position: absolute;
   top: 50%;
   left: 50%;
-  transform: translate(-50%,-50%);
+  transform: translate(-50%, -50%);
 }
+
 .thumbnail .keen-slider__slide.active {
-  border: 3px solid rgba(255,255,255,0.5);
+  border: 3px solid rgba(255, 255, 255, 0.5);
 }
-.btn-active:active{
+
+.btn-active:active {
   filter: brightness(0.8);
-  background:rgba(42, 42, 42, 0.25);
+  background: rgba(42, 42, 42, 0.25);
 }
 </style>
