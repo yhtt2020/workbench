1f04f5e0db60134a03e6cb8d9d9238f2852f11f3
陈一枭 <cyx@thisky.com>
2023-06-29 14:36:14 +0800

    【调整】隐藏工作台顶部的搜索框

--- vite/packages/table/components/TopPanel.vue
+++ vite/packages/table/components/TopPanel.vue
@@ -13,10 +13,10 @@
     </a-col>
     <a-col :span="8" style="text-align: center">
       <!-- 快速搜索 头部 搜索栏 -->
-      <div @click="openGlobalSearch" class="input-box no-drag pointer"
-           style=" background: var( --primary-bg); color: var(--secondary-text)">
+      <div hidden="" @click="openGlobalSearch" class="input-box no-drag pointer inline-block"
+           style=" background: var( --primary-bg); color: var(--secondary-text);width: 40px">
         <Icon icon="sousuo"></Icon>
-        搜索 网页内容、标签
+<!--        搜索 网页内容、标签-->
         <!--        <span style="float: right; padding-right: 10px">-->
         <!--          <span style="margin-right: 3px" class="key-button">Alt</span-->
         <!--          ><span class="key-button">F</span>-->
