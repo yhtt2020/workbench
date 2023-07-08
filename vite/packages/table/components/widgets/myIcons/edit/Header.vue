b594ed3ccae49d15412bf510bedb53624de3315b
xiaoyang <1611037570@qq.com>
2023-07-08 14:18:00 +0800

    【新增】编辑切换调整效果功能

---
+++ vite/packages/table/components/widgets/myIcons/edit/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <Tab
+    class="h-12 my-5"
+    boxClass="p-1 xt-bg-2"
+    v-model:data="selectTab"
+    v-model:list="tabList"
+  ></Tab>
+</template>
+
+<script>
+import Tab from "../../../card/components/tab/index.vue";
+export default {
+  components: { Tab },
+  data() {
+    return {
+      selectTab: "config",
+      tabList: [
+        {
+          name: "图标配置",
+          value: "config",
+        },
+        {
+          name: "图标样式",
+          value: "style",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
