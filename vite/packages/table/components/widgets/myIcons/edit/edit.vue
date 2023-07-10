48978239dd08511f23825812e861c004b324bd70
xiaoyang <1611037570@qq.com>
2023-07-10 10:54:49 +0800

    【新增】新版编辑功能入口

---
+++ vite/packages/table/components/widgets/myIcons/edit/edit.vue
@@ -0,0 +1,48 @@
+<template>
+  <Header v-model:data="selectTab"></Header>
+  <template v-if="selectTab == 'config'">
+    <Size></Size>
+    <Name></Name>
+    <Open></Open>
+    <ImgUpload></ImgUpload>
+  </template>
+  <template v-else>
+    <ImgRadius></ImgRadius>
+    <ImgShape></ImgShape>
+    <ImgState></ImgState>
+    <BgColor></BgColor>
+  </template>
+</template>
+
+<script>
+import Header from "../edit/modules/Header.vue";
+import Size from "../edit/modules/Size.vue";
+import Open from "../edit/modules/Open.vue";
+import Name from "../edit/modules/Name.vue";
+import ImgUpload from "../edit/modules/ImgUpload.vue";
+import ImgState from "../edit/modules/ImgState.vue";
+import ImgRadius from "../edit/modules/ImgRadius.vue";
+import BgColor from "../edit/modules/BgColor.vue";
+import ImgShape from "../edit/modules/ImgShape.vue";
+
+export default {
+  components: {
+    Header,
+    Size,
+    Open,
+    Name,
+    ImgUpload,
+    ImgState,
+    ImgRadius,
+    BgColor,
+    ImgShape,
+  },
+  data() {
+    return {
+      selectTab: "config",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
