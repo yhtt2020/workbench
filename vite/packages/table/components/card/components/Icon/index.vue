b46d64ed84b253071e34f2fd284ca0fa17308a6b
xiaoyang <1611037570@qq.com>
2023-07-12 19:33:29 +0800

    【优化】替换原版公共组件

---
+++ vite/packages/table/components/card/components/Icon/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="typeBgClass" class="w-12 h-12 xt-active">
+    <Icon :icon="icon" :style="fontSizeStyle"></Icon>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "XtIcon",
+  computed: {
+    fontSizeStyle() {
+      return {
+        "font-size": this.size + "px",
+      };
+    },
+    typeBgClass() {
+      if (this.type === "default") {
+        return "xt-btn";
+      } else if (this.type === "active") {
+        return "xt-active-btn";
+      } else {
+        return "";
+      }
+    },
+  },
+  props: {
+    type: {
+      type: String,
+      default: "default",
+    },
+    icon: {
+      type: String,
+    },
+    size: {
+      type: Number,
+      default: 26,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
