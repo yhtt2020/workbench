d93f6c970c2b9a02dea53098b8591a1602aaad99
xiaoyang <1611037570@qq.com>
2023-07-25 17:12:17 +0800

    【优化】解决之前剩下的问题

--- vite/packages/toolbox/store/main.ts
+++ vite/packages/toolbox/store/main.ts
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 
 // 工具箱主要的全局状态存储在这里
-export const useMainStore = defineStore("main", {
+export const main = defineStore("main", {
   state: () => ({
     count: 0,
     useTool: "",
