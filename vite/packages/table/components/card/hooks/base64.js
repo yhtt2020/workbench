76893f0221eef2e75b3a23893c5812216611c063
xiaoyang <1611037570@qq.com>
2023-07-25 14:33:14 +0800

    【优化】图标base64转图片保存

--- vite/packages/table/components/card/hooks/base64.js
+++ vite/packages/table/components/card/hooks/base64.js
@@ -1,5 +1,5 @@
 const { fs } = window.$models;
-import { myIcons } from "../../../../../store/myIcons";
+import { myIcons } from "../../../store/myIcons";
 
 import { SHA256 } from "crypto-js";
 const { join } = require("path");
@@ -24,39 +24,38 @@ const saveFile = async (filePath, file) => {
 };
 const saveBase64AsImage = async (filePath, base64) => {
   let file = Buffer.from(await dataURLToBlob(base64).arrayBuffer());
-  let fileDir = await fileDirs();
-  fs.pathExists(fileDir, (err, exists) => {
+  let fileDir = await getFileDir();
+  try {
+    let exists = await fs.pathExists(fileDir);
     if (exists) {
-      // console.log("1 :>> ", 1);
-      saveFile(filePath, file);
+      // console.log("文件夹存在 :>> ");
+      await saveFile(filePath, file);
     } else {
-      fs.mkdirp(fileDir, (err) => {
-        // console.log("1 :>> ", 2);
-
-        if (err) throw err;
-        saveFile(filePath, file);
-      });
+      // console.log("创建文件夹 :>> ");
+      await fs.mkdirp(fileDir);
+      await saveFile(filePath, file);
     }
-  });
+  } catch (err) {
+    console.error(`Error saving file: ${err}`);
+  }
 };
 
 // 用户路径拼接
-const fileDirs = () => {
+const getFileDir = () => {
   let fileDir = window.globalArgs["user-data-path"];
   fileDir = join(fileDir, "temporaryIcon");
 
   return fileDir;
 };
 export const useBase64AsImage = async (base64) => {
-  let fileDir = await fileDirs();
+  let fileDir = await getFileDir();
   let name = Date.now() + ".png";
   const fileSavePath = join(fileDir, name);
   let fileUsePath = join("file://", fileDir, name);
-  // 拿到哈希去匹配
+  // 匹配
   let sha256Hash = SHA256(base64).toString();
   const store = myIcons();
   const temporaryIcon = store.temporaryIcon;
-
   if (temporaryIcon.length != 0) {
     let file = temporaryIcon.find((item) => {
       return item.hash == sha256Hash;
