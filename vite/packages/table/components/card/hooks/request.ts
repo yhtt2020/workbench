37464a1345171fcece510425cf694062fdffaa89
xiaoyang <1611037570@qq.com>
2023-07-26 08:50:42 +0800

    【新增】统一api请求

---
+++ vite/packages/table/components/card/hooks/request.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+// import {Server} from "../../../../../../../../src/util/"
+// import {Server} from "../../../../../../../../src/util/"
+// C:\Users\16110\Desktop\demo1 (2)\browser\vite\packages\table\consts.ts
+import { Server } from "../../../consts";
+console.log("object :>> ", Server);
+const service = axios.create({
+  baseURL: "https://wad.apps.vip/api", //请求的地址
+  timeout: 5000, //访问超时的时间
+});
+
+// 响应拦截器
+service.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+export default service;
--- vite/packages/toolbox/views/tools/translate/index.vue
+++ vite/packages/toolbox/views/tools/translate/index.vue
@@ -18,18 +18,18 @@
 // C:\Users\16110\Desktop\demo1 (2)\browser\vite\packages\table\consts.ts
 // C:\Users\16110\Desktop\demo1 (2)\browser\src\util\axios.js
 import { sUrl } from "../../../../table/consts";
-// import {service} from "../../../../"
+import service from "../../../../table/components/card/hooks/request";
 const translateApi = sUrl("/app/translate");
 // import service from "@/request";
 import { getTranslate } from "./api/api";
-console.log('object :>> ', service);
+console.log("object :>> ", service);
 export default {
   async mounted() {
-    let res = await getTranslate({
-      toLang: "zh",
-      queryStr: "like",
-    });
-    console.log("res :>> ", res);
+    // let res = await getTranslate({
+    //   toLang: "zh",
+    //   queryStr: "like",
+    // });
+    // console.log("res :>> ", res);
   },
 };
 </script>
