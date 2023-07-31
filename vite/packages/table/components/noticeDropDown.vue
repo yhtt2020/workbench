e498787e4a335da722f31c3b336022cf0af3b29e
wjl <3123911548@qq.com>
2023-07-31 14:50:36 +0800

    【新增】消息通知面板右侧更多逻辑交互

--- vite/packages/table/components/noticeDropDown.vue
+++ vite/packages/table/components/noticeDropDown.vue
@@ -5,7 +5,7 @@
     </div>
     <template #overlay>
       <a-menu class="custom-dropdown-menu flex-col flex items-center justify-center" style="background: var(--secondary-bg);">
-        <a-menu-item style="color: var(--secondary-text);" v-for="(item,index) in dropList" @click="">
+        <a-menu-item style="color: var(--secondary-text);" class="rounded-lg" :class="{select:dropDownIndex === index}" v-for="(item,index) in dropList" @click="selectMenuItem(item,index)">
           {{ item.title }}
         </a-menu-item>
       </a-menu>
@@ -14,14 +14,29 @@
 </template>
 
 <script>
+import { mapActions } from 'pinia'
+import { noticeStore } from '../store/notice'
 export default {
+  props:{
+    select:{
+      type:Number
+    }
+  },
   data(){
     return{
-      dropList:[{title:'打开应用'},{title:'全部清理'}],
+      dropList:[{title:'打开应用',name:'open'},{title:'全部清理',name:'clear'}],
+      dropDownIndex:0,
+
     }
   },
   methods:{
-
+    ...mapActions(noticeStore,['delAllNotification']),
+    selectMenuItem(item,index){
+      this.dropDownIndex = index
+      if(item.name === 'clear'){
+        this.delAllNotification(this.select)
+      }
+    }
   }
 }
 </script>
@@ -32,5 +47,10 @@ export default {
   background-color:var(--secondary-bg) !important;
   box-shadow: 0px 0px 20px 0px rgba(0,0,0,0.5);
   width: 120px;
+}
+
+:deep(.select){
+  background: var(--active-bg);
+  color: var(--active-text) !important;
 }
 </style>
