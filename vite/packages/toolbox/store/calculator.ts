022cdc686b21ab4eb1401d6b639c6192b6d651ec
xiaoyang <1611037570@qq.com>
2023-08-02 14:48:48 +0800

    【新增】重新上传计算器

---
+++ vite/packages/toolbox/store/calculator.ts
@@ -0,0 +1,20 @@
+import { defineStore } from "pinia";
+// @ts-ignore
+export const calculator = defineStore("calculator", {
+  state: () => ({
+    computeList: [""],
+    countList: [""],
+    calculators: 1,
+    selectIndex: 0,
+  }),
+  actions: {},
+  persist: {
+    enabled: true,
+    strategies: [
+      {
+        paths: ["computeList", "countList", "calculators"],
+        storage: localStorage,
+      },
+    ],
+  },
+});
