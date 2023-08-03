e245c0ef0e9036f505b469fdbe5ee8bfc9b74300
xiaoyang <1611037570@qq.com>
2023-08-03 17:00:28 +0800

    【新增】工具箱 颜色转换

--- vite/packages/table/store/dbStorage.ts
+++ vite/packages/table/store/dbStorage.ts
@@ -1,39 +1,46 @@
-import {rejects} from "assert";
+import { rejects } from "assert";
 
-const {storageModel} = window.$models
-function getSign():string{
-  let href=window.location.href
-  if(href.startsWith('tsbapp://./')){
-    return 'table.com'
-  }else if(href.startsWith('http://localhost:1600')){
-    return 'table.com'
-  }else{
-    let url=new URL(href)
-    return url.hostname
-  }
+const userAgent = window.navigator.userAgent;
+const isXT = userAgent.indexOf("想天工作台") > -1;
+let model: any = "";
+if (isXT) {
+  const { storageModel } = window.$models;
+  model = storageModel;
+} else {
+  model = localStorage;
 }
 
+function getSign(): string {
+  let href = window.location.href;
+  if (href.startsWith("tsbapp://./")) {
+    return "table.com";
+  } else if (href.startsWith("http://localhost:1600")) {
+    return "table.com";
+  } else {
+    let url = new URL(href);
+    return url.hostname;
+  }
+}
 
 const dbStorage: Storage = {
   async setItem(key: string, value: string) {
-    await storageModel.setItem(key, value, getSign())
+    await model.setItem(key, value, getSign());
   },
   async getItem(key: string): string | null | Promise<string> {
-    let value = await storageModel.getItem(key, getSign())
+    let value = await model.getItem(key, getSign());
     if (!value) {
-      try{
-        value= localStorage.getItem(key)
-      }catch (e) {
-        console.warn('本地也没存储')
+      try {
+        value = localStorage.getItem(key);
+      } catch (e) {
+        console.warn("本地也没存储");
       }
     }
-    if(!window.loadedStore){
-      window.loadedStore={}
+    if (!window.loadedStore) {
+      window.loadedStore = {};
     }
-    window.loadedStore[key]=true
-    return value
-  }
-}
+    window.loadedStore[key] = true;
+    return value;
+  },
+};
 
-
-export default dbStorage
+export default dbStorage;
