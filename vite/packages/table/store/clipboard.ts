acc0a9e28ede0f16c6546087719a3428747918aa
陈一枭 <cyx@thisky.com>
2023-08-16 19:19:47 +0800

    【调整】调整剪切板监听实现

--- vite/packages/table/store/clipboard.ts
+++ vite/packages/table/store/clipboard.ts
@@ -1,39 +1,79 @@
 import {defineStore} from "pinia";
-import {ClipboardObserver} from '../js/common/clipboardObserver'
 import dbStorage from "./dbStorage";
-
+//todo 此处要兼容web版
+const {win32} = window.$models
+let clipboardChanged = null
+win32.watchClipboard(() => {
+  if (clipboardChanged) {
+    clipboardChanged()
+  }
+})
+const clipboard = require('electron').clipboard
 // @ts-ignore
 export const clipboardStore = defineStore("clipboardStore", {
   state: () => ({
     enable: false,
-    previewShow:false,    // 控制预览显示
-    clipSetShow:true, // 是否打开代码高亮
-    showLineNumber:true, // 是否显示行号
-    clipSize:4,
-    clipMode:'javascript',  // 存储代码块语言包 默认js
-    clipTheme:'dracula',      // 存储代码块的主题颜色 默认monokai
+    previewShow: false,    // 控制预览显示
+    clipSetShow: true, // 是否打开代码高亮
+    showLineNumber: true, // 是否显示行号
+    clipSize: 4,
+    clipMode: 'javascript',  // 存储代码块语言包 默认js
+    clipTheme: 'dracula',      // 存储代码块的主题颜色 默认monokai
     items: [],
     clipboardObserver: {},
     settings: {
       duration: 500//ms
-    }
+    },
   }),
   actions: {
     prepare() {
-      console.log('准备')
-      this.clipboardObserver = new ClipboardObserver({
-        duration: this.settings.duration,
-        textChange: this.textChange,
-        imageChange: this.imageChange
-      })
+      clipboardChanged = this.changed
+    },
+    changed() {
+      console.log('剪切板内容变化')
+      const availableFormats = clipboard.availableFormats()
+      const formats = [
+        'image/png',
+        'text/uri-list',
+        'text/plain',
+        'text/html',
+        'text/rtf'
+      ]
+      if (availableFormats.some((content) => {
+        return content.includes('image')
+      })) {
+        //是图片
+        console.log('剪切板图片变化')
+        this.imageChange(clipboard.readImage())
+        return
+      }
+
+      if (availableFormats.some((content) => {
+
+        return content.includes('text/uri-list')
+      })) {
+        console.log('剪切板uri变化')
+        //是文件
+        return
+      }
+      if (availableFormats.some((content) => {
+
+        return content.includes('text/plain')
+      })) {
+        //纯文字
+        console.log('剪切板文本变化')
+        this.textChange(clipboard.readText())
+      }
+
+
     },
     start() {
-      this.clipboardObserver.start()
+       clipboardChanged=this.changed
     },
     stop() {
-      this.clipboardObserver.stop()
+      clipboardChanged=null
     },
-    isRunning(){
+    isRunning() {
       return this.clipboardObserver.isRunning()
     },
     textChange(text: string, beforeText: string) {
@@ -45,36 +85,37 @@ export const clipboardStore = defineStore("clipboardStore", {
       this.items.unshift(item)
       console.log('监测到新[文本]剪切板内容', text)
     },
-    imageChange(image: any, beforeImage: any) {
+    imageChange(image: any) {
       let item = {
         type: 'image',
         content: image,
-        time: Date.now()
+        time: Date.now(),
+
       }
       //this.items.unshift(item)
       console.log('监测到[图片]新剪切板内容', image)
     },
-    changeClipMode(code:string){
+    changeClipMode(code: string) {
       this.clipMode = code
       // console.log('语言包替换成功',this.clipMode);
     },
-    isOpenPreview(val:boolean){
+    isOpenPreview(val: boolean) {
       this.previewShow = val
       // console.log('打开预览',this.previewShow);
     },
-    isClipLineNumber(val:boolean){
+    isClipLineNumber(val: boolean) {
       this.showLineNumber = val
       // console.log('显示行号',this.showLineNumber);
     },
-    isSetCodeHighlight(val:boolean){
+    isSetCodeHighlight(val: boolean) {
       this.clipSetShow = val
       // console.log('设置代码高亮',val);
     },
-    updateClipSize(val:number){
-      console.log('修改代码缩进',val);
+    updateClipSize(val: number) {
+      console.log('修改代码缩进', val);
       this.clipSize = val
     },
-    updateTheme(val:string){
+    updateTheme(val: string) {
       // console.log('修改主题色',val);
       this.clipTheme = val
     }
