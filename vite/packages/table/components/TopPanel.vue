b65964da4448237dbb13fc3ce5110b530731ebce
wjl <3123911548@qq.com>
2023-08-21 09:40:08 +0800

    【补充】判断用户是否进入聊天界面，如果进入就不显示消息提示弹窗和消息入口的逻辑

--- vite/packages/table/components/TopPanel.vue
+++ vite/packages/table/components/TopPanel.vue
@@ -28,13 +28,14 @@
       </div>
     </div>
     <div class="flex flex-1 items-end justify-end align-items-end xt-text ">
-      <div  v-if="noticeSettings.show"  class="no-drag  flex items-center pointer" @click="messageAlert" style="color: var(--primary-text);">
+      <div  v-if="noticeSettings.show && hasChat"  class="no-drag  flex items-center pointer" @click="messageAlert" style="color: var(--primary-text);">
         <div class=" flex items-center notification justify-center" style="width: 20px;height: 20px;position: relative;">
           <img src="/icons/logo128.png" class="w-full h-full object-cover">
           <div class="new-message-tag"></div>
         </div>
         <div class="primary-title pointer pl-1" style="color: var(--primary-text);">新消息</div>
         <a-divider type="vertical" style="height: 18px;width: 1px; background: var(--primary-text);opacity: 0.2 "/>
+
       </div>
 
       <div class="mr-2"
@@ -189,6 +190,10 @@ export default {
     hasWeather () {
       return this.cities.length > 0
     },
+
+    hasChat(){
+      return this.$route.path !== '/chat'
+    }
   },
   async mounted () {
     window.onblur = () => {
--- vite/packages/table/js/common/sessionNotice.ts
+++ vite/packages/table/js/common/sessionNotice.ts
@@ -80,118 +80,123 @@ export class Notifications{
   // 接收IM消息通知方法 
   public async receiveNotification(notification: Notification) {
     noticeStore().showNoticeEntry()
-    
-    // 判断是否为群聊,如果是群聊,根据群聊id获取群聊头像
-    if(notification.data[0].conversationType === 'GROUP'){
-      const res = await window.$chat.getGroupProfile({ groupID:notification.data[0].to })
-      this.groupAvatar = res.data.group.avatar
-    }
-    
 
-    if(notification.data[0].payload.hasOwnProperty('text')){  // 判断payload.text是否存在
-      if(noticeStore().$state.noticeSettings.enablePlay){  // 判断提示语开关是否打开
-        noticeStore().setMessagePlay()  // 打开消息提示语
-      }else{
-        return;
+    if(router.options.history.state.current !== '/chat'){ // 判断是否在聊天页面
+
+      // 判断是否为群聊,如果是群聊,根据群聊id获取群聊头像
+      if(notification.data[0].conversationType === 'GROUP'){
+       const res = await window.$chat.getGroupProfile({ groupID:notification.data[0].to })
+       this.groupAvatar = res.data.group.avatar
       }
 
-      const friendTitle = notification.data[0].nick ? `${notification.data[0].nick}` : ''  // 好友名称标题
-      const groupTitle = await this.getGroupName(notification.data[0]) !== '' ?  `${await this.getGroupName(notification.data[0])}` : '' // 群组名称标题
-     
-      const msg = {  // 定义临时变量将数据进行存储
-        title:notification.data[0].conversationType === 'C2C' ? friendTitle : groupTitle,
-        icon:notification.data[0].conversationType === 'C2C' ?  notification.data[0].avatar : this.groupAvatar,
-        body:notification.data[0].payload.text,
-        time:notification.data[0].time,
-      }
-
-      if(noticeStore().$state.noticeSettings.enable){ // 判断是否开启消息弹窗
-        toast.info(  // 显示通知弹窗
-        {
-          component:NoticeToastButton,props:{message:msg,messageType:'message'},
-          listeners:{
-            'nowCheck':function(){  // 立即查看 跳转指定会话
-              router.push({name:'chat'})
-              const name = `${notification.data[0].conversationID}`
-              window.$TUIKit.TUIServer.TUIConversation.getConversationProfile(name).then((imResponse) => {
-                // 通知 TUIConversation 添加当前会话
-                // Notify TUIConversation to toggle the current conversation
-                
-                window.$TUIKit.TUIServer.TUIConversation.handleCurrentConversation(imResponse.data.conversation);
-              })
-              noticeStore().hideNoticeEntry()  
-              noticeStore().putIMChatData(msg,'message')
-            }
-          }
-        },
-        {
-          icon:false,closeOnClick:false, closeButton:false,pauseOnFocusLoss:true, 
-          pauseOnHover:true,timeout: 5000,toastClassName:'notice-toast'
+      if(notification.data[0].payload.hasOwnProperty('text')){  // 判断payload.text是否存在
+        if(noticeStore().$state.noticeSettings.enablePlay){  // 判断提示语开关是否打开
+          noticeStore().setMessagePlay()  // 打开消息提示语
+        }else{
+          return;
         }
-        )
+  
+        const friendTitle = notification.data[0].nick ? `${notification.data[0].nick}` : ''  // 好友名称标题
+        const groupTitle = await this.getGroupName(notification.data[0]) !== '' ?  `${await this.getGroupName(notification.data[0])}` : '' // 群组名称标题
+       
+        const msg = {  // 定义临时变量将数据进行存储
+          title:notification.data[0].conversationType === 'C2C' ? friendTitle : groupTitle,
+          icon:notification.data[0].conversationType === 'C2C' ?  notification.data[0].avatar : this.groupAvatar,
+          body:notification.data[0].payload.text,
+          time:notification.data[0].time,
+        }
+  
+        if(noticeStore().$state.noticeSettings.enable){ // 判断是否开启消息弹窗
+          toast.info(  // 显示通知弹窗
+          {
+            component:NoticeToastButton,props:{message:msg,messageType:'message'},
+            listeners:{
+              'nowCheck':function(){  // 立即查看 跳转指定会话
+                router.push({name:'chat'})
+                const name = `${notification.data[0].conversationID}`
+                window.$TUIKit.TUIServer.TUIConversation.getConversationProfile(name).then((imResponse) => {
+                  // 通知 TUIConversation 添加当前会话
+                  // Notify TUIConversation to toggle the current conversation
+                  
+                  window.$TUIKit.TUIServer.TUIConversation.handleCurrentConversation(imResponse.data.conversation);
+                })
+                noticeStore().hideNoticeEntry()  
+                noticeStore().putIMChatData(msg,'message')
+              }
+            }
+          },
+          {
+            icon:false,closeOnClick:false, closeButton:false,pauseOnFocusLoss:true, 
+            pauseOnHover:true,timeout: 5000,toastClassName:'notice-toast'
+          }
+          )
+        }else{
+          noticeStore().putIMChatData(msg,'message')
+        }
+  
+        this.notices.push(msg);
+        if (this.callback) {
+          this.callback(msg);
+        }
+  
       }else{
-        noticeStore().putIMChatData(msg,'message')
-      }
-
-      this.notices.push(msg);
-      if (this.callback) {
-        this.callback(msg);
-      }
-
-    }
-
-    else{
-
-      if(noticeStore().$state.noticeSettings.enablePlay){  // 判断提示语开关是否打开
-        noticeStore().setNoticePlay()  // 打开通知提示语
-      }else{
-        return;
-      }
-     
-
-      const body = await this.classNotice(notification.data[0])
-      
-      const notice = {
-        title:'系统通知',
-        body:body,
-        icon:'/icons/IM.png',
-        time:notification.data[0].time
-      }
-      
-
-      if(noticeStore().$state.noticeSettings.enable){ // 判断是否开启消息弹窗
-        toast.info(  // 显示通知弹窗
-         {
-         component:SystemNoticeToast,props:{content:notice,noticeType:'notice'},
-         listeners:{
-           'nowCheck':function(){  // 立即查看
-              router.push({name:'chat'})
-              const name = `${notification.data[0].conversationID}`;
-              window.$TUIKit.TUIServer.TUIConversation.getConversationProfile(name).then((imResponse:any) => {
-                // 通知 TUIConversation 添加当前会话
-                // Notify TUIConversation to toggle the current conversation
-                window.$TUIKit.TUIServer.TUIConversation.handleCurrentConversation(imResponse.data.conversation);
-              })
-              noticeStore().hideNoticeEntry()  
-              noticeStore().putIMChatData(notice,'Notice')
+  
+        if(noticeStore().$state.noticeSettings.enablePlay){  // 判断提示语开关是否打开
+          noticeStore().setNoticePlay()  // 打开通知提示语
+        }else{
+          return;
+        }
+       
+  
+        const body = await this.classNotice(notification.data[0])
+        
+        const notice = {
+          title:'系统通知',
+          body:body,
+          icon:'/icons/IM.png',
+          time:notification.data[0].time
+        }
+        
+  
+        if(noticeStore().$state.noticeSettings.enable){ // 判断是否开启消息弹窗
+          toast.info(  // 显示通知弹窗
+           {
+           component:SystemNoticeToast,props:{content:notice,noticeType:'notice'},
+           listeners:{
+             'nowCheck':function(){  // 立即查看
+                router.push({name:'chat'})
+                const name = `${notification.data[0].conversationID}`;
+                window.$TUIKit.TUIServer.TUIConversation.getConversationProfile(name).then((imResponse:any) => {
+                  // 通知 TUIConversation 添加当前会话
+                  // Notify TUIConversation to toggle the current conversation
+                  window.$TUIKit.TUIServer.TUIConversation.handleCurrentConversation(imResponse.data.conversation);
+                })
+                noticeStore().hideNoticeEntry()  
+                noticeStore().putIMChatData(notice,'Notice')
+             }
            }
-         }
-         },
-         {
-          icon:false,closeOnClick:false, closeButton:false,pauseOnFocusLoss:true, 
-          pauseOnHover:true,timeout: 0,toastClassName:'notice-toast'
-         }
-       )
-      }else{
-        noticeStore().putIMChatData(notice,'Notice')
+           },
+           {
+            icon:false,closeOnClick:false, closeButton:false,pauseOnFocusLoss:true, 
+            pauseOnHover:true,timeout: 0,toastClassName:'notice-toast'
+           }
+         )
+        }else{
+          noticeStore().putIMChatData(notice,'Notice')
+        }
+       
+        
+        this.notices.push(notice);
+        if (this.callback) {
+          this.callback(notice);
+        }
       }
-     
       
-      this.notices.push(notice);
-      if (this.callback) {
-        this.callback(notice);
-      }
+    }else{
+      return;
     }
+  
+   
 
   }
   
