22324047cf50a9bccd8a04879dbd2db6162f39f7
xiaoyang <1611037570@qq.com>
2023-08-23 17:29:04 +0800

    【新增】左侧菜单组件 和 下拉选择组件

--- vite/packages/table/ui/components/LeftMenu/index.vue
+++ vite/packages/table/ui/components/LeftMenu/index.vue
@@ -1,51 +1,120 @@
 <template>
-  <div>
-    <div class="flex flex-col items-center h-full" style="width: 60px">
+  <!-- 动态头部 -->
+  <div class="flex flex-col items-center h-full xt-br mr-3" style="width: 70px">
+    <xt-icon
+      class="mb-3"
+      v-for="item in list.slice(0, last)"
+      v-bind="config"
+      :icon="item.icon"
+      @click="iconClick(item)"
+      :class="{
+        box: item.icon == currentIndex,
+      }"
+    ></xt-icon>
+
+    <!-- 动态适中 -->
+    <div
+      class="xt-scrollbar   xt-container  h-full xt-bt py-3 xt-bm flex flex-col items-center"
+    >
       <xt-icon
-        v-bind="modelValue[0]"
-        w="40"
-        size="18"
-        @click="iconClick(0)"
-        class="xt-b"
+        v-for="item in list.slice(last, -1 * end)"
+        v-bind="config"
+        class="mb-3"
+        :icon="item.icon"
+        @click="iconClick(item)"
+        :class="{
+          box: item.icon == currentIndex,
+        }"
       ></xt-icon>
-      <div class="my-3 xt-bm w-full"></div>
-      <div class="xt-scrollbar h-full w-full"></div>
-      <div>
-        <div
-          v-for="item in modelValue.slice(-1 * end)"
-          v-bind="modelValue[0]"
-          w="40"
-          size="18"
-          @click="iconClick(0)"
-          class="xt-b"
-        >
-          {{ item }} , {{ end * -1 }}
-        </div>
-      </div>
     </div>
+    <!-- 底部循环 -->
+    <xt-icon
+    class="mt-3"
+      v-for="item in list.slice(-1 * end)"
+      v-bind="config"
+      :icon="item.icon"
+      @click="iconClick(item, true)"
+    >
+    </xt-icon>
   </div>
 </template>
 
 <script setup>
-const end = 2;
-const modelValue = [
-  {
-    icon: "message",
-  },
+import { ref } from "vue";
 
-  {
-    icon: "message",
+const props = defineProps({
+  config: {
+    default: () => {
+      return {
+        w: "40",
+        size: "18",
+      };
+    },
   },
-  {
-    icon: "message",
+  last: {
+    default: 1,
   },
-  {
-    icon: "message",
+  end: {
+    default: 1,
   },
-];
-const iconClick = (item) => {
-  console.log("item :>> ", item);
+  modelValue: {},
+  list: {
+    default: () => {
+      return [
+        {
+          icon: "message",
+        },
+        {
+          icon: "star",
+        },
+        {
+          icon: "smile",
+        },
+        {
+          icon: "aixin",
+        },
+        {
+          icon: "yanjing",
+        },
+
+        // ----------
+        {
+          icon: "smile",
+        },
+        {
+          icon: "aixin",
+        },
+        {
+          icon: "yanjing",
+        },
+        {
+          icon: "smile",
+        },
+        {
+          icon: "aixin",
+        },
+        {
+          icon: "yanjing",
+        },
+        // -----------
+        {
+          icon: "shezhi1",
+        },
+      ];
+    },
+  },
+});
+const currentIndex = ref(props.list[0].icon);
+
+const iconClick = (item, flag) => {
+  if (!flag) currentIndex.value = item.icon;
+  item.callBack && item.callBack(item);
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.box {
+  border: 4px solid var(--active-bg) !important;
+  border-radius: 12px;
+}
+</style>
