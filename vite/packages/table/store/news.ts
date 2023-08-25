045dd1d3116109be91002556ed8dea6f4066bb4a
陈一枭 <cyx@thisky.com>
2023-08-25 19:46:41 +0800

    【新增】对接上服务器端api，替换掉聚合api

--- vite/packages/table/store/news.ts
+++ vite/packages/table/store/news.ts
@@ -1,30 +1,40 @@
-import { defineStore } from "pinia";
+import {defineStore} from "pinia";
 import axios from "axios";
 import dbStorage from "./dbStorage";
+import {sUrl} from "../consts";
+import {get} from "../js/axios/request";
 
+const juheGet = sUrl('/app/juhe/get')
+
+// @ts-ignore
 export const newsStore = defineStore("news", {
   state: () => ({
     NewsMsgList: [],
   }),
   actions: {
     async getNewsMsg(tag, num) {
-        let response = await axios.get("http://v.juhe.cn/toutiao/index", {
-          params: {
-            type: tag,
-            page_size: num,
-            key: "2ce91c43301c9528f2f9d1cd6f310e2b",
-          },
-        });
-        // console.log(response);
-
-        if(response.status==200){
-          this.NewsMsgList=response.data.result.data
-          console.log(this.NewsMsgList);
-          
+      let response = await get(juheGet, {
+        apiName: 'toutiao.index',
+        params: {
+          type: tag,
+          page_size: num,
+        },
+        options: {
+          key: 'toutiao.'+tag,
+          ttl: 300,
+          cache: 1
         }
-      
+      })
+
+      console.log(response, '接口返回')
+      if(response.status){
+        this.NewsMsgList = response.data
+      }
+      else{
+        return false
+      }
     },
-    
+
   },
   persist: {
     enabled: true,
