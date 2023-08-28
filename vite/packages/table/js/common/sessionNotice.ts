518d8270d853051d67f3198ae46364e4602a30aa
wjl <3123911548@qq.com>
2023-08-28 16:12:14 +0800

    【优化】根据新修改的需要将消息通知进行区分

--- vite/packages/table/js/common/sessionNotice.ts
+++ vite/packages/table/js/common/sessionNotice.ts
@@ -7,104 +7,37 @@ import SystemNoticeToast from '../../components/notice/systemNoticeToast.vue'
 
 const toast = useToast()
 
-export class Notifications {
-  /**
-  * @param notices 接收消息通知内容
-  * **/
+export class Notifications{
 
-
-  private notices: Notification[] = [];
-  private callback?: (notification: Notification) => void;
-  constructor() {}
-
-
-  // 根据不同系统通知获取不同通知内容
-  private async classNotice(msg: any) {
-    const data = msg.data[0].payload
-    const groupName = data.groupProfile?.name || data.groupProfile?.groupID;
-    const userName = await this.getUserProfile(msg);
-    let info: any; // 接收不同系统通知消息
-    switch (data.operationType) {
-      case 1:
-        if ( data.handleMessage === "加群" && data.hasOwnProperty("handleMessage")) {
-          info = `${userName}申请加入群组:${groupName}`;
-        } else {
-          info = `${userName}接受${msg.nick}加入${groupName}群组`;
+  // 系统toast
+  private systemToast(msg:any,conversationID:any){
+    toast.info(
+      {
+        component:SystemNoticeToast,props:{content:msg,noticeType:'notice',isPlay:noticeStore().$state.noticeSettings.noticePlay},
+        listeners:{
+          'nowCheck':function(){
+            noticeStore().hideNoticeEntry()  
+            noticeStore().putIMChatData(msg,'Notice')
+          },
+          'systemExamine':function(){
+            router.push({name:'chat'})
+            window.$TUIKit.TUIServer.TUIConversation.getConversationProfile(conversationID).then((imResponse:any) => {
+              // 通知 TUIConversation 添加当前会话
+              // Notify TUIConversation to toggle the current conversation
+              window.$TUIKit.TUIServer.TUIConversation.handleCurrentConversation(imResponse.data.conversation);
+            })
+          }
         }
-        break;
-      case 2:
-        info = `成功加入群组：${groupName}`;
-        break;
-      case 3:
-        info = `申请加入群组:${groupName}被拒绝`;
-        return info;
-      case 4:
-        info = `你被管理员${userName}踢出群组：${groupName}`;
-        break;
-      case 5:
-        info = `群：${groupName}被${userName} 解散`;
-        break;
-      case 6:
-        info = `${userName} 创建群：${groupName}`;
-        break;
-      case 8:
-        info = `你退出群组：${groupName}`;
-        break;
-      case 9:
-        info = `你被${userName} 设置为群：${groupName} 的管理员`;
-        break;
-      case 10:
-        info = `你被${userName} 撤销群：${groupName} 的管理员身份`;
-        break;
-      case 12:
-        info`${userName} 邀请你加群：${groupName}`;
-        break;
-      case 13:
-        info = `${userName} 同意加群：${groupName}`;
-        break;
-      case 14:
-        info = `${userName} 拒接加群：${groupName}`;
-        break;
-      case 255:
-        info = `自定义群系统通知: ${data.userDefinedField}`;
-        break;
-      default:
-        break;
-    }
-    return info;
+      },
+      {
+        icon:false,closeOnClick:false, closeButton:false,pauseOnFocusLoss:true, 
+        pauseOnHover:true,timeout: 5000,toastClassName:'notice-toast'
+      }
+    )
   }
 
-
-  // 根据参数获取用户名称
-  private async getUserProfile(msg: any) {
-    // 根据用户名称
-    const res = await window.$chat.getUserProfile({
-      userIDList: [`${msg.data[0].payload.operatorID}`],
-    });
-    return res.data[0].nick;
-  }
-
-
-  // 获取群组名称
-  private async getGroupName(msg: any) {
-    if (msg.conversationType === "GROUP") {
-      const res = await window.$chat.getGroupProfile({ groupID: msg.to });
-      return res.data.group.name;
-    } else {
-      return "";
-    }
-  }
-
-  // 获取群组头像
-  private async getGroupAvatar(val:any){
-    if(val.data[0].conversationType === 'GROUP'){
-      const res = await window.$chat.getGroupProfile({ groupID:val.data[0].to })
-      return res.data.group.avatar
-    }
-  }
-
-  // 普通消息弹窗
-  private commonMessage(msg:any,conversationID:any){
+  // 普通toast
+  private commonToast(msg:any,conversationID:any){
     toast.info(
       {
         component:NoticeToastButton,props:{message:msg,messageType:'message',isPlay:noticeStore().$state.noticeSettings.enablePlay},
@@ -121,94 +54,205 @@ export class Notifications {
               window.$TUIKit.TUIServer.TUIConversation.handleCurrentConversation(imResponse.data.conversation);
             })
           }
-        }
+        },
       },
       {
        icon:false,closeOnClick:false, closeButton:false,pauseOnFocusLoss:true, 
-       pauseOnHover:true,timeout: 5000,toastClassName:'notice-toast'
+       pauseOnHover:true,timeout: 5000,toastClassName:'notice-toast',
+       onClose() {
+        noticeStore().putIMChatData(msg,'message')
+       },
       }
     )
-    this.notices.push(msg);
-    if (this.callback) {
-      this.callback(msg)
+  }
+
+  // 获取名称
+  private async getName(msg:any){
+    if (msg.conversationType === "GROUP") {
+      const res = await window.$chat.getGroupProfile({ groupID: msg.to });
+      return res.data.group.name;
+    } else {
+      return "";
     }
   }
 
-  // 系统通知弹窗
-  private systemNotice(notice:any,conversationID:any){
-    toast.info(
-      {
-        component:SystemNoticeToast,props:{content:notice,noticeType:'notice',isPlay:noticeStore().$state.noticeSettings.noticePlay},
-        listeners:{
-          'nowCheck':function(){
-            noticeStore().hideNoticeEntry()  
-            noticeStore().putIMChatData(notice,'Notice')
-          },
-          'systemExamine':function(){
-            router.push({name:'chat'})
-            window.$TUIKit.TUIServer.TUIConversation.getConversationProfile(conversationID).then((imResponse:any) => {
-              // 通知 TUIConversation 添加当前会话
-              // Notify TUIConversation to toggle the current conversation
-              window.$TUIKit.TUIServer.TUIConversation.handleCurrentConversation(imResponse.data.conversation);
-            })
-          }
+  // 获取群组头像
+  private async getGroupAvatar(val:any){
+    if(val.data[0].conversationType === 'GROUP'){
+      const res = await window.$chat.getGroupProfile({ groupID:val.data[0].to })
+      return res.data.group.avatar
+    }
+  }
+  
+  // 根据参数获取用户名称
+  private async getUserProfile(msg: any) {
+    // 根据用户名称
+    const res = await window.$chat.getUserProfile({
+      userIDList: [`${msg.operatorID}`],
+    });
+    return res.data[0].nick;
+  }
+
+  // 获取系统通知内容
+  private async getSystemBody(msg:any){
+    const data = msg.payload
+    const groupName = data.groupProfile?.name || data.groupProfile?.groupID;
+    const userName = await this.getUserProfile(data);
+    let info: any; // 接收不同系统通知消息
+   
+    console.log(data.operationType);
+    
+    switch (data.operationType) {
+      case 1:
+        if (msg.handleMessage === "加群" && msg.hasOwnProperty("handleMessage")) {
+          info = `${userName}申请加入群组:${groupName}`;
+        } else {
+          info = `${userName}接受${msg.nick}加入${groupName}群组`;
         }
-      },
-      {
-        icon:false,closeOnClick:false, closeButton:false,pauseOnFocusLoss:true, 
-        pauseOnHover:true,timeout: 0,toastClassName:'notice-toast'
-      }
-    )
-    this.notices.push(notice);
-    if (this.callback) {
-      this.callback(notice);
+        break;
+      case 2:
+        info = `成功加入群组：${groupName}`;
+        break;
+      case 3:
+        info = `申请加入群组:${groupName}被拒绝`;
+        break;
+      case 4:
+        if(data.hasOwnProperty('memberList') && data.memberList.length !== 0 && data.memberList[0].role === 300){
+          info = `${data.memberList[0].nick}被${userName} 设置为群：${groupName} 的管理员`;
+        }else{
+          info = `你${msg.nick}被管理员${userName}踢出群组：${groupName}`
+        }
+        break;
+      case 5:
+        if(data.hasOwnProperty('memberList') && data.memberList.length !== 0 && data.memberList[0].role === 200){
+          info = `${data.memberList[0].nick}被${userName} 撤销群：${groupName} 的管理员身份`; 
+        }else{
+          info = `群：${groupName}被${userName} 解散`;
+        }
+        break;
+      case 6:
+        info = `${userName} 创建群：${groupName}`;
+        break;
+      case 8:
+        info = `你退出群组：${groupName}`;
+        break;
+      case 9:
+        info = '';
+        // info = `你被${userName} 设置为群：${groupName} 的管理员`;
+        break;
+      case 10:
+        info = '';
+        // info = `你被${userName} 撤销群：${groupName} 的管理员身份`;
+        break;
+      case 12:
+        info`${userName} 邀请你加群：${groupName}`;
+        break;
+      case 13:
+        info = `${userName} 同意加群：${groupName}`;
+        break;
+      case 14:
+        info = `${userName} 拒接加群：${groupName}`;
+        break;
+      case 20:
+        info = ''
+        break;
+      case 255:
+        info = `自定义群系统通知: ${data.userDefinedField}`;
+        break;
     }
+    
+    return info
   }
 
 
-
-
-  // 接收IM聊天消息通知方法
-  public async receiveNotification(notification: Notification) {
-    // 显示消息入口
-    noticeStore().showNoticeEntry()
+  // 接收IM聊天消息通知公共方法
+  public async receiveNotification(notification:any){
+    // 创建一个audio标签
+    function createOrUpdateAudioElement(): HTMLAudioElement {
+      let audioElement = document.getElementById('messageAudio') as HTMLAudioElement;
+      
+      if (!audioElement) {
+        audioElement = document.createElement('audio');
+        audioElement.src = '/sound/message.mp3'
+        audioElement.setAttribute('id', 'messageAudio');
+        document.body.appendChild(audioElement);
+      }
+      
+      return audioElement;
+    }
+    const audioElement: HTMLAudioElement = createOrUpdateAudioElement();
     
-
+    const data = notification.data[0]
+    const settings = {
+      isStore:Object.keys(window.$TUIKit.TUIServer.TUIChat.store.conversation).length !== 0, // 判断chat界面缓存不为空
+      isEnable:noticeStore().$state.noticeSettings.enable, // 消息开关
+      isGlobal:router.options.history.state.current !== '/chat', // 判断是否在全局页面
+      enablePlay:noticeStore().$state.noticeSettings.enablePlay, // 提示音开关
+      isCurrentChat:window.$TUIKit.TUIServer.TUIChat.store.conversation?.conversationID === data.conversationID, // 判断是否为当前会话
+      isAT:data.atUserList.length !== 0, // 判断@消息
+      noTrouble:window.$TUIKit.TUIServer.TUIChat.store.conversation?.groupProfile?.selfInfo.messageRemindType === 'AcceptAndNotify' // 是否开启免打扰
+    }
     
-    // 判断IM聊天是否切换聊天页面,
-    if(router.options.history.state.current !== '/chat' && noticeStore().$state.noticeSettings.enable){
-
-      // 判断消息是否为文本
-      const isText = notification.data[0].payload.hasOwnProperty('text')
+    // 判断消息开关和音频开关是否打开
+    if(settings.isEnable && settings.enablePlay && settings.noTrouble){
+      // 根据text判断是否为系统通知
+      const isText = data.payload.hasOwnProperty('text')
       if(isText){
         // 好友名称标题
-        const friendTitle = notification.data[0].nick ? `${notification.data[0].nick}` : '' 
+        const friendTitle = data.nick ? `${data.nick}` : ''
         // 群组名称标题
-        const groupTitle = await this.getGroupName(notification.data[0]) !== '' ?  `${await this.getGroupName(notification.data[0])}` : '' 
+        const groupTitle = await this.getName(data) !== '' ?  `${await this.getName(data)}` : '' 
+        // 获取头像
         const avatar = await this.getGroupAvatar(notification)
-        
-        // 定义临时变量将数据进行存储
-        const msg = {  
-         title:notification.data[0].conversationType === 'C2C' ? friendTitle : groupTitle,
-         icon:notification.data[0].conversationType === 'C2C' ? notification.data[0].avatar : avatar,
-         body:notification.data[0].payload.text,
-         time:notification.data[0].time,
+
+        // 全局情况下的普通消息弹窗
+        if(settings.isGlobal){
+          // 显示消息入口
+          noticeStore().showNoticeEntry()
+          const msg = {  
+            title:data.conversationType === 'C2C' ? friendTitle : groupTitle,
+            icon:data.conversationType === 'C2C' ? data.avatar : avatar,
+            body:data.payload.text,
+            time:data.time,
+          }
+          this.commonToast(msg,data.conversationID)
         }
-        this.commonMessage(msg,notification.data[0].conversationID)
-        
-      }else{
-        const body = await this.classNotice(notification)   
-        const notice = {
-          title:'系统通知',
-          body:body,
-          icon:'/icons/IM.png',
-          time:notification.data[0].time
+
+        // 非当前会话只提示音频
+        else if(settings.isStore === false && settings.isCurrentChat === false){
+          if(settings.isAT){
+            audioElement.play()
+          }else{
+            audioElement.play()
+          }
         }
-        this.systemNotice(notice,notification.data[0].conversationID)
+
+        // 当前会话没有任何提示
+        else{
+          return;
+        }        
       }
-    }else{
-      // noticeStore().putIMChatData(msg,'message')
-      // noticeStore().putIMChatData(notice,'notice')
+      // 团队系统通知
+      else if(data.payload.description !== ''){
+        const body = await this.getSystemBody(data)
+        if(body !== ''){
+          const notice = {
+            title:'团队系统通知',
+            body:body,
+            icon:'/icons/IM.png',
+            time:data.time
+          }
+          this.commonToast(notice,data.conversationID)
+        }
+      }
+    }
+    else{
+
     }
   }
+
+  
+
+  
+
 }
