b69e76072454bff04b36ae1a55a7c1b00a7f6f6f
陈一枭 <cyx@thisky.com>
2023-09-06 10:40:36 +0800

    【调整】调整弹幕功能

--- vite/packages/table/apps/barrage/page/Barrage.vue
+++ vite/packages/table/apps/barrage/page/Barrage.vue
@@ -1,16 +1,29 @@
 <script lang="ts">
 import {defineComponent} from 'vue'
 import BarragePanel from "../../../components/comp/BarragePanel.vue";
+import BarrageSetting from "../../../page/settings/BarrageSetting.vue";
 
 export default defineComponent({
   name: "Barrage",
-  components: {BarragePanel}
+  components: {BarrageSetting, BarragePanel}
 })
 </script>
 
 <template>
-  <div class="xt-bg xt-text" style="width:400px;margin:auto;height: 100%">
-    <BarragePanel   ></BarragePanel>
+
+  <div class="xt-text" style="width:800px;margin:auto;height: 100%">
+    <a-row :gutter="10" style="height: 100%">
+      <a-col style="height: 100%" :span="12" class="">
+        <BarragePanel></BarragePanel>
+      </a-col>
+      <a-col :span="1">
+        <div style="border-right: 1px solid var(--secondary-text);height:calc(100% - 40px);opacity: 0.5;width: 0;margin: auto;margin-top: 20px"></div>
+      </a-col>
+      <a-col :span="11" class="pt-2 pl-2">
+        <BarrageSetting></BarrageSetting>
+      </a-col>
+    </a-row>
+
   </div>
 
 </template>
