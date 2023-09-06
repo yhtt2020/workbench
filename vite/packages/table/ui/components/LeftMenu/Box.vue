1ffcc505ad70c6e6b0f84457536a8fc160ec59b9
xiaoyang <1611037570@qq.com>
2023-09-06 17:28:16 +0800

    【优化】适配title属性

--- vite/packages/table/ui/components/LeftMenu/Box.vue
+++ vite/packages/table/ui/components/LeftMenu/Box.vue
@@ -1,13 +1,15 @@
 <template>
   <!-- 统一选择模板 -->
-  <div
-    @click="selectClick(item.id, item.flag)"
-    class="xt-base-btn"
-    style="width: 50px; height: 50px; border-radius: 14px"
-    :style="[item.id == id ? 'border: 3px solid var(--active-bg)' : '']"
-  >
-    <slot></slot>
-  </div>
+  <a-tooltip :title="item.title" placement="right">
+    <div
+      @click="selectClick(item.id, item.flag)"
+      class="xt-base-btn"
+      style="width: 50px; height: 50px; border-radius: 14px"
+      :style="[item.id == id ? 'border: 3px solid var(--active-bg)' : '']"
+    >
+      <slot></slot>
+    </div>
+  </a-tooltip>
 </template>
 
 <script setup>
@@ -15,7 +17,7 @@ const props = defineProps({
   item: {},
   id: "",
 });
-const emits = defineEmits([ "itemClick"]);
+const emits = defineEmits(["itemClick"]);
 const selectClick = () => {
   emits("itemClick", props.item);
 };
