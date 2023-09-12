d8eaff45810d34d26129ad301fdd46f40899aa63
陈一枭 <cyx@thisky.com>
2023-09-12 21:06:48 +0800

    【新增】增加iframe打开link频道

--- vite/packages/table/js/data/chatList.js
+++ vite/packages/table/js/data/chatList.js
@@ -27,17 +27,18 @@ export const chatList = [
             role: 'channel', type: 'group', name: '直面开发团队', props: { id: 'develop_group' }
           },
           {
-            role: 'channel', type: 'link', title: '阿皮动态',
+            role: 'channel', type: 'link', name: '阿皮动态',
             props: {
               url: 'https://space.bilibili.com/167397379/video',
-              openMethod: 'userSelect'  //根据用户工作台设置
+              openMethod: 'userSelect'  //根据用户工作台设置 不填写openMethod则以iframe加载。部分网页无法正常加载。
             }
           },
           {
-            role: 'channel', type: 'link', title: 'B站视频',
+            role: 'channel', type: 'link', name: 'B站视频',
             props: {
               gossipID: '',
-              url: 'https://space.bilibili.com/167397379/dynamic'
+              url: 'https://space.bilibili.com/167397379/dynamic',
+              openMethod: 'userSelect'  //根据用户工作台设置
             }
           }
         ]
@@ -49,15 +50,20 @@ export const chatList = [
         children: [
           {
             role: 'channel', name: '新手入门', type: 'forum', props: {
-              id: ''
+              id: 3
             },
           },
           {
             role: 'channel', name: '版本更新', type: 'forum', props: {
-              id: ''
+              id: 100304
             },
           },
-          { role: 'channel', title: 'Roadmap', type: 'link', props: { url: 'http://kanban.xiangtian.ren/b/Bd6fRiZPo3RTxq9ok/board' } },
+          {
+            role: 'channel',
+            name: 'Roadmap',
+            type: 'link',
+            props: { url: 'http://kanban.xiangtian.ren/b/Bd6fRiZPo3RTxq9ok/board' }
+          },
         ]
       },
       {
@@ -65,16 +71,18 @@ export const chatList = [
         name: '交流群',
         props: {},
         children: [
-          { role: 'channel', name: '官方运营', type: 'group', props: {
-            id:'yunying'
-            } },
-          { role: 'channel', name: '群主交流', type: 'group', props: {id:'qunzhu'} },
-          { role: 'channel', name: '开发者交流', type: 'group', props: {id:'developer'} },
-          { role: 'channel', name: '3D打印', type: 'group', props: {id:'3dprint'} },
-          { role: 'channel', name: '3C数码交流', type: 'group', props: {id:'3cdigital'} },
-          { role: 'channel', name: '屏幕DIY', type: 'group', props: {id:'screen_diy'} },
-          { role: 'channel', name: '游戏玩家交流', type: 'group', props: {id:'player'} },
-          { role: 'channel', name: '闲置交易群', type: 'group', props: {id:'trade'} },
+          {
+            role: 'channel', name: '官方运营', type: 'group', props: {
+              id: 'yunying'
+            }
+          },
+          { role: 'channel', name: '群主交流', type: 'group', props: { id: 'qunzhu' } },
+          { role: 'channel', name: '开发者交流', type: 'group', props: { id: 'developer' } },
+          { role: 'channel', name: '3D打印', type: 'group', props: { id: '3dprint' } },
+          { role: 'channel', name: '3C数码交流', type: 'group', props: { id: '3cdigital' } },
+          { role: 'channel', name: '屏幕DIY', type: 'group', props: { id: 'screen_diy' } },
+          { role: 'channel', name: '游戏玩家交流', type: 'group', props: { id: 'player' } },
+          { role: 'channel', name: '闲置交易群', type: 'group', props: { id: 'trade' } },
         ]
       }
     ]
--- vite/packages/table/page/chat/page/thiskyIndex.vue
+++ vite/packages/table/page/chat/page/thiskyIndex.vue
@@ -1,6 +1,8 @@
 <template>
   <a-row class="w-full h-full">
-    <a-col flex=" 0 1 300px" class="find-left h-full flex flex-col px-3" :style="doubleCol ? { maxWidth:'336px' } :{ maxWidth:'240px'}"  style=" border-right:1px solid var(--divider);" >
+    <a-col flex=" 0 1 300px" class="find-left h-full flex flex-col px-3"
+           :style="doubleCol ? { maxWidth:'336px' } :{ maxWidth:'240px'}"
+           style=" border-right:1px solid var(--divider);">
       <!-- v-if="isFloat === false" -->
       <div class="flex flex-col">
         <div class="flex justify-between w-full mb-2.5">
@@ -12,7 +14,7 @@
         </div>
       </div>
 
-      <a-divider style="height: 1px;margin: 12px 0; background-color: var(--divider)" />
+      <a-divider style="height: 1px;margin: 12px 0; background-color: var(--divider)"/>
 
 
       <vue-custom-scrollbar :settings="settingsScroller" style="height: 100%;">
@@ -20,7 +22,7 @@
           <ChatFold :title="item.name">
             <div class="flex flex-col" v-if="doubleCol === false">
               <div v-for="item in item.children" class="flex items-center py-3 px-4 rounded-lg pointer group-item"
-              @click="currentItem(item)"
+                   @click="currentItem(item)"
               >
                 <template v-if="item.type === 'group'">
                   <MessageOutlined style="color:var(--warning);font-size: 1.25em;"/>
@@ -30,13 +32,14 @@
                 </template>
                 <template v-if="item.type === 'forum'">
                   <AppstoreOutlined style="color:var(--success);font-size: 1.25em;"/>
-                 </template>
+                </template>
                 <span class="ml-3 font-16" style="color: var(--primary-text);">{{ item.name || item.title }}</span>
               </div>
             </div>
 
             <div class="flex grid grid-cols-2 gap-1" v-else>
-              <div v-for="item in item.children"  @click="currentItem(item)" class="flex items-center py-2  rounded-lg pointer group-item">
+              <div v-for="item in item.children" @click="currentItem(item)"
+                   class="flex items-center py-2  rounded-lg pointer group-item">
                 <div class="mx-2 flex items-center">
                   <template v-if="item.type === 'group'">
                     <MessageOutlined style="color:var(--warning);font-size: 1.25em;"/>
@@ -46,7 +49,7 @@
                   </template>
                   <template v-if="item.type === 'forum'">
                     <AppstoreOutlined style="color:var(--success);font-size: 1.25em;"/>
-                   </template>
+                  </template>
                 </div>
                 <span class="ml-1 font-16" style="color: var(--primary-text);">{{ item.name || item.title }}</span>
               </div>
@@ -55,7 +58,7 @@
         </div>
       </vue-custom-scrollbar>
     </a-col>
-    <a-col flex=" 1 1 200px" class="h-full">
+    <a-col flex=" 1 1 200px" class="h-full flex flex-col">
       <div class="line-title px-4 mb-0">
         <span style="vertical-align: text-top"><template v-if="currentChannel.type === 'group'">
           <MessageOutlined style="color:var(--warning);font-size: 1.25em;"/>
@@ -65,36 +68,46 @@
         </template>
         <template v-if="currentChannel.type === 'forum'">
           <AppstoreOutlined style="color:var(--success);font-size: 1.25em;"/>
-        </template> </span> {{currentChannel.name}}
+        </template> </span> {{ currentChannel.name }}
       </div>
-      <template v-if="mainType === ''">
-        <div class="flex items-center h-full justify-center">
-          <a-empty :image="simpleImage" description="暂无内容"></a-empty>
-        </div>
-      </template>
-      <Community :forum-id="currentChannel.props.id" v-else-if="mainType === 'forum'" />
 
-      <TUIChat v-else></TUIChat>
+      <div style="height: 0;flex:1">
+        <template v-if="!currentChannel.name">
+          <div class="flex items-center h-full justify-center">
+            <a-empty :image="simpleImage" description="暂无内容"></a-empty>
+          </div>
+        </template>
+        <Community v-else-if="currentChannel.type === 'forum'" :forum-id="currentChannel.props.id" />
+        <TUIChat v-else-if="currentChannel.type==='group'"></TUIChat>
+        <template v-else-if="currentChannel.type==='link'">
+          <div v-if="currentChannel.props.openMethod==='userSelect'"  style="text-align: center;margin-top: 30%"><Emoji icon="link" :size="20"></Emoji> 当前频道需要浏览器打开。</div>
+          <iframe  v-else :src="currentChannel.props.url" class="m-2" style="border: none;background: none;border-radius: 4px;width: calc(100% - 10px);height: calc(100% - 10px)"></iframe>
+
+        </template>
+
+      </div>
     </a-col>
   </a-row>
 
 </template>
 
 <script>
-import { defineComponent, reactive, toRefs,ref,computed } from 'vue'
+import { defineComponent, reactive, toRefs, ref, computed } from 'vue'
 import { chatList } from '../../../js/data/chatList'
 import ChatDropDown from '../components/chatDropDown.vue'
 import ChatFold from '../components/chatFold.vue'
-import { AppstoreOutlined, MessageOutlined,LinkOutlined} from '@ant-design/icons-vue'
+import { AppstoreOutlined, MessageOutlined, LinkOutlined } from '@ant-design/icons-vue'
 import Community from '../Community.vue'
 import { chatStore } from '../../../store/chat'
-import  browser  from '../../../js/common/browser'
+import browser from '../../../js/common/browser'
+import Emoji from '../../../components/comp/Emoji.vue'
 
 export default defineComponent({
-  components:{
+  components: {
+    Emoji,
     ChatDropDown,
-    ChatFold,Community,
-    AppstoreOutlined,MessageOutlined,LinkOutlined
+    ChatFold, Community,
+    AppstoreOutlined, MessageOutlined, LinkOutlined
   },
 
   setup () {
@@ -104,66 +117,55 @@ export default defineComponent({
     const doubleCol = ref(chat.$state.settings.showDouble)
 
     const data = reactive({
-      list:chatList,
-      groupName:chatList[0].name,
-      enable:chatList[0].props.enableColumns,
-      summary:chatList[0].summary,
+      list: chatList,
+      groupName: chatList[0].name,
+      enable: chatList[0].props.enableColumns,
+      summary: chatList[0].summary,
 
       settingsScroller: {
-       useBothWheelAxes: true,
-       swipeEasing: true,
-       suppressScrollY: false,
-       suppressScrollX: true,
-       wheelPropagation: true
+        useBothWheelAxes: true,
+        swipeEasing: true,
+        suppressScrollY: false,
+        suppressScrollX: true,
+        wheelPropagation: true
       },
-      mainType:'',
-      currentChannel:{},
+      mainType: '',
+      currentChannel: {},
       simpleImage: '/public/img/test/load-ail.png',
     })
 
-    const updatePage = () =>{
-     doubleCol.value = chat.$state.settings.showDouble
+    const updatePage = () => {
+      doubleCol.value = chat.$state.settings.showDouble
     }
 
-    const currentItem = (item) =>{
-      // console.log('排查问题',item);
-      if(item.type === 'link'){
-        // console.log('排查',item.props.url);
-        const url = item.props.url !== '' ? item.props.url : ''
-        browser.openInUserSelect(url)  // 想天浏览器打开
-        // browser.openInSystem(url)
+    const currentItem = (item) => {
+      if(item.type==='link'){
+        if(item.props.openMethod==='userSelect'){
+          browser.openInUserSelect(item.props.url)
+        }
       }
-
-      if(item.type === 'group'){
+      if (item.type === 'group') {
         const name = `GROUP${item.props.id}`
         data.mainType = 'group'
-        console.log('排查::>>',name);
+        console.log('排查::>>', name)
         window.TUIKitTUICore.TUIServer.TUIConversation.getConversationProfile(name).then((imResponse) => {
-          console.log('排查111::>>',imResponse);
-         // 通知 TUIConversation 添加当前会话
-         // Notify TUIConversation to toggle the current conversation
-         window.TUIKitTUICore.TUIServer.TUIConversation.handleCurrentConversation(imResponse.data.conversation);
-        });
+          console.log('排查111::>>', imResponse)
+          // 通知 TUIConversation 添加当前会话
+          // Notify TUIConversation to toggle the current conversation
+          window.TUIKitTUICore.TUIServer.TUIConversation.handleCurrentConversation(imResponse.data.conversation)
+        })
       }
-
-      if(item.type === 'forum'){
-        data.mainType = 'forum'
-
-      }
-
-      data.currentChannel=item
+      data.currentChannel = item
     }
 
-
     // 通过计算属性获取是否收起侧边栏
-    const isFloat = computed(()=>{
+    const isFloat = computed(() => {
       return chat.$state.settings.enableHide
     })
 
-
     return {
-      doubleCol,isFloat,
-      ...toRefs(data),updatePage,
+      doubleCol, isFloat,
+      ...toRefs(data), updatePage,
       currentItem,
     }
   }
@@ -171,36 +173,37 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.font-500{
- font-family: PingFangSC-Medium;
- font-size: 16px;
- font-weight: 500;
+.font-500 {
+  font-family: PingFangSC-Medium;
+  font-size: 16px;
+  font-weight: 500;
 }
 
-.font-14{
- font-family: PingFangSC-Regular;
- font-size: 14px;
- font-weight: 400;
+.font-14 {
+  font-family: PingFangSC-Regular;
+  font-size: 14px;
+  font-weight: 400;
 }
 
-.font-16{
- font-family: PingFangSC-Regular;
- font-size: 16px;
- font-weight: 400;
+.font-16 {
+  font-family: PingFangSC-Regular;
+  font-size: 16px;
+  font-weight: 400;
 }
 
-.group-item{
-  &:active{
+.group-item {
+  &:active {
     filter: brightness(0.8);
     opacity: 0.8;
   }
-  &:hover{
+
+  &:hover {
     background: var(--active-secondary-bg);
   }
 }
 
-:deep(.ant-row){
+:deep(.ant-row) {
   display: flex !important;
-  flex:row nowrap !important;
+  flex: row nowrap !important;
 }
 </style>
