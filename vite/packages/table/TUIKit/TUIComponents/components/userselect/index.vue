8bc42197d45daef2d73af718173455c3a6eea85d
wjl <3123911548@qq.com>
2023-09-20 15:02:38 +0800

    【补充】创建群聊时左侧列表选中状态

--- vite/packages/table/TUIKit/TUIComponents/components/userselect/index.vue
+++ vite/packages/table/TUIKit/TUIComponents/components/userselect/index.vue
@@ -2,10 +2,10 @@
   <div class="flex" style="color: var(--primary-text);width: 650px;height: 534px;padding: 16px;">
     <div style="width:293px;">
      <vue-custom-scrollbar :settings="settingsScroller" style="height:100%; margin-bottom: 16px;">
-      <div v-for="item in userList" class="flex pointer"  style="margin-bottom: 16px;" @click="selectAdmin(item)">
-       <div class="flex">
+      <div v-for="(item,index) in userList" class="flex pointer px-4 py-3 rounded-lg" :class="{'select-bg':isSelected(index)}"  style="margin-bottom: 16px;" @click="selectAdmin(item)">
+       <div class="flex items-center">
         <a-avatar :size="32" :src="item.avatar"></a-avatar>
-        <div class="font-16" style="color: var(--primary-text);margin-left: 16px;">
+        <div class="font-16 flex items-center" style="color: var(--primary-text);margin-left: 16px;">
          {{ item.nick }}
         </div>
        </div>
@@ -18,9 +18,9 @@
     <div class="flex flex-col" style="width: 293px;">
      <vue-custom-scrollbar :settings="settingsScroller" style="height:88%; margin-bottom: 16px;">
       <div v-for="item in adminList" class="flex items-center justify-between pointer" style="margin-bottom: 16px;" >
-       <div class="flex">
+       <div class="flex items-center">
         <a-avatar :size="32" :src="item.avatar"></a-avatar>
-        <div class="font-16" style="color: var(--primary-text);margin-left: 16px;">
+        <div class="font-16 flex items-center" style="color: var(--primary-text);margin-left: 16px;">
          {{ item.nick }}
         </div>
        </div>
@@ -87,7 +87,6 @@ export default defineComponent({
    const index = _.findIndex(data.adminList,function(o){ return o.userID === item.userID})
    data.adminList.splice(index,1)
   }
-
 
   const submit = async () =>{
     switch (props.type) {
@@ -156,19 +155,25 @@ export default defineComponent({
     }
   }
 
-
   const cancel = () =>{  // 取消弹窗
     ctx.emit('closeUser')
   }
 
+  const isSelected = (index) =>{
+    return data.adminList.includes(userList.value[index])
+  }
+
   return{
    userList,
-   ...toRefs(data),selectAdmin,clearAdmin,submit,cancel
+   ...toRefs(data),selectAdmin,clearAdmin,submit,cancel,isSelected
   }
  }
 })
 </script>
 
 <style lang="scss" scoped>
-
+.select-bg{
+  background: var(--active-secondary-bg) !important;
+  border:1px solid var(--active-bg) !important;
+}
 </style>
