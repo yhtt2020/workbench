e65604744b98e87a294283d8a9f57cca4848aa2d
庄斌涛 <1436898169@qq.com>
2023-09-23 17:34:08 +0800

    【新增】 新增番茄钟优化

---
+++ vite/packages/table/components/widgets/tomatoTimer/Chart.vue
@@ -0,0 +1,116 @@
+<template>
+    <Widget :options="options" :customIndex="customIndex" :desk="desk">
+        <div class="title" @click="onClick">本周番茄时间3</div>
+        <div class="time">4h15msda</div>
+        <!-- <div class="echarts" id="mychart" :style="myChartStyle"></div> -->
+
+    </Widget>
+  </template>
+  
+<script>
+    import Widget from "../../card/Widget.vue";
+    // import * as echarts from "echarts";
+    export default {
+        name: "Chart",
+        components:{
+            Widget,
+            echarts,
+        },
+        props: {
+            customIndex: {
+                type: Number,
+                default: 0
+            },
+            customData: {
+                type: Object,
+                default: () => {}
+            },
+            desk: {
+                type: Object,
+            }
+        },
+        data(){
+            return {
+                options:{
+                    className:'card small',
+                    title:'',
+                    icon:'',
+                    type:'timer',
+                    noTitle:true,
+                },
+                // xData:["1","2","3","4","5","6","7"],
+                // yClock:[1,2,5,3,2,4,4],
+                // myChartStyle:{
+                //     // float:"left",
+                //     width:"252px",
+                //     height:"90px",
+                // },
+            }
+        },
+        async mounted() {
+            //  this.initEcharts();
+        },
+        methods: {
+            // initEcharts(){
+            //     console.log("开始渲染了");
+            //     // 多列柱状图
+            //     const mulColumnData = {
+            //         xAxis:{
+            //             data: this.xData,
+            //         },
+            //         // 图例属性
+            //         legend:{
+            //             data:["日期","时间"],
+            //         },
+            //         // y轴线条
+            //         yAxis:{
+            //             splitLine:{
+            //                 show: true,
+            //                 lineStyle: {
+            //                     type: 'solid',
+            //                     color:'#FCFCFC',//网格颜色
+            //                     opacity:"0.3"
+            //                 }
+            //             }
+            //         },
+            //         // 内部属性
+            //         series:[
+            //             {
+            //                 type: "bar",
+            //                 data:this.yClock,
+            //                 name:"时间",
+            //                 label:{
+            //                     show:true,
+            //                     position:"top",
+            //                 }
+            //             },
+            //         ],
+                    
+            //     }
+            //     const myChart = echarts.init(document.getElementById("mychart"))
+
+            //     myChart.setOption(mulColumnData);
+            //     window.addEventListener("resize",()=>{
+            //         myChart.resize();
+            //     })
+            // },
+            onClick(){
+                console.log("nihao")
+            }
+        },
+    };
+</script>
+  
+  <style scoped lang="scss">
+    // .echarts{
+    //     font-family: Helvetica;
+    //     font-size: 12px;
+    //     color: rgba(255,255,255,0.85);
+    //     text-align: left;
+    //     font-weight: 400;
+    //     width: 252px;
+    //     height: 90px;
+    //     border: 1px solid #fff;
+    // }
+  </style>
+  
---
+++ vite/packages/table/components/widgets/tomatoTimer/Clock.vue
@@ -0,0 +1,47 @@
+<template>
+    <Widget :options="options" :customIndex="customIndex" :desk="desk">
+        番茄时钟1
+    </Widget>
+  </template>
+  
+  <script>
+  import Widget from "../../card/Widget.vue";
+  export default {
+    name: "Clock",
+    components:{
+      Widget
+    },
+    props: {
+      customIndex: {
+        type: Number,
+        default: 0
+      },
+      customData: {
+        type: Object,
+        default: () => {}
+      },
+      desk: {
+        type: Object,
+      }
+    },
+    data(){
+      return {
+        options:{
+          className:'card small',
+          title:'sd',
+          icon:'',
+          type:'timer',
+          noTitle:true
+        },
+      }
+    },
+    methods: {
+
+    },
+  };
+  </script>
+  
+  <style scoped lang="scss">
+
+  </style>
+  
