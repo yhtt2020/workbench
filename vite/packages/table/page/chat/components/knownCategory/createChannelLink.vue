1ba487974b856804d047aa62c2e0e1a909ebaa4d
wjl <3123911548@qq.com>
2023-09-25 15:03:04 +0800

    【修复】代码页面错乱问题

--- vite/packages/table/page/chat/components/knownCategory/createChannelLink.vue
+++ vite/packages/table/page/chat/components/knownCategory/createChannelLink.vue
@@ -1,187 +1,126 @@
+<!-- 更新社群频道链接,在没有选择分类的情况下 -->
 <template>
- <div class="flex flex-col my-3" style="width:667px;">
-  <div class="flex w-full mb-5 h-10 items-center justify-center" style="position: relative;">
-   <div class="back-button w-10 h-10 flex items-center rounded-lg pointer active-button justify-center" style="background: var(--secondary-bg);" @click="backChannel">
-    <LeftOutlined style="font-size: 1.25em;"></LeftOutlined>
-   </div>
-   <span class="font-16-400" style="color:var(--primary-text);">选择群聊</span>
-   <div class="close-channel w-10 h-10 flex items-center rounded-lg pointer active-button justify-center"  style="background: var(--secondary-bg);" @click="closeGroup">
-    <CloseOutlined  style="font-size: 1.25em;"/>
-   </div>
-  </div>
-
-  <div class="flex px-6">
-   <div class="flex flex-col" style="width: 293px;">
-    <a-input class="h-10 search" style="border-radius: 12px;" placeholder="搜索">
-     <template #suffix>
-       <SearchOutlined style="font-size: 1.5em;color:var(--secondary-text);" class="pointer"/>
-     </template>
-    </a-input>
-
-    <span class="my-4 font-14-400" style="color:var(--secondary-text);">我创建的群聊</span>
-
-    <vue-custom-scrollbar :settings="settingsScroller" style="height:335px;">
-     <div class="flex flex-col">
-       <div v-for="(item,index) in filterList" :class="{'select-bg':isSelected(index)}" class="flex rounded-lg items-center px-4 py-3 mb-3" @click="leftListClick(item)">
-        <a-avatar shape="square" :size="40" :src="item.avatar"></a-avatar>
-        <span class="font-16-400 ml-4" style="color:var(--primary-text);">{{ item.name }}</span>
-       </div>
-     </div>
-    </vue-custom-scrollbar>
-   </div>
-   
-   <a-divider type="vertical" style="height:442px; margin: 0 16px; background-color:var(--divider);" />
-   
-   <div class="flex flex-col justify-between" style="width: 293px;">
-    <span class="">已选({{ selectGroup.length }}个)</span>
-    <vue-custom-scrollbar :settings="settingsScroller" style="height:335px;">
-     <div class="flex flex-col">
-      <div v-for="item in selectGroup" class="flex  items-center justify-between px-4 py-3 mb-2">
-        <div class="flex items-center">
-         <a-avatar shape="square" :size="40" :src="item.avatar"></a-avatar>
-         <span class="font-16-400 ml-4" style="color:var(--primary-text);">{{ item.name }}</span>
-        </div>
-        <div class="flex items-center pointer justify-center" @click="removeGroup(item)">
-         <MinusCircleFilled  style="font-size:1.5em;color:var(--secondary-text);"/>
-        </div>
-      </div>
-     </div>
-    </vue-custom-scrollbar>
-     
-    <div class="flex items-center justify-end">
-     <XtButton style="width: 64px;height:40px;margin-right: 12px;" @click="closeGroup">
-      取消
-     </XtButton>
- 
-     <XtButton style="width: 64px;height:40px; background: var(--active-bg);color:var(--active-text);">
-      确定
-     </XtButton>
+  <div class="flex flex-col my-3" style="width:500px;">
+   <div class="flex w-full mb-5 h-10 items-center justify-center" style="position: relative;">
+    <div class="back-button w-10 h-10 flex items-center rounded-lg pointer active-button justify-center" style="background: var(--secondary-bg);" @click="backChannel">
+     <LeftOutlined style="font-size: 1.25em;"></LeftOutlined>
+    </div>
+    <span class="font-16-400" style="color:var(--primary-text);">自定义网页链接</span>
+    <div class="close-channel w-10 h-10 flex items-center rounded-lg pointer active-button justify-center"  style="background: var(--secondary-bg);" @click="closeChannel">
+     <CloseOutlined  style="font-size: 1.25em;"/>
     </div>
    </div>
+ 
+   <div class="flex flex-col px-6">
+    <div class="p-4 flex rounded-lg flex-col mb-4" style="background: var(--secondary-bg);">
+     <span class="font-16-400 mb-2.5" style="color:var(--primary-text);">操作指南</span>
+     <span class="font-14-400" style="color:var(--secondary-text)">
+      输入带有“https://”或“http://”前缀的，完整的网页地址。支持选择点击链接后的打开方式，默认使用工作台设置。
+     </span>
+    </div>
+ 
+    <a-input class="h-10" v-model:value="linkName" style="border-radius: 12px; margin-bottom: 12px;" placeholder="链接名称" />
+ 
+    <a-input class="h-10" v-model:value="link" style="border-radius: 12px;" placeholder="请输入" />
+    
+    <span class="font-16-400 my-4" style="color:var(--primary-text);">链接打开方式</span>
+    <RadioTab :navList="linkType" v-model:selectType="defaultType"></RadioTab>
+    <span class="font-14-400 my-4" style="color:var(--secondary-text)">当前工作台内链接默认使用“内部浏览器”打开。</span>
+    <div class="flex items-center justify-end">
+     <XtButton style="width: 64px;height:40px;margin-right: 12px;" @click="closeChannel">取消</XtButton>
+     <XtButton style="width: 64px;height:40px; background: var(--active-bg);color:var(--active-text);" @click="submitSelect">确定</XtButton>
+    </div>
+   </div>
+ 
   </div>
-
-  
-
- </div>
-</template>
-
-<script>
-import { computed, defineComponent,reactive,toRefs} from 'vue'
-import { CloseOutlined,LeftOutlined,SearchOutlined,MinusCircleFilled } from '@ant-design/icons-vue'
-import _ from 'lodash-es'
-
-export default defineComponent({
- components:{
-  CloseOutlined,LeftOutlined,SearchOutlined,MinusCircleFilled
- },
- setup (props,ctx) {
-  const tui = window.$TUIKit
-
-  const data = reactive({
-   settingsScroller: {
-    useBothWheelAxes: true,
-    swipeEasing: true,
-    suppressScrollY: false,
-    suppressScrollX: true,
-    wheelPropagation: true
-   },
-   selectGroup:[],
-  })
-  
-  // 返回上一层
-  const backChannel = ()=>{
-   ctx.emit('back')
-  }
-
-  // 关闭和取消
-  const closeGroup = () =>{
-   ctx.emit('close')
-  }
-
-  // 过滤自己创建的群聊
-  const filterList = computed(()=>{
-   const list = tui.TUIServer.TUIGroup.store.groupList
-   const arr = _.filter(list,function(o){
-    return o.selfInfo.role !== 'Member'
+ </template>
+ 
+ <script>
+ import { defineComponent, reactive, toRefs } from 'vue'
+ import { CloseOutlined,LeftOutlined } from '@ant-design/icons-vue'
+ 
+ export default defineComponent({
+ 
+  components:{
+   CloseOutlined,LeftOutlined
+  },
+ 
+  setup (props,ctx) {
+ 
+   const data = reactive({
+    link:'',
+    linkName:''
    })
-   return arr
-  })
-
-  // 点击左侧选中
-  const leftListClick = (item) =>{
-   const index = _.findIndex(data.selectGroup,function(o){ return o.groupID === item.groupID })
-   if(index === -1){
-    data.selectGroup.push(item)
-   }else{
-    return;
+   
+   const backChannel = () =>{
+    ctx.emit('back')
+   }
+ 
+   const closeChannel = () =>{
+    ctx.emit('close')
+   }
+ 
+   const submitSelect = () =>{
+    if(data.link === '' && data.linkName !== ''){
+     const option = {
+      name:data.linkName,
+      type:'link',
+      
+     }
+    }
+   }
+ 
+ 
+ 
+   return {
+    ...toRefs(data),
+    backChannel,closeChannel,submitSelect
    }
   }
-
-  // 判断是否选中状态
-  const isSelected = (index)=>{
-   return data.selectGroup.includes(filterList.value[index])
+ })
+ </script>
+ 
+ <style lang="scss" scoped>
+ .font-16-400{
+  font-family: PingFangSC-Regular;
+  font-size: 16px;
+  font-weight: 400;
+ }
+ 
+ .close-channel{
+  position: absolute;
+  top:1px;
+  right:12px;
+ }
+ 
+ .active-button{
+  &:active{
+   filter: brightness(0.8);
+   opacity: 0.8;
   }
-  
-  // 清除选中的群聊
-  const removeGroup = (item) =>{
-   const index = _.findIndex(data.selectGroup,function(o){ return o.groupID === item.groupID })
-   data.selectGroup.splice(index,1) 
-  }
-
-  return {
-   filterList,
-   ...toRefs(data),
-   backChannel,closeGroup,leftListClick,isSelected,removeGroup
+  &:hover{
+   opacity: 0.8;
   }
  }
-})
-</script>
-
-<style lang="scss" scoped>
-
-.font-16-400{
- font-family: PingFangSC-Regular;
- font-size: 16px;
- font-weight: 400;
-}
-
-.close-channel{
- position: absolute;
- top:1px;
- right:12px;
-}
-
-.active-button{
- &:active{
-  filter: brightness(0.8);
-  opacity: 0.8;
+ 
+ .font-14-400{
+  font-family: PingFangSC-Regular;
+  font-size: 14px;
+  font-weight: 400;
  }
- &:hover{
-  opacity: 0.8;
+ 
+ .back-button{
+  position: absolute;
+  top: 1px ;
+  left: 12px;
  }
-}
-
-.font-14-400{
- font-family: PingFangSC-Regular;
- font-size: 14px;
- font-weight: 400;
-}
-
-.back-button{
- position: absolute;
- top: 1px ;
- left: 12px;
-}
-
-.select-bg{
- background: var(--active-secondary-bg) !important;
- border:1px solid var(--active-bg) !important;
-}
-
-:deep(.ant-input){
- font-size: 1.15em;
- &::placeholder{
-   color: var(--secondary-text) !important;
+ 
+ :deep(.ant-input){
+  color: var(--secondary-text) !important;
+  font-size: 1.15em;
+  &::placeholder{
+    color: var(--secondary-text) !important;
+  }
  }
-}
-</style>
+ </style>
+ 
