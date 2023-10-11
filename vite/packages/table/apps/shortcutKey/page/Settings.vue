debca8fdff9a88a5be75a54bcd3f310acf542351
陈一枭 <cyx@thisky.com>
2023-10-11 18:44:40 +0800

    【新增】支持在左侧栏隐藏软件，避免一些不想设置快捷键方案的软件干扰

--- vite/packages/table/apps/shortcutKey/page/Settings.vue
+++ vite/packages/table/apps/shortcutKey/page/Settings.vue
@@ -20,29 +20,42 @@ export default defineComponent({
       this.sessionList = []
       message.success('清理会话列表成功')
       console.log('当前的会话', this.sessionList)
+    },
+    enterEdit(app){
+      this.$router.push({
+        name:'editApp',
+        params:{
+          'exeName':app.exeName
+        }
+      })
     }
   }
 })
 </script>
 
 <template>
-  <xt-button class="mb-2" @click="clearSessionList" type="theme">清空会话列表</xt-button>
-  <div class="line-title">已经被隐藏的软件：</div>
-  <ul class="hide-app-list w-full flex gap-2">
-    <li v-for="app in hideApps">
-      <div class="hide-app">
-        <a-row :gutter="10">
-          <a-col>
-            <a-avatar :src="'file://'+app.software.icon" shape="square"></a-avatar>
-          </a-col>
-          <a-col>
-            <div>{{ app.title }}</div>
-            <div> {{ app.exeName }}</div>
-          </a-col>
-        </a-row>
-      </div>
-    </li>
-  </ul>
+  <h3>快捷键设置</h3>
+  <div class="p-4">
+    <xt-button class="mb-5" @click="clearSessionList" type="theme">清空会话列表</xt-button>
+    <div class="line-title">已经被隐藏的软件：</div>
+    <div class="pl-2" v-if="hideApps.length===0">暂无被隐藏的软件，您可以在软件设置中隐藏不需要的软件。</div>
+    <ul class="hide-app-list w-full flex gap-2">
+      <li class="pointer " v-for="app in hideApps" @click="enterEdit(app)">
+        <div class="hide-app">
+          <a-row :gutter="10">
+            <a-col>
+              <a-avatar :src="'file://'+app.software.icon" shape="square"></a-avatar>
+            </a-col>
+            <a-col>
+              <div>{{ app.title }}</div>
+              <div> {{ app.exeName }}</div>
+            </a-col>
+          </a-row>
+        </div>
+      </li>
+    </ul>
+  </div>
+
 </template>
 
 <style scoped lang="scss">
