8d60697feb08793aa38adf0c96515e45730a9022
胡家羊 <mengzgh@163.com>
2023-10-12 16:17:10 +0800

    【修复bug】社群里发布内容时，应该默认选中该版块

--- vite/packages/table/page/chat/com/publishModal.vue
+++ vite/packages/table/page/chat/com/publishModal.vue
@@ -65,7 +65,7 @@
             <div class="flex items-center justify-between h-[56px] ">
                 <!-- <a-button type="text" class=" xt-text xt-bg-2 font-14"
                     style="border-radius:10px ; color: var(--secondary-text) !important;">想天工作台/桌面分享 ></a-button> -->
-                <a-select v-model:value="cascaderValue" :options="options" placeholder="想天工作台/桌面分享" :loadData="loadData" :bordered="false" @change="handleChange"
+                <a-select v-model:value="cascaderValue" :options="options" :placeholder="holderName.name" :loadData="loadData" :bordered="false" @change="handleChange"
                     style=" font-size: 14px; border-radius: 10px;" change-on-select>
                     <template #suffixIcon>
                         <Icon icon="fluent:chevron-left-16-filled" class="text-base rotate-180"></Icon>
@@ -195,7 +195,7 @@ onMounted(() => {
     textareaElement?.focus()
     useCommunStore.getCommunityInfo(props.forumId)
     useCommunStore.getCommunityCate(props.forumId)
-    // console.log(useCommunStore.communityCate[0].id);
+    console.log(useCommunStore.communityInfo.forum.name);
     // console.log(navigator.plugins);
 
 
@@ -225,6 +225,9 @@ const handleChange = (value) => {
     cascaderValue.value = value
     console.log(cascaderValue.value);
 }
+const holderName=computed(() => {
+    return useCommunStore.communityInfo.forum
+})
 const settingsScroller = reactive({
     useBothWheelAxes: true,
     swipeEasing: true,
