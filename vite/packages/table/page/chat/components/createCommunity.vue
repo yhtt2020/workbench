15055fc439dff5bf8d72d7a77a550a117949426f
庄斌涛 <1436898169@qq.com>
2023-10-16 09:10:22 +0800

    【修复】推广中心bug修复

--- vite/packages/table/page/chat/components/createCommunity.vue
+++ vite/packages/table/page/chat/components/createCommunity.vue
@@ -26,11 +26,7 @@
       <div class="overflow-hidden">
         <a-avatar :src="avatarUrl" style="height:64px;width: 64px;border-radius: 0;" :style="{'filter': bgColor?`drop-shadow(#${bgColor} 80px 0)`:'',transform:bgColor?'translateX(-80px)':''}"></a-avatar>
       </div>
-      <div class="flex items-center rounded-full p-3 justify-center"
-       style="width:24px;height:24px;position: absolute;bottom:-3px;right:-3px;background: var(--active-bg);border: 2px solid var(--primary-text);"
-      >
-       <CameraOutlined style="font-size:1em;"/>
-      </div>
+    <communityIcon icon="fluent:add-16-filled" width="20" height="20" style="font-size: 1.5rem;width:24px;height:24px;position: absolute;bottom:-3px;right:-3px;border: 2px solid var(--secondary-text);border-radius: 50%;" color="var(--secondary-text)"/>
     </div>
     <SelectIcon @isIconShow="iconVisible = false" @getAvatar="getAvatar" v-show="iconVisible" :isCustom="isCustom" :customTitle="customTitle"></SelectIcon>
      <div class="flex items-center justify-center font-16"  style="color:var(--secondary-text);margin-top: 12px;"> 推荐图片尺寸：256*256，不能超过4MB </div>
