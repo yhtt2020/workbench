be30cb29464c3575adc2d493917c4ac9165fbb4e
陈一枭 <cyx@thisky.com>
2023-10-19 10:39:20 +0800

    【调整】增加取消全屏的功能，mac测试通过

--- vite/packages/table/js/common/common.ts
+++ vite/packages/table/js/common/common.ts
@@ -2,16 +2,23 @@ import { notification } from "ant-design-vue";
 import {appStore} from "../../store";
 
 export async function toggleFullScreen() {
-  tsbApi.window.setFullScreen(true)
-  appStore().showWindowController = false
-  notification.info({
-    message: '功能提示',
-    description:
-      '进入全屏模式，会自动隐藏窗口控制按钮，可在设置中重新开启。',
-    onClick: () => {
-      console.log('Notification Clicked!');
-    },
-  });
+  const isFullScreen=await tsbApi.window.isFullScreen()
+  if(!isFullScreen){
+    tsbApi.window.setFullScreen(true)
+    appStore().showWindowController = false
+    notification.info({
+      message: '功能提示',
+      description:
+        '进入全屏模式，会自动隐藏窗口控制按钮，可在设置中重新开启。',
+      onClick: () => {
+        console.log('Notification Clicked!');
+      },
+    });
+  }else{
+    tsbApi.window.setFullScreen(false)
+    appStore().showWindowController = true
+  }
+
 
   // this.fullScreen()   //触发全屏的按钮
 }
