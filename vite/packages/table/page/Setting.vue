0738ec9fc1fe8551d1897d169341696ebb7b4340
庄斌涛 <1436898169@qq.com>
2023-11-07 17:24:50 +0800

    【修改】离线模式下部分开关隐藏

--- vite/packages/table/page/Setting.vue
+++ vite/packages/table/page/Setting.vue
@@ -13,7 +13,7 @@
           ">
           <h3 style="color: var(--primary-text)">快速开关功能</h3>
           <a-row :gutter="[20, 20]" style="font-size: 1.2em; text-align: center">
-            <a-col :span="12">
+            <a-col :span="12" v-if="!isOffline">
               <div class="btn relative">
                 弹幕
                 <br />
@@ -42,7 +42,7 @@
               </div>
             </a-col>
 
-            <a-col  :span="12">
+            <a-col  :span="12" v-if="!isOffline">
               <div class="btn relative">
                 社群沟通<br />
                 <a-switch v-model:checked="settings.enableChat"></a-switch>
@@ -54,7 +54,7 @@
                 <a-switch v-model:checked="settings.noticeEnable"></a-switch>
               </div>
             </a-col>
-            <a-col :span="12">
+            <a-col :span="12" v-if="!isOffline">
               <div style="cursor: help" @click="tipSimple" class="btn relative">
                 极简模式<br />
                 <a-switch @click.stop="() => { }" v-model:checked="simple"></a-switch>
