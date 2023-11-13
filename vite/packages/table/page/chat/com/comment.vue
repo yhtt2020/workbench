fff1baadce14436c9b0f10caba71e9ce3715972f
胡家羊 <mengzgh@163.com>
2023-11-13 10:38:08 +0800

    【调整】修改评论加载方式

--- vite/packages/table/page/chat/com/comment.vue
+++ vite/packages/table/page/chat/com/comment.vue
@@ -5,7 +5,7 @@
             评论 {{ props.reply }}
         </div>
         <MainReplyComment :commentList="item" v-for="(item, index) in commentsList" :key="index" :uid="props.uid"></MainReplyComment>
-        <xt-button class="check-more" @click="loadingMoreComments" v-if="loadingMoreVisible">查看更多评论</xt-button>
+        <xt-button class="check-more" @click="loadingMoreComments" :w="120" :h="40" >查看更多评论</xt-button>
     </div>
 </template>
 
@@ -44,7 +44,7 @@ const tid=computed(()=>{
     return store.communityPostDetail.pay_set.tid?store.communityPostDetail.pay_set.tid:store.communityPostDetail.id
 })
 onMounted(async()=>{
-    // console.log(props.tid,tid.value,'onMounted')
+    console.log(commentList.value,'onMounted')
     // await store.getCommunityPostReply(props.tid,1)
     commentsList.value.push(commentList.value)
     commentsList.value=commentsList.value.flat(1)
@@ -56,12 +56,19 @@ onUnmounted(()=>{
     console.log(commentsList.value,'onUnmounted')
     commentsList.value=[]
 })
-const loadingMoreVisible=computed(()=>{
-    if((totalComment.value && commentsList.value.length>= totalComment.value.length) || commentList.value.length<10){
-        return false
-    }else{
-        return true
-    }
+// const loadingMoreVisible=computed(()=>{
+//     if((totalComment.value && commentsList.value.length>= totalComment.value.length) || commentList.value.length<10){
+//         return false
+//     }else{
+//         return true
+//     }
+// })
+// 监听tid变化
+watch(() => props.tid,(newValue,oldValue)=>{
+    //if(newValue!==oldValue){
+       // commentsList.value=[]
+       console.log(newValue,oldValue,'tid')
+    //}
 })
 </script>
 <style lang='scss' scoped>
@@ -88,35 +95,50 @@ const loadingMoreVisible=computed(()=>{
 // }
 </style> -->
 
-
-<!-- 之前版本 -->
 <template>
     <div class="w-full ">
         <reply @addComment="getReplyText"/>
         <div class="mb-4 text-smxt-text">
             评论 {{ props.reply }}
         </div>
-        <MainReplyComment :commentList="item" v-for="(item, index) in commentList.list" :key="index" :uid="props.uid"></MainReplyComment>
-        <a-pagination v-model:current="current" :total="totalReply" simple @change="changePage" v-if="paginationVisible" class="xt-text-2"/>
+        <MainReplyComment :commentList="item" v-for="(item, index) in moreComments" :key="index" :uid="props.uid"></MainReplyComment>
+        <!-- <a-pagination v-model:current="current" :total="totalReply" simple @change="changePage" v-if="paginationVisible" class="xt-text-2"/> -->
+        <xt-button class="check-more" @click="changePage" :w="120" :h="40" v-if="paginationVisible">查看更多评论</xt-button>
     </div>
 </template>
 
 <script setup lang='ts'>
-import { ref, computed } from 'vue'
+import { ref, computed,onMounted } from 'vue'
 import MainReplyComment from './MainReplyComment.vue'
 import reply from './Reply.vue'
 import {useCommunityStore} from '../commun'
 const current = ref(1)
-const changePage=(val)=>{
+const list=ref([])
+const changePage=async()=>{
+    if(list.value.length==0){
+      list.value=commentList.value.list  
+    }
+    console.log(list.value,'changePage-before')
     let tid=store.communityPostDetail.pay_set.tid?store.communityPostDetail.pay_set.tid:store.communityPostDetail.id
-    current.value=val
-    store.getCommunityPostReply(tid,val)
+    current.value++
+    await store.getCommunityPostReply(tid,current.value)
+    list.value.push(commentList.value.list)
+    list.value=list.value.flat(1)
+    console.log(list.value,'changePage-after')
+    console.log(moreComments.value,'moreComments')
+    
 }
 const totalReply=computed(()=>{
     return props.reply
 })
 const paginationVisible=computed(()=>{
-    return totalReply.value>10?true:false
+    // return commentList.value.totalCount>10?true:false
+    if(commentList.value.totalCount>10 && list.value.length<=commentList.value.totalCount ){
+        return true
+    }else{
+        return false
+        
+    }
 })
 const store=useCommunityStore()
 const props=defineProps({
@@ -128,13 +150,24 @@ const props=defineProps({
 const commentList = computed(()=>{
     return store.communityReply
 })
-
 const getReplyText=(val)=>{
     commentList.value=val.value
 }
+const moreComments=computed(()=>{
+    if(list.value.length>0){
+        return list.value
+    }else{
+        return commentList.value.list
+    }
+})
 
 </script>
 <style lang='scss' scoped>
+.check-more{
+    margin: 10px auto;
+    background-color: transparent;
+    color: var(--active-bg) !important;
+}
 :deep(.ant-pagination){
     text-align: center;
 }
