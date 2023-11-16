186b8ca1c3c6185f226f79783f845d434efcb3ba
胡家羊 <mengzgh@163.com>
2023-11-16 17:33:44 +0800

    【新增】导航栏设置数据

---
+++ vite/packages/table/components/desk/navigationBar/index.ts
@@ -0,0 +1,30 @@
+export const navigationPosition = {
+    bottomBar:{
+        title:'底部导航',
+        describe:'在工作台底部显示导航栏',
+    },
+    leftBar:{
+        title:'左侧导航',
+        describe:'在工作台左侧显示导航栏',
+    },
+    rightBar:{
+        title:'右侧导航',
+        describe:'在工作台右侧显示导航栏',
+    },
+
+}
+
+export const navigationFunction={
+    userCenter:{
+        title:'用户中心',
+        describe:'在工作台底部显示用户中心',
+    },
+    communityAssistant:{
+        title:'社区助手',
+        describe:'在工作台底部显示社区助手',
+    },
+    taskCenter:{
+        title:'任务中心',
+        describe:'在工作台底部显示任务中心',
+    }
+}
---
+++ vite/packages/table/components/desk/navigationBar/navigationSetting.vue
@@ -0,0 +1,14 @@
+<template>
+<Modal title="导航栏设置" v-model:visible="settingVisible">
+
+</Modal>
+</template>
+
+<script setup lang='ts'>
+import {ref,reactive} from 'vue'
+// 常量设置
+const settingVisible=ref(false)
+</script>
+<style lang='scss' scoped>
+
+</style>
