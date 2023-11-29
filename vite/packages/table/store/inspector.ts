44f072c20a38a37ff36a014b297a1ebab0b69f8b
陈一枭 <yixiao2020@qq.com>
2023-11-29 15:31:06 +0800

    【优化】解除部分存储dbStorage存储

--- vite/packages/table/store/inspector.ts
+++ vite/packages/table/store/inspector.ts
@@ -136,7 +136,7 @@ export const inspectorStore = defineStore(
         // 自定义存储的 key，默认是 store.$id
         // 可以指定任何 extends Storage 的实例，默认是 sessionStorage
         paths:['frequent','dataSource'],
-        storage: dbStorage,
+        storage: localStorage,
         // state 中的字段名，按组打包储存
       }]
     }
