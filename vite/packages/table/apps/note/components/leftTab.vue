4038844859d5f0bc402ea35bc09769388bf3b65d
庄斌涛 <1436898169@qq.com>
2023-11-30 14:42:08 +0800

    【优化】备忘录数据丢失问题

--- vite/packages/table/apps/note/components/leftTab.vue
+++ vite/packages/table/apps/note/components/leftTab.vue
@@ -17,47 +17,54 @@ export default {
       ...mapWritableState(noteStore, ['isSelTab','selNote','searchValue']),
     },
     data() {
-    return {
-      menuList:[
-        {
-          newIcon: "fluent:notepad-12-regular",
-          isSel:false,
-          title:'便签',
-          callBack: () => {
-            this.isSelTab = false
-            this.getNotes()
-            this.menuList[0].isSel=true
-            this.menuList[1].isSel=false
-            this.selNote=-1
-            this.searchValue = ''
+      return {
+        flag: true,
+        menuList:[
+          {
+            newIcon: "fluent:notepad-12-regular",
+            isSel:false,
+            title:'便签',
+            callBack: () => {
+              if (this.flag) {
+                this.flag=false
+                this.isSelTab = false
+                this.getNotes().then(()=>{
+                  this.menuList[0].isSel=true
+                  this.menuList[1].isSel=false
+                  this.selNote=-1
+                  this.searchValue = ''
+                  this.flag=true
+                })
+              }
+            },
           },
-        },
-        {
-          newIcon: "akar-icons:trash-can",
-          isSel:false,
-          title:"回收站",
-          callBack: () => {
-            this.isSelTab = true
-            this.getNotes()
-            this.menuList[0].isSel=false
-            this.menuList[1].isSel=true
-            
-            this.selNote=-1
-            this.searchValue = ''
+          {
+            newIcon: "akar-icons:trash-can",
+            isSel:false,
+            title:"回收站",
+            callBack: () => {
+              if (this.flag) {
+                this.flag=false
+                this.isSelTab = true
+                this.getNotes().then(()=>{
+                  this.menuList[0].isSel=false
+                  this.menuList[1].isSel=true
+                  this.selNote=-1
+                  this.searchValue = ''
+                  this.flag=true
+                })
+              }
+            },
           },
-        },
-        {
-          full: true,
-        },
-      ],
-
-    };
-  },
-  methods:{
-    
-    ...mapActions(noteStore, ['getNotes']),
-
-  }
+          {
+            full: true,
+          },
+        ],
+      };
+    },
+    methods:{
+      ...mapActions(noteStore, ['getNotes']),
+    }
 };
 </script>
 <style lang="scss" scoped>
