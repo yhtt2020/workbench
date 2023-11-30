94e4c77a8225e5d53e6dd3798e8f14bcb11cd6c8
wjl <3123911548@qq.com>
2023-11-30 15:01:30 +0800

    【优化】工作台顶部辅助模式状态样式

--- vite/packages/table/components/drawer/TopPanelButton.vue
+++ vite/packages/table/components/drawer/TopPanelButton.vue
@@ -1,23 +1,16 @@
 <template>
  <div class="flex items-center justify-center h-12 relative mr-3 no-drag">
-  <div v-if="!assistStatus" style="width:auto !important;"  @click="closeAided" class="p-1 btn-hover  rounded-md pointer category-button">
-  <div class="flex items-center justify-center">
-   <div class="mr-1 flex items-center justify-center">
-    <TopButtonIcon icon="fluent:accessibility-16-regular" class="xt-text" style="font-size: 1.25rem;"/>
-   </div>
-   <span class="xt-text xt-font font-14">辅助模式</span>
-  </div>
-  </div>
-  <div class="absolute xt-bg-2 flex items-center p-3 justify-center h-14 rounded-md no-drag" v-if="showClose" style="width: 200px;top:38px; right: -63px; z-index: 50;">
-   <xt-button h="40" class="category-button" style="border-radius: 6px; padding:0 8px !important; width: auto !important;" @click="closeAidedModal">
+  <xt-menu :menus="menus" model="click" name="name"  v-if="assistStatus">
+   <div style="width:auto !important;"  class="p-1 btn-hover  rounded-md pointer category-button">
     <div class="flex items-center justify-center">
-     <TopButtonIcon icon="fluent:accessibility-16-regular" class="xt-text mr-4" style="font-size: 1.25rem;"/>
-     <span class="xt-text xt-font font-14">关闭辅助模式</span>
+    <div class="mr-1 flex items-center justify-center">
+     <TopButtonIcon icon="fluent:accessibility-16-regular" class="xt-text" style="font-size: 1.25rem;"/>
     </div>
-   </xt-button>
-  </div>
+    <span class="xt-text xt-font font-14">辅助模式</span>
+    </div>
+   </div>
+  </xt-menu>
  </div>
- 
 </template>
 
 <script setup>
@@ -28,22 +21,23 @@ import { Icon as TopButtonIcon } from '@iconify/vue';
 
 const app = appStore();
 const {  aided } = storeToRefs(app);
+const showClose = ref(false);
 
 const assistStatus = computed(()=>{
  return aided.value
 })
 
-const showClose = ref(false);
 
-const closeAided = () =>{
- showClose.value = !showClose.value;
-}
-
-const closeAidedModal = () =>{
- aided.value = false;
- showClose.value = false;
-}
-
+const menus = ref([
+  {
+   newIcon:'fluent:accessibility-16-regular',
+   name:'关闭辅助模式',
+   callBack:()=>{
+    aided.value = false;
+    showClose.value = false;
+   }
+  },
+])
 </script>
 
 <style lang="scss" scoped>
