590975c6190ea8232ac6eec4ac276e031862c763
xiaoyang <1611037570@qq.com>
2023-12-02 08:47:53 +0800

    【优化】多图标模式样式

--- vite/packages/table/components/widgets/myIcons/icons/Custom.vue
+++ vite/packages/table/components/widgets/myIcons/icons/Custom.vue
@@ -1,71 +1,35 @@
 <template>
   <xt-zoom v-model:width="w" v-model:height="h" :style="[size]">
-    <div
-      class="xt-bg xt-shadow rounded-xl xt-text relative controller"
-      :style="[size]"
-    >
-      <XtBaseIcon
-        @click="iconsRightClick()"
-        icon="gengduo1"
-        class="absolute icon"
-        style="right: 8px; top: 4px"
-      ></XtBaseIcon>
-      <div
-        class="text-center cursor-pointer mx-auto truncate"
-        :style="[titleSize]"
-        @click="titleVisible = true"
-      >
+    <div class="xt-bg xt-shadow rounded-xl xt-text relative controller" :style="[size]">
+      <XtBaseIcon @click="iconsRightClick()" icon="gengduo1" class="absolute icon" style="right: 8px; top: 4px">
+      </XtBaseIcon>
+      <div class="text-center cursor-pointer mx-auto truncate flex justify-center  items-center" style="height: 30px;" :style="[titleSize]"
+        @click="titleVisible = true">
         {{ groupTitle }}
       </div>
       <div class="flex flex-wrap justify-between">
         <div v-for="i in count" @contextmenu.prevent.stop="rightClick(i - 1)">
-          <Icon
-            v-if="iconList[i - 1]"
-            v-bind="iconList[i - 1]"
-            :index="i - 1"
-            :size="icons"
-            :data-index="i - 1"
-            :state="true"
-          >
+          <Icon v-if="iconList[i - 1]" v-bind="iconList[i - 1]" :index="i - 1" :size="icons" :data-index="i - 1"
+            :state="true">
           </Icon>
         </div>
-        <div
-          :style="[iconsState]"
-          class="flex justify-center items-center flex-col cursor-pointer xt-hover rounded-xl"
-          @click="fullScreenClick($event)"
-          v-if="iconList.length > count"
-        >
+        <div :style="[iconsState]" class="flex justify-center items-center flex-col cursor-pointer xt-hover rounded-xl"
+          @click="fullScreenClick($event)" v-if="iconList.length > count">
           <XtIcon icon="gengduo1" w="38"></XtIcon>
-          <div
-            class="truncate w-full text-center text-xs"
-            style="margin-top: 6px"
-          >
+          <div class="truncate w-full text-center text-xs" style="margin-top: 6px">
             更多
           </div>
         </div>
         <!-- 占位 -->
-        <div
-          v-for="i in blankLabel"
-          :key="`placeholder-${i}`"
-          style="width: 93px; height: 99px"
-        ></div>
+        <div v-for="i in blankLabel" :key="`placeholder-${i}`" style="width: 93px; height: 99px"></div>
       </div>
     </div>
   </xt-zoom>
   <XtDrawer v-model="visible">
-    <Set
-      @editIcons="editIcons(index)"
-      @deleteIcons="deleteIcons(index)"
-      v-model:data="visible"
-    ></Set>
+    <Set @editIcons="editIcons(index)" @deleteIcons="deleteIcons(index)" v-model:data="visible"></Set>
   </XtDrawer>
   <Teleport to="body">
-    <xt-old-modal
-      v-model="titleVisible"
-      @close="titleVisible = false"
-      title="修改分组名"
-      :isFooter="false"
-    >
+    <xt-old-modal v-model="titleVisible" @close="titleVisible = false" title="修改分组名" :isFooter="false">
       <div class="h-12" style="width: 400px">
         <XtInput v-model="title" @blur="titleBlur()"></XtInput>
       </div>
@@ -85,7 +49,7 @@ export default {
     height: {},
     iconList: {
       type: Object,
-      default: () => {},
+      default: () => { },
     },
     groupTitle: {
       type: String,
@@ -207,7 +171,12 @@ export default {
 .icon {
   display: none;
 }
+
 .controller:hover .icon {
   display: block !important;
+}
+
+:deep(.box) {
+  height: 86px !important;
 }
 </style>
