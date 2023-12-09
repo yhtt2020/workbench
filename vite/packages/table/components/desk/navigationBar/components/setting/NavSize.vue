060576c32a621c29a6e5bdc715ef885f40766cec
胡家羊 <mengzgh@163.com>
2023-12-09 17:30:43 +0800

    【优化】导航栏设置

--- vite/packages/table/components/desk/navigationBar/components/setting/NavSize.vue
+++ vite/packages/table/components/desk/navigationBar/components/setting/NavSize.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="w-[452px]">
-        <SliderList :sliderList="navSize"></SliderList>
-        <SliderList :sliderList="sliderList"></SliderList>
-        <CustomIconBg></CustomIconBg>
+        <SliderList :sliderList="navSize" @change="changeNav" @reset="resetNav"></SliderList>
+        <SliderList :sliderList="sliderList" @change="changeSlider" @reset="resetSlider"></SliderList>
+        <CustomIconBg :colorSwitch="navigationStore.navColorSwitch"
+            @update:colorSwitch="navigationStore.navColorSwitch = !navigationStore.navColorSwitch" @changeBg="changeBg">
+        </CustomIconBg>
     </div>
 </template>
 
@@ -12,39 +14,47 @@ import { useNavigationStore } from '../../navigationStore'
 import CustomIconBg from '../EditIcon/Children/CustomIconBg.vue';
 import SliderList from './SliderList.vue';
 const navigationStore = useNavigationStore()
-const sliderList=ref([
+const sliderList = ref([
     {
         title: '导航栏圆角',
         min: 1,
         max: 80,
         value: navigationStore.navRadius,
-        step:2
+        step: 2
     },
     {
         title: '图标圆角',
         min: 1,
         max: 52,
         value: navigationStore.iconRadius,
-        step:2
+        step: 2
 
     },
 ])
-const navSize=ref([
+const navSize = ref([
     {
-        title:'导航栏尺寸',
-        min:1,
-        max:300,
-        value:navigationStore.navSize,
-        step:10
+        title: '导航栏尺寸',
+        min: 1,
+        max: 300,
+        value: navigationStore.navSize,
+        step: 10
     }
 ])
-</script>
-<style lang='scss' scoped>
-.input{
-    margin-left: 16px !important;
-    height: 40px !important;
-    width: 80px !important;
-    border-radius: 12px !important;
-    border: 1px solid var(--divider) !important;
+const changeBg = (value) => {
+    console.log(value, '====>>>>');
 }
-</style>
+const resetNav = (item, index) => {
+    item.value = '100%'
+    navigationStore.navSize = '100%'
+}
+const changeNav = (value) => {
+    navigationStore.navSize = value
+}
+const changeSlider = (value) => {
+    console.log(value, '====>>>>');
+}
+const resetSlider = (item, index) => {
+    console.log(item,index,'====>>>>')
+}
+</script>
+<style lang='scss' scoped></style>
--- vite/packages/table/components/desk/navigationBar/components/setting/SliderList.vue
+++ vite/packages/table/components/desk/navigationBar/components/setting/SliderList.vue
@@ -3,15 +3,18 @@
         <div class="w-full" v-for="(item, index) in props.sliderList">
             <div class="flex mb-2">
                 <div class="mr-2 text-base xt-text">{{ item.title }}</div>
-                <xt-new-icon icon="fluent:arrow-clockwise-16-regular" size="16" @click="reset"></xt-new-icon>
+                <xt-new-icon icon="fluent:arrow-clockwise-16-regular" size="18" @click="reset(item,index)"></xt-new-icon>
             </div>
-            <a-row>
+            <a-row align="middle">
                 <a-col :span="18">
-                    <a-slider v-model:value="item.value" :min="item.min" :max="item.max" :step="item.step"/>
+                    <a-slider v-model:value="item.value" :min="item.min" :max="item.max" :step="item.step" @change="changeValue"/>
                 </a-col>
                 <a-col :span="4">
                     <a-input-number v-model:value="item.value" :min="1" :max="20" class="input"
-                        style="width: 80px;height: 40px;border: 1px solid var(--divider);border-radius: 12px;margin-left: 16px;" />
+                        style="
+                        width: 80px;height: 40px;border: 1px solid var(--divider);
+                        border-radius: 12px;margin-left: 16px;line-height: 38px;
+                        color: var(--primary-text);" @change="changeValue"/>
                 </a-col>
             </a-row>
             <div class="h-[1px] bg-[var(--divider)] mt-3 mb-3" v-if="index !==props.sliderList.length-1"></div>
@@ -22,6 +25,7 @@
 
 <script setup lang='ts'>
 import { ref, reactive, watch } from 'vue'
+const emit=defineEmits(['reset','change'])
 const props = defineProps({
     sliderList: {
         type: Object,
@@ -35,10 +39,16 @@ const props = defineProps({
     }
 
 })
-const reset = () => {
-    console.log(1111111);
+// 重置数据
+const reset = (item,index) => {
+    emit('reset',item,index)
 
 }
-
+// 修改数据
+const changeValue=(value)=>{
+    console.log(value,'====>>>>');
+    
+    emit('change',value)
+}
 </script>
 <style lang='scss' scoped></style>
