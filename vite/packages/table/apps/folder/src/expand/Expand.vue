f51961c945153747b5cea390fd03765fc4f12da6
xiaoyang <1611037570@qq.com>
2023-12-26 14:31:23 +0800

    【优化】遗漏补充

---
+++ vite/packages/table/apps/folder/src/expand/Expand.vue
@@ -0,0 +1,28 @@
+<template>
+  <Folder
+    v-bind="newData"
+    :expand="{
+      disabled: true,
+    }"
+  ></Folder>
+</template>
+
+<script setup>
+import Folder from "../../index";
+const props = defineProps({
+  data: {},
+});
+const newData = {
+  ...props.data,
+};
+newData.customData.cardSize = {
+  name: "860,630",
+  width: 3,
+  height: 3,
+};
+
+
+// 860,420
+</script>
+
+<style lang="scss" scoped></style>
