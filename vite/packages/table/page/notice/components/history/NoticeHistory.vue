b02dfbfbeb03d4cadeada95b901a2a0f67b096c8
wjl <3123911548@qq.com>
2023-12-29 13:57:08 +0800

    【新增】搭建消息通知发送弹窗（开发中）

---
+++ vite/packages/table/page/notice/components/history/NoticeHistory.vue
@@ -0,0 +1,29 @@
+<template>
+ <div class="p-4 w-full flex flex-col h-full">
+  <div class="w-full flex items-center justify-between">
+    <div class="flex items-center justify-center">
+       <xt-new-icon icon="fluent:clock-16-regular" size="16"></xt-new-icon>
+       <span class="xt-font xt-text font-14 font-400 ml-3">历史消息</span>
+    </div>
+    <xt-button w="32" h="32" style="border-radius: 8px;" @click="closeModal">
+      <div class="flex items-center justify-center">
+        <xt-new-icon icon="fluent:dismiss-16-regular" size="16"></xt-new-icon>
+      </div>
+    </xt-button>
+  </div>
+ </div>
+</template>
+
+<script setup>
+
+const emit = defineEmits(['close']);
+
+const closeModal = () =>{
+ emit('close')
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
