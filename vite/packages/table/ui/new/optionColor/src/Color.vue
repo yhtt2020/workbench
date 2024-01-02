fa09c7351b86dc0ca5e4cee4c044d1af301a4699
胡家羊 <mengzgh@163.com>
2024-01-02 13:55:00 +0800

    【修改】颜色选择器参数

--- vite/packages/table/ui/new/optionColor/src/Color.vue
+++ vite/packages/table/ui/new/optionColor/src/Color.vue
@@ -3,15 +3,12 @@
     <template v-if="switch">
       <div class="item-box">
         <div class="flex items-center justify-center mr-4 item" :key=" item "
-          :style=" { background: props.colorList[`${'color' + item}`] } " v-for="   item, index    in   6   "
+          :style=" { background: props.colorList[`${'color' + item}`] } " v-for="     item, index      in     6     "
           @click="chooseBg(item)">
-          <xt-new-icon
-            v-show="color == colorList[`${'color' + item}`]"
-            icon="ri:checkbox-circle-fill"
-            size="20"
-          ></xt-new-icon>
+          <xt-new-icon v-show=" color == colorList[`${'color' + item}`] " icon="ri:checkbox-circle-fill"
+            size="20"></xt-new-icon>
         </div>
-        <colorP @change=" updateBackground " />
+        <colorP @change=" updateBackground " :defaultList=" colorList " :linearList=" linearList " :isLinear=" isLinear "/>
       </div>
 
     </template>
@@ -25,6 +22,9 @@ export interface colorProps {
   switch?: boolean;
   color?: string;
   colorList?: any;
+  linearList?: any;
+  // 是否有渐变
+  isLinear?: boolean;
 }
 const props = withDefaults(defineProps<colorProps>(), {
   switch: true,
@@ -35,7 +35,24 @@ const props = withDefaults(defineProps<colorProps>(), {
     color4: '#DE5D5D',
     color5: '#57BF60',
     color6: '#DE5DB3',
-  }
+    color7: '#000000',
+    // color8: '#DE5DB3',
+    color8: '#43CADE',
+    color9: '#FFFFFF',
+  },
+  linearList: {
+    color1: ' linear-gradient(135deg, #A8A9EE 0%, #3398DA 100%)',
+    color2: ' linear-gradient(135deg, #CEA8EE 0%, #333ADA 100%)',
+    color3: ' linear-gradient(135deg, #549FA6 0%, #33DA9E 100%)',
+    color4: ' linear-gradient(135deg, #EEA8A8 0%, #DA337A 100%',
+    color5: '  linear-gradient(135deg, #C0EEA8 0%, #DAAD33 100%)',
+    color6: '  linear-gradient(135deg, #D55E5E 0%, #DA6733 100%)',
+    color7: '  linear-gradient(135deg, #A8BCEE 0%, #3363DA 100%)',
+    color8: ' linear-gradient(135deg, #EEA8A8 0%, #33B4DA 100%)',
+    color9: '  linear-gradient(135deg, #EEA8A8 0%, #B633DA 100%)',
+    color10: ' linear-gradient(135deg, #A8EEA8 0%, #3354DA 100%)',
+  },
+  isLinear: true
 });
 const { color } = toRefs(props);
 // 按钮回调
@@ -46,7 +63,7 @@ const chooseBg = (item) => {
 };
 const updateBackground = (item) => {
   console.log("item :>> ", item);
-  
+
   emit("update:color", item);
 };
 const colorIndex = ref(0);
